ef5b54fe7ff56e400518093076dfe4afb8f479d8
whyrusleeping <why@ipfs.io>
2019-07-15 23:07:03 -0700

    implement commit sector and some storage market methods

--- chain/actors/actor_storagemarket.go
+++ chain/actors/actor_storagemarket.go
@@ -21,6 +21,8 @@ func (sma StorageMarketActor) Exports() []interface{} {
 	return []interface{}{
 		nil,
 		sma.CreateStorageMiner,
+		nil, // TODO: slash consensus fault
+		sma.UpdateStorage,
 	}
 }
 
@@ -97,4 +99,71 @@ func SupportedSectorSize(ssize types.BigInt) bool {
 		return true
 	}
 	return false
+}
+
+type UpdateStorageParams struct {
+	Delta types.BigInt
+}
+
+func (sma StorageMarketActor) UpdateStorage(act *types.Actor, vmctx types.VMContext, params *UpdateStorageParams) (types.InvokeRet, error) {
+	var self StorageMarketState
+	if err := vmctx.Storage().Get(vmctx.Storage().GetHead(), &self); err != nil {
+		return types.InvokeRet{}, err
+	}
+
+	_, ok := self.Miners[vmctx.Message().From]
+	if !ok {
+		//Fatal("update storage must only be called by a miner actor")
+		return types.InvokeRet{
+			ReturnCode: 1,
+		}, nil
+	}
+
+	self.TotalStorage = types.BigAdd(self.TotalStorage, params.Delta)
+	return types.InvokeRet{}, nil
+}
+
+func (sma StorageMarketActor) GetTotalStorage(act *types.Actor, vmctx types.VMContext, params struct{}) (types.InvokeRet, error) {
+	var self StorageMarketState
+	if err := vmctx.Storage().Get(vmctx.Storage().GetHead(), &self); err != nil {
+		return types.InvokeRet{}, err
+	}
+
+	return types.InvokeRet{
+		Result: self.TotalStorage.Bytes(),
+	}, nil
+}
+
+type PowerLookupParams struct {
+	Miner address.Address
+}
+
+func (sma StorageMarketActor) PowerLookup(act *types.Actor, vmctx types.VMContext, params *PowerLookupParams) (types.InvokeRet, error) {
+	var self StorageMarketState
+	if err := vmctx.Storage().Get(vmctx.Storage().GetHead(), &self); err != nil {
+		return types.InvokeRet{}, err
+	}
+
+	if _, ok := self.Miners[params.Miner]; !ok {
+		//Fatal("miner not registered with storage market")
+		return types.InvokeRet{
+			ReturnCode: 1,
+		}, nil
+	}
+
+	ret, code, err := vmctx.Send(params.Miner, 9999, types.NewInt(0), nil)
+	if err != nil {
+		return types.InvokeRet{}, err
+	}
+
+	if code != 0 {
+		return types.InvokeRet{
+			// TODO: error handling... these codes really don't tell us what the problem is very well
+			ReturnCode: code,
+		}, nil
+	}
+
+	return types.InvokeRet{
+		Result: ret,
+	}, nil
 }
