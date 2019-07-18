b7d7f5fb7166b7e585a04416e7dbcbccd40e673d
whyrusleeping <why@ipfs.io>
2019-07-17 23:21:50 -0700

    write a couple simple benchmarks

---
+++ chain/types/types_test.go
@@ -0,0 +1,41 @@
+package types
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/filecoin-project/go-lotus/chain/address"
+)
+
+func blsaddr(n int64) address.Address {
+	buf := make([]byte, 48)
+	r := rand.New(rand.NewSource(n))
+	r.Read(buf)
+
+	addr, err := address.NewBLSAddress(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	return addr
+}
+
+func BenchmarkSerializeMessage(b *testing.B) {
+	m := &Message{
+		To:       blsaddr(1),
+		From:     blsaddr(2),
+		Nonce:    197,
+		Method:   1231254,
+		Params:   []byte("some bytes, idk. probably at least ten of them"),
+		GasLimit: NewInt(126723),
+		GasPrice: NewInt(1776234),
+	}
+
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := m.Serialize()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
