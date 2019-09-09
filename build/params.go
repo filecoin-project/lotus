8b29c989197eea486acbf9a1eb0344f1fd30af2e
Łukasz Magiera <magik6k@gmail.com>
2019-09-09 15:55:06 +0200

    paych actor: pass verification errors

--- build/params.go
+++ build/params.go
@@ -7,6 +7,8 @@ const UnixfsLinksPerLevel = 1024
 
 const SectorSize = 1024
 
+const PaymentChannelClosingDelay = 6 * 60 * 2 // six hours
+
 const ForkLengthThreshold = 20
 
 // TODO: Move other important consts here
