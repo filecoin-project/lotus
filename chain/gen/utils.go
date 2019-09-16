0c59ebc549af346ac517af2de8ff1754462cfa32
whyrusleeping <why@ipfs.io>
2019-09-16 09:47:42 -0700

    rename 'Fil' to 'FromFil'

--- chain/gen/utils.go
+++ chain/gen/utils.go
@@ -98,7 +98,7 @@ func MakeInitialStateTree(bs bstore.Blockstore, actmap map[address.Address]types
 		return nil, xerrors.Errorf("set storage market actor: %w", err)
 	}
 
-	netAmt := types.Fil(types.NewInt(build.TotalFilecoin))
+	netAmt := types.FromFil(build.TotalFilecoin)
 	for _, amt := range actmap {
 		netAmt = types.BigSub(netAmt, amt)
 	}
