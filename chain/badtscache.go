b333247fab63b14dc0b41189a4a8a56da3678e08
whyrusleeping <why@ipfs.io>
2019-10-09 21:23:45 +0900

    switch to a bounded cache

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -3,29 +3,34 @@ package chain
 import (
 	"sync"
 
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/ipfs/go-cid"
 )
 
 type BadBlockCache struct {
 	lk        sync.Mutex
-	badBlocks map[cid.Cid]struct{}
+	badBlocks *lru.ARCCache
 }
 
 func NewBadBlockCache() *BadBlockCache {
+	cache, err := lru.NewARC(8192)
+	if err != nil {
+		panic(err)
+	}
+
 	return &BadBlockCache{
-		badBlocks: make(map[cid.Cid]struct{}),
+		badBlocks: cache,
 	}
 }
 
 func (bts *BadBlockCache) Add(c cid.Cid) {
 	bts.lk.Lock()
 	defer bts.lk.Unlock()
-	bts.badBlocks[c] = struct{}{}
+	bts.badBlocks.Add(c, nil)
 }
 
 func (bts *BadBlockCache) Has(c cid.Cid) bool {
 	bts.lk.Lock()
 	defer bts.lk.Unlock()
-	_, ok := bts.badBlocks[c]
-	return ok
+	return bts.badBlocks.Contains(c)
 }
