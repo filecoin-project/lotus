f19108454a521815b9d298e7da9ffd1e30d4ddcd
Łukasz Magiera <magik6k@gmail.com>
2019-10-15 04:11:11 +0200

    scripts: separate miner deploy from node deploy

--- scripts/deploy-miner.sh
+++ scripts/deploy-miner.sh
@@ -2,24 +2,6 @@
 
 HOST=$1
 
-# upload binaries
-# TODO: destroy
-
-ssh "$HOST" 'systemctl stop lotus-storage-miner'
-ssh "$HOST" 'systemctl stop lotus-daemon'
-
-ssh "$HOST" 'mkdir -p .lotus .lotusstorage' &
-scp "./lotus"  "$HOST:/usr/local/bin" &
-scp "./lotus-storage-miner"  "$HOST:/usr/local/bin" &
-scp -C scripts/daemon.service "${HOST}:/etc/systemd/system/lotus-daemon.service" &
-scp -C scripts/sminer.service "${HOST}:/etc/systemd/system/lotus-storage-miner.service" &
-wait
-
-ssh "$HOST" 'systemctl daemon-reload'
-ssh "$HOST" 'systemctl start lotus-daemon' &
-ssh "$HOST" 'systemctl start lotus-storage-miner' &
-wait
-
 ssh "$HOST" 'lotus wallet new bls > addr'
 ssh "$HOST" 'curl http://147.75.80.29:777/sendcoll?address=$(cat addr)' &
 ssh "$HOST" 'curl http://147.75.80.29:777/sendcoll?address=$(cat addr)' &
@@ -27,9 +9,8 @@ ssh "$HOST" 'curl http://147.75.80.29:777/send?address=$(cat addr)' &
 wait
 
 echo "SYNC WAIT"
+sleep 30
 
 ssh "$HOST" 'lotus sync wait'
 ssh "$HOST" 'lotus-storage-miner init --owner=$(cat addr)'
 ssh "$HOST" 'systemctl start lotus-storage-miner' &
-
-# setup miner actor
