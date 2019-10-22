26b473c37281573e898307f74845c26355e67008
frrist <forrest@protocol.ai>
2019-10-22 12:02:33 -0700

    lotus_test passes

--- chain/validation/lotus_test.go
+++ chain/validation/lotus_test.go
@@ -1,19 +1,56 @@
 package validation
 
 import (
+	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/chain-validation/pkg/chain"
+	"github.com/filecoin-project/chain-validation/pkg/state"
+	"github.com/filecoin-project/chain-validation/pkg/suites"
 )
 
 // A basic example validation test.
 // At present this code is verbose and demonstrates the opportunity for helper methods.
 func TestLotusExample(t *testing.T) {
-	//factories := NewFactories()
-	//drv := suites.NewStateDriver(t, factories.NewState())
+	factory := NewFactories()
+	drv := suites.NewStateDriver(t, factory.NewState())
 
-	//alice := drv.NewAccountActor(2000)
-	//bob := drv.NewAccountActor(0)
-	//miner := drv.NewAccountActor(0) // Miner owner
+	// create the init actor
+	initActorAddress, err := state.NewIDAddress(0)
+	require.NoError(t, err)
+	_, _, err = drv.State().SetActor(initActorAddress, state.InitActorCodeCid, state.AttoFIL(big.NewInt(0)))
+	require.NoError(t, err)
 
-	// TODO: write lotus-specific tests here.
+	alice := drv.NewAccountActor(2000)
+	bob := drv.NewAccountActor(0)
+	miner := drv.NewAccountActor(0) // Miner owner
+
+
+	gasPrice := big.NewInt(1)
+	gasLimit := state.GasUnit(1000)
+
+	producer := chain.NewMessageProducer(factory.NewMessageFactory(drv.State()), gasLimit, gasPrice)
+	msg, err := producer.Transfer(alice, bob, 0, 50)
+	require.NoError(t, err)
+
+	validator := chain.NewValidator(factory)
+	exeCtx := chain.NewExecutionContext(1, miner)
+
+	msgReceipt, err := validator.ApplyMessage(exeCtx, drv.State(), msg)
+	require.NoError(t, err)
+	require.NotNil(t, msgReceipt)
+
+	expectedGasUsed := 126 // NB: should be derived from the size of the message + some other lotus VM bits
+	assert.Equal(t, uint8(0), msgReceipt.ExitCode)
+	assert.Empty(t, msgReceipt.ReturnValue)
+	assert.Equal(t, state.GasUnit(expectedGasUsed), msgReceipt.GasUsed)
+
+	drv.AssertBalance(alice, uint64(1950 - expectedGasUsed))
+	drv.AssertBalance(bob, 50)
+	// This should become non-zero after gas tracking and payments are integrated.
+	drv.AssertBalance(miner, uint64(expectedGasUsed))
 
 }
