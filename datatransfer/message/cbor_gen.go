87f89ca8ff1f87a71d9e5994d37c78697456c159
shannonwells <shannonwells@users.noreply.github.com>
2019-11-01 14:01:26 -0700

    * Bring cbor-gen stuff into datatransfer package * make transferRequest private struct * add transferResponse + funcs * Rename VoucherID to VoucherType * more tests passing

--- datatransfer/message/cbor_gen.go
+++ datatransfer/message/cbor_gen.go
@@ -11,7 +11,7 @@ import (
 
 var _ = xerrors.Errorf
 
-func (t *TransferRequest) MarshalCBOR(w io.Writer) error {
+func (t *transferRequest) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		_, err := w.Write(cbg.CborNull)
 		return err
@@ -72,18 +72,18 @@ func (t *TransferRequest) MarshalCBOR(w io.Writer) error {
 		return err
 	}
 
-	// t.t.VID (string)
-	if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajTextString, uint64(len(t.VID)))); err != nil {
+	// t.t.VTyp (string)
+	if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajTextString, uint64(len(t.VTyp)))); err != nil {
 		return err
 	}
-	if _, err := w.Write([]byte(t.VID)); err != nil {
+	if _, err := w.Write([]byte(t.VTyp)); err != nil {
 		return err
 	}
 	return nil
 }
 
 
-func (t *TransferRequest) UnmarshalCBOR(r io.Reader) error {
+func (t *transferRequest) UnmarshalCBOR(r io.Reader) error {
 	br := cbg.GetPeeker(r)
 
 	maj, extra, err := cbg.CborReadHeader(br)
@@ -220,7 +220,7 @@ func (t *TransferRequest) UnmarshalCBOR(r io.Reader) error {
 
 		t.BCid = string(sval)
 	}
-	// t.t.VID (string)
+	// t.t.VTyp (string)
 
 	{
 		sval, err := cbg.ReadString(br)
@@ -228,8 +228,75 @@ func (t *TransferRequest) UnmarshalCBOR(r io.Reader) error {
 			return err
 		}
 
-		t.VID = string(sval)
+		t.VTyp = string(sval)
 	}
+	return nil
+}
+
+func (t *transferResponse) MarshalCBOR(w io.Writer) error {
+	if t == nil {
+		_, err := w.Write(cbg.CborNull)
+		return err
+	}
+	if _, err := w.Write([]byte{130,}); err != nil {
+		return err
+	}
+
+	// t.t.Acpt (bool)
+	if err := cbg.WriteBool(w, t.Acpt); err != nil {
+		return err
+	}
+
+	// t.t.XferID (uint64)
+	if _, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, t.XferID)); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (t *transferResponse) UnmarshalCBOR(r io.Reader) error {
+	br := cbg.GetPeeker(r)
+
+	maj, extra, err := cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+	if maj != cbg.MajArray {
+		return fmt.Errorf("cbor input should be of type array")
+	}
+
+	if extra != 2 {
+		return fmt.Errorf("cbor input had wrong number of fields")
+	}
+
+	// t.t.Acpt (bool)
+
+	maj, extra, err = cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+	if maj != cbg.MajOther {
+		return fmt.Errorf("booleans must be major type 7")
+	}
+	switch extra {
+	case 20:
+		t.Acpt = false
+	case 21:
+		t.Acpt = true
+	default:
+		return fmt.Errorf("booleans are either major type 7, value 20 or 21 (got %d)", extra)
+	}
+	// t.t.XferID (uint64)
+
+	maj, extra, err = cbg.CborReadHeader(br)
+	if err != nil {
+		return err
+	}
+	if maj != cbg.MajUnsignedInt {
+		return fmt.Errorf("wrong type for uint64 field")
+	}
+	t.XferID = extra
 	return nil
 }
 
--- datatransfer/message/message.go
+++ datatransfer/message/message.go
@@ -1,36 +1,35 @@
 package message
 
 import (
-	typegen "github.com/whyrusleeping/cbor-gen"
 	"io"
 
 	ggio "github.com/gogo/protobuf/io"
-	cid "github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/network"
+	cborgen "github.com/whyrusleeping/cbor-gen"
 
-	datatransfer "github.com/filecoin-project/lotus/datatransfer"
+	"github.com/filecoin-project/lotus/datatransfer"
 	pb "github.com/filecoin-project/lotus/datatransfer/message/pb"
 )
 
 // Reference file: https://github.com/ipfs/go-graphsync/blob/master/message/message.go
 // though here we have a simpler message type that serializes/deserializes to two
-// different types that share an interface
+// different types that share an interface, and we serialize to CBOR and not Protobuf.
 
 // DataTransferMessage is a message for the data transfer protocol
 // (either request or response) that can serialize to a protobuf
 type DataTransferMessage interface {
 	IsRequest() bool
 	TransferID() datatransfer.TransferID
-	typegen.CBORMarshaler
-	typegen.CBORUnmarshaler
-	Loggable
+	cborgen.CBORMarshaler
+	cborgen.CBORUnmarshaler
 }
 
 // DataTransferRequest is a response message for the data transfer protocol
 type DataTransferRequest interface {
 	DataTransferMessage
 	IsPull() bool
-	VoucherIdentifier() string
+	VoucherType() string
 	Voucher() []byte
 	BaseCid() cid.Cid
 	Selector() []byte
@@ -43,34 +42,35 @@ type DataTransferResponse interface {
 	Accepted() bool
 }
 
-// Loggable means the message supports the loggable interface
-type Loggable interface {
-	Loggable() map[string]interface{}
-}
-
 // NewRequest generates a new request for the data transfer protocol
 // TODO: Write this method, and an implementation of the data transfer request interface
 func NewRequest(id datatransfer.TransferID, isPull bool, voucherIdentifier string, voucher []byte, baseCid cid.Cid, selector []byte) DataTransferRequest {
-	return &TransferRequest{
+	return &transferRequest{
 		XferID: uint64(id),
 		Pull:   isPull,
 		Vouch:  voucher,
 		Stor:   selector,
 		BCid:   baseCid.String(),
-		VID:    voucherIdentifier,
+		VTyp:   voucherIdentifier,
 	}
 }
 
 // CancelRequest request generates a request to cancel an in progress request
 // TODO: Write this method, and an implementation of the data transfer request interface
 func CancelRequest(id datatransfer.TransferID) DataTransferRequest {
-	return nil
+	return &transferRequest{
+		XferID: uint64(id),
+		Canc:   true,
+	}
 }
 
 // NewResponse builds a new Data Transfer response
 // TODO: Write this method, and an implementation of the data transfer response interface
 func NewResponse(id datatransfer.TransferID, accepted bool) DataTransferResponse {
-	return nil
+	return &transferResponse{
+		Acpt:   accepted,
+		XferID: uint64(id),
+	}
 }
 
 // NewMessageFromProto generates a new DataTransferMessage
@@ -90,11 +90,10 @@ func FromNet(r io.Reader) (DataTransferMessage, error) {
 
 // FromPBReader can deserialize a protobuf message into a GraphySyncMessage.
 func FromPBReader(pbr ggio.Reader) (DataTransferMessage, error) {
-	pb := new(pb.Message)
 	// TODO: Needs generate actual PB Message struct to fulfill interface
 	//if err := pbr.ReadMsg(pb); err != nil {
 	//	return nil, err
 	//}
 
-	return NewMessageFromProto(*pb)
+	return nil, nil
 }
