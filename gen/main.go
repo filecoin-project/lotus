52517c0487728939a9492bf78379756523fd0027
shannonwells <shannonwells@users.noreply.github.com>
2019-10-31 17:09:40 -0700

    WIP using CBOR encoding for dataxfermsg

--- gen/main.go
+++ gen/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/filecoin-project/lotus/paych"
 	"github.com/filecoin-project/lotus/retrieval"
 	"github.com/filecoin-project/lotus/storage"
+	"github.com/filecoin-project/lotus/datatransfer/message"
 )
 
 func main() {
@@ -152,6 +153,14 @@ func main() {
 	err = gen.WriteTupleEncodersToFile("./storage/cbor_gen.go", "storage",
 		storage.SealTicket{},
 		storage.SectorInfo{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = gen.WriteTupleEncodersToFile("./datatransfer/message/cbor_gen.go", "message",
+		message.TransferRequest{},
 	)
 	if err != nil {
 		fmt.Println(err)
