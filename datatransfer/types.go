9ffd730ae6436456c37e4497c8780a47cbddd64c
shannonwells <shannonwells@users.noreply.github.com>
2019-11-05 10:23:57 -0800

    Some cleanup for PR

--- datatransfer/types.go
+++ datatransfer/types.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 
 	"github.com/ipfs/go-cid"
-	ipld "github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
@@ -74,8 +74,8 @@ func NewChannel(transferID TransferID, baseCid cid.Cid,
 	sender peer.ID,
 	recipient peer.ID,
 	totalSize uint64) Channel {
-		return Channel{transferID, baseCid, selector, voucher, sender, recipient, totalSize}
-	}
+	return Channel{transferID, baseCid, selector, voucher, sender, recipient, totalSize}
+}
 
 // TransferID returns the transfer id for this channel
 func (c Channel) TransferID() TransferID { return c.transferID }
