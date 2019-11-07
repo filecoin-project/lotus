d011c1d7eadacec6f7249f35334607dbfda0d47c
ergastic <>
2019-11-06 16:07:15 -0800

    Added implementation for StateKey (renamed from StateID.) Added implementations for MostRecentState (renamed from MostRecentStateId,) GetMinerState, GetRandomness and GetProvingPeriod.

--- smm/node.go
+++ smm/node.go
@@ -2,19 +2,15 @@ package smm
 
 import (
     "context"
-    "github.com/filecoin-project/lotus/chain/address"
-    "github.com/filecoin-project/lotus/chain/types"
     "github.com/ipfs/go-cid"
 )
-
-type SubID uint64
 
 type StorageMiningEvents interface {
     // Called when the chain state changes.
     // Epoch may change by >1.
     // In case of a re-org, state will always change back to the fork
     // point before advancing down the new chain.
-    OnChainStateChanged(Epoch, StateID)
+    OnChainStateChanged(Epoch, StateKey)
 }
 
 // add the Node interface
@@ -26,19 +22,19 @@ type Node interface {
     SubscribeMiner(ctx context.Context, cb StorageMiningEvents) error
 
     // Fetches key for the most recent state known by the node.
-    MostRecentStateId(ctx context.Context) (StateID, Epoch)
+    MostRecentState(ctx context.Context) (*StateKey, Epoch, error)
 
     // Cancels a subscription
     UnsubscribeMiner(ctx context.Context, cb StorageMiningEvents) error
 
     // Gets miner-related on-chain state.
-    GetMinerState(ctx context.Context, state StateID) MinerChainState
+    GetMinerState(ctx context.Context, state *StateKey) (MinerChainState, error)
 
     // Submits a self-deal to the chain.
     SubmitSelfDeal(ctx context.Context, size uint64) error
 
     // Retrieves a ticket used in sealing and proving operations.
-    GetRandomness(ctx context.Context, state StateID, e Epoch, offset uint) []byte
+    GetRandomness(ctx context.Context, state *StateKey, e Epoch, offset uint) ([]byte, error)
 
     // Submits replicated sector information and requests a seal seed
     // be generated on-chain.
@@ -46,27 +42,27 @@ type Node interface {
     // chain and then await some delay before the seed is provided.
     // The parameters are a subset of OnChainSealVerifyInfo.
     // The miner chooses sector ID.
-    SubmitSectorPreCommitment(ctx context.Context, miner address.Address, id SectorID, commR cid.Cid, deals []cid.Cid)
+    SubmitSectorPreCommitment(ctx context.Context, miner Address, id SectorID, commR cid.Cid, deals []cid.Cid)
 
     // Reads a seal seed previously requested with
     // SubmitSectorPreCommitment.
     // Returns empty if the request and delay have not yet elapsed.
-    GetSealSeed(ctx context.Context, miner address.Address, state StateID, id SectorID) SealSeed
+    GetSealSeed(ctx context.Context, miner Address, state StateKey, id SectorID) SealSeed
 
     // Submits final commitment of a sector, with a proof including the
     // seal seed.
-    SubmitSectorCommitment(ctx context.Context, miner address.Address, id SectorID, proof Proof)
+    SubmitSectorCommitment(ctx context.Context, miner Address, id SectorID, proof Proof)
 
     // Returns the current proving period and, if the miner has
     // been challenged, the challenge seed and period.
-    GetProvingPeriod(ctx context.Context, miner address.Address, state StateID) ProvingPeriod
+    GetProvingPeriod(ctx context.Context, state *StateKey) (ProvingPeriod, error)
 
     // Submits a PoSt proof to the chain.
-    SubmitPoSt(ctx context.Context, miner address.Address, proof Proof)
+    SubmitPoSt(ctx context.Context, miner Address, proof Proof)
 
     // Submits declaration of IDs of faulty sectors to the chain.
-    SubmitDeclaredFaults(ctx context.Context, miner address.Address, faults types.BitField)
+    //SubmitDeclaredFaults(ctx context.Context, miner Address, faults types.BitField)
 
     // Submits declaration of IDs of recovered sectors to the chain.
-    SubmitDeclaredRecoveries(ctx context.Context, miner address.Address, recovered types.BitField)
+    //SubmitDeclaredRecoveries(ctx context.Context, miner Address, recovered types.BitField)
 }
