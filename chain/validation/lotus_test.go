6545c20666035137c6b4e09ae77007fb6d7618d4
frrist <forrest@protocol.ai>
2019-11-12 11:23:59 -0800

    fixup! first pass at integrating chain-validation

--- chain/validation/lotus_test.go
+++ chain/validation/lotus_test.go
@@ -11,29 +11,10 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/chain-validation/pkg/state"
-	"github.com/filecoin-project/chain-validation/pkg/suites"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/address"
 )
-
-func TestStorageMinerValidation(t *testing.T) {
-	factory := NewFactories()
-	suites.CreateStorageMinerAndUpdatePeerIDTest(t, factory)
-
-}
-
-func TestValueTransfer(t *testing.T) {
-	factory := NewFactories()
-	suites.AccountValueTransferSuccess(t, factory, 126)
-	suites.AccountValueTransferZeroFunds(t, factory, 112)
-	suites.AccountValueTransferOverBalanceNonZero(t, factory, 0)
-	suites.AccountValueTransferOverBalanceZero(t, factory, 0)
-	suites.AccountValueTransferToSelf(t, factory, 0)
-	suites.AccountValueTransferFromKnownToUnknownAccount(t, factory, 0)
-	suites.AccountValueTransferFromUnknownToKnownAccount(t, factory, 0)
-	suites.AccountValueTransferFromUnknownToUnknownAccount(t, factory, 0)
-}
 
 //
 // some helpers tests to verify chain-validation can encode parameters to the CBOR lotus uses
