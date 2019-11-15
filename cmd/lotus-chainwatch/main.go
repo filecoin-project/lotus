2488d7dc727c15b95a71e142e142277d176d3f01
Łukasz Magiera <magik6k@gmail.com>
2019-11-15 17:38:56 +0100

    Begin implementing ChainWatch

---
+++ cmd/lotus-chainwatch/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"github.com/filecoin-project/lotus/build"
+	lcli "github.com/filecoin-project/lotus/cli"
+	logging "github.com/ipfs/go-log"
+	"gopkg.in/urfave/cli.v2"
+	"net/http"
+	"os"
+)
+
+var log = logging.Logger("chainwatch")
+
+func main() {
+	logging.SetLogLevel("*", "INFO")
+
+	log.Info("Starting chainwatch")
+
+	local := []*cli.Command{
+		runCmd,
+	}
+
+	app := &cli.App{
+		Name:    "lotus-chainwatch",
+		Usage:   "Devnet token distribution utility",
+		Version: build.Version,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "repo",
+				EnvVars: []string{"LOTUS_PATH"},
+				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
+			},
+		},
+
+		Commands: local,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Warn(err)
+		return
+	}
+}
+
+var runCmd = &cli.Command{
+	Name:  "run",
+	Usage: "Start lotus chainwatch",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "front",
+			Value: "127.0.0.1:8418",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+		ctx := lcli.ReqContext(cctx)
+
+		v, err := api.Version(ctx)
+		if err != nil {
+			return err
+		}
+
+		log.Info("Remote version: %s", v.Version)
+
+		//http.Handle("/", http.FileServer(rice.MustFindBox("site").HTTPBox()))
+
+		fmt.Printf("Open http://%s\n", cctx.String("front"))
+
+		st, err := openStorage()
+		if err != nil {
+			return err
+		}
+		defer st.close()
+
+		runSyncer(ctx, api, st)
+
+		go func() {
+			<-ctx.Done()
+			os.Exit(0)
+		}()
+
+		return http.ListenAndServe(cctx.String("front"), nil)
+	},
+}
