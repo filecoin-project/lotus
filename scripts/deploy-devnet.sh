7d746faaad61c2c883037a60aacce58a8c9fae45
Łukasz Magiera <magik6k@gmail.com>
2019-12-05 15:05:17 +0100

    Devnet 10

--- scripts/deploy-devnet.sh
+++ scripts/deploy-devnet.sh
@@ -91,32 +91,7 @@ make
 
 for host in "${BOOTSTRAPPERS[@]}"
 do
-  log "> Deploying bootstrap node $host"
-  log "Stopping lotus daemon"
-
-	ssh "$host" 'systemctl stop lotus-daemon' &
-  ssh "$host" 'systemctl stop lotus-storage-miner' &
-
-  wait
-
-  ssh "$host" 'rm -rf .lotus' &
-  ssh "$host" 'rm -rf .lotusstorage' &
-
-  scp -C lotus "${host}":/usr/local/bin/lotus &
-  scp -C lotus-storage-miner "${host}":/usr/local/bin/lotus-storage-miner &
-
-  wait
-
-  log 'Initializing repo'
-
-  ssh "$host" 'systemctl start lotus-daemon'
-  scp scripts/bootstrap.toml "${host}:.lotus/config.toml"
-  ssh "$host" "echo -e '[Metrics]\nNickname=\"Boot-$host\"' >> .lotus/config.toml"
-  ssh "$host" 'systemctl restart lotus-daemon'
-
-  log 'Extracting addr info'
-
-  ssh "$host" 'lotus net listen' | grep -v '/10' | grep -v '/127' >> build/bootstrap/bootstrappers.pi
+  ./scripts/deploy-bootstrapper.sh $host
 done
 
 log 'Updating genesis node with bootstrapable binaries'
