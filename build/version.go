e6dd4711031708d77c453d3235a47badffc6fe2f
Łukasz Magiera <magik6k@gmail.com>
2019-12-11 01:22:58 +0100

    Version fixes

--- build/version.go
+++ build/version.go
@@ -1,7 +1,7 @@
 package build
 
 // Version is the local build version, set by build system
-const Version = "0.11.0"
+const Version = "0.1.0"
 
 // APIVersion is a hex semver version of the rpc api exposed
 //
@@ -12,15 +12,19 @@ const Version = "0.11.0"
 //                   R R  H
 //                   |\vv/|
 //                   vv  vv
-const APIVersion = 0x000b01
+const APIVersion = 0x000100
 
 const (
 	MajorMask = 0xff0000
 	MinorMask = 0xffff00
 	PatchMask = 0xffffff
-)
+
+	MajorOnlyMask = 0xff0000
+	MinorOnlyMask = 0x00ff00
+	PatchOnlyMask = 0x0000ff
+c)
 
 // VersionInts returns (major, minor, patch) versions
 func VersionInts(version uint32) (uint32, uint32, uint32) {
-	return (version & MajorMask) >> 16, (version & MinorMask) >> 8, version & PatchMask
+	return (version & MajorOnlyMask) >> 16, (version & MinorOnlyMask) >> 8, version & PatchOnlyMask
 }
