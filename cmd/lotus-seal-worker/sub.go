bc34bdde9d04a7bd56993050e16cfa51e143a3c5
marco-storswift <marco@storswift.com>
2019-12-21 15:20:57 +0800

    change to one process

--- cmd/lotus-seal-worker/sub.go
+++ cmd/lotus-seal-worker/sub.go
@@ -179,9 +179,10 @@ func (w *worker) processTask(ctx context.Context, task sectorbuilder.WorkerTask)
 		os.RemoveAll(bakname)
 
 	case sectorbuilder.WorkerCommit:
-		//proof, err := w.sb.SealCommit(task.SectorID, task.SealTicket, task.SealSeed, task.Pieces, task.Rspco, constRemoteID)
+		//One process
+		proof, err := w.sb.SealCommit(task.SectorID, task.SealTicket, task.SealSeed, task.Pieces, task.Rspco, constRemoteID)
 		//Two process
-		proof, err := w.sb.SealCommitLocal(task.SectorID, task.SealTicket, task.SealSeed, task.Pieces, task.Rspco)
+		//proof, err := w.sb.SealCommitLocal(task.SectorID, task.SealTicket, task.SealSeed, task.Pieces, task.Rspco)
 		//if err := w.push("cache", task.SectorID); err != nil {
 		//	return errRes(xerrors.Errorf("pushing precommited data: %w", err))
 		//}
