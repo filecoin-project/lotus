84de47377c59b4aaeca451b358be165fbdef9ea3
Jakub Sztandera <kubuxu@protocol.ai>
2020-01-16 23:21:16 +0100

    Intoroduce AMT v2 and amtFsck
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

--- chain/stmgr/forks.go
+++ chain/stmgr/forks.go
@@ -3,7 +3,12 @@ package stmgr
 import (
 	"context"
 
+	"github.com/filecoin-project/lotus/chain/types"
+
+	amt "github.com/filecoin-project/go-amt-ipld/v2"
 	"github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
 )
 
 func (sm *StateManager) handleStateForks(ctx context.Context, pstate cid.Cid, height, parentH uint64) (_ cid.Cid, err error) {
@@ -13,4 +18,32 @@ func (sm *StateManager) handleStateForks(ctx context.Context, pstate cid.Cid, he
 	}
 
 	return pstate, nil
+}
+
+func amtFsck(s types.Storage, ss cid.Cid) (cid.Cid, error) {
+	a, err := amt.LoadAMT(types.WrapStorage(s), ss)
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("could not load AMT: %w", a)
+	}
+
+	b := amt.NewAMT(types.WrapStorage(s))
+
+	err = a.ForEach(func(id uint64, data *cbg.Deferred) error {
+		err := b.Set(id, data)
+		if err != nil {
+			return xerrors.Errorf("could not copy at idx (%d): %w", id, err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("could not copy: %w", err)
+	}
+
+	nss, err := b.Flush()
+	if err != nil {
+		return cid.Undef, xerrors.Errorf("could not flush: %w", err)
+	}
+
+	return nss, nil
 }
