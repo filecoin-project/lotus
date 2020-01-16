cc424d64fe4dfbc2d0d1edf98468d37bfb787644
Łukasz Magiera <magik6k@gmail.com>
2020-01-16 03:53:59 +0100

    sealing: Handle events based on current state

--- storage/sealing/fsm_events.go
+++ storage/sealing/fsm_events.go
@@ -1,14 +1,44 @@
 package sealing
 
 import (
-	"github.com/ipfs/go-cid"
-
 	"github.com/filecoin-project/lotus/api"
+	"github.com/ipfs/go-cid"
 )
 
 type mutator interface {
 	apply(state *SectorInfo)
 }
+
+// globalMutator is an event which can apply in every state
+type globalMutator interface {
+	// applyGlobal applies the event to the state. If if returns true,
+	//  event processing should be interrupted
+	applyGlobal(state *SectorInfo) bool
+}
+
+// Global events
+
+type SectorRestart struct{}
+func (evt SectorRestart) applyGlobal(*SectorInfo) bool { return false }
+
+type SectorFatalError struct{ error }
+func (evt SectorFatalError) applyGlobal(state *SectorInfo) bool {
+	log.Errorf("Fatal error on sector %d: %+v", state.SectorID, evt.error)
+	// TODO: Do we want to mark the state as unrecoverable?
+	//  I feel like this should be a softer error, where the user would
+	//  be able to send a retry event of some kind
+	return true
+}
+
+type SectorForceState struct {
+	state api.SectorState
+}
+func (evt SectorForceState) applyGlobal(state *SectorInfo) bool {
+	state.State = evt.state
+	return true
+}
+
+// Normal path
 
 type SectorStart struct {
 	id     uint64
@@ -18,10 +48,6 @@ func (evt SectorStart) apply(state *SectorInfo) {
 	state.SectorID = evt.id
 	state.Pieces = evt.pieces
 }
-
-type SectorRestart struct{}
-
-type SectorFatalError struct{ error }
 
 type SectorPacked struct{ pieces []Piece }
 func (evt SectorPacked) apply(state *SectorInfo) {
@@ -40,8 +66,10 @@ func (evt SectorSealed) apply(state *SectorInfo) {
 }
 
 type SectorSealFailed struct{ error }
+func (evt SectorSealFailed) apply(*SectorInfo) {}
 
 type SectorPreCommitFailed struct{ error }
+func (evt SectorPreCommitFailed) apply(*SectorInfo) {}
 
 type SectorPreCommitted struct {
 	message cid.Cid
@@ -71,9 +99,12 @@ func (evt SectorCommitted) apply(state *SectorInfo) {
 type SectorProving struct{}
 func (evt SectorProving) apply(*SectorInfo) {}
 
-type SectorFaultReported struct{ reportMsg cid.Cid }
-type SectorFaultedFinal struct{}
+type SectorFaulty struct{}
+func (evt SectorFaulty) apply(state *SectorInfo) {}
 
-type SectorForceState struct {
-	state api.SectorState
+type SectorFaultReported struct{ reportMsg cid.Cid }
+func (evt SectorFaultReported) apply(state *SectorInfo) {
+	state.FaultReportMsg = &evt.reportMsg
 }
+
+type SectorFaultedFinal struct{}
