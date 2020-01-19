3886e050b0cfff6a0ec3b54491754a9ed376a5d6
Łukasz Magiera <magik6k@gmail.com>
2020-01-19 21:18:35 +0100

    lotuslog: don't override log levels

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,12 +1,18 @@
 package lotuslog
 
-import logging "github.com/ipfs/go-log/v2"
+import (
+	"os"
+
+	logging "github.com/ipfs/go-log/v2"
+)
 
 func SetupLogLevels() {
-	logging.SetLogLevel("*", "INFO")
-	logging.SetLogLevel("dht", "ERROR")
-	logging.SetLogLevel("swarm2", "WARN")
-	logging.SetLogLevel("bitswap", "WARN")
-	logging.SetLogLevel("pubsub", "WARN")
-	logging.SetLogLevel("connmgr", "WARN")
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+		logging.SetLogLevel("*", "INFO")
+		logging.SetLogLevel("dht", "ERROR")
+		logging.SetLogLevel("swarm2", "WARN")
+		logging.SetLogLevel("bitswap", "WARN")
+		logging.SetLogLevel("pubsub", "WARN")
+		logging.SetLogLevel("connmgr", "WARN")
+	}
 }
