0b4671e47ed691ff4f339774627b435746726e96
Lucas Molas <schomatis@gmail.com>
2020-02-06 14:55:10 -0300

    fuzz: add address and big int encoders

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -2,9 +2,13 @@
 
 package types
 
-import "bytes"
+import (
+	"bytes"
+	"github.com/filecoin-project/go-address"
+)
 
-func FuzzMessage(data []byte) int {
+// FIXME: Separate this into `UnmarshalCBOR` calls of the underlying elements first.
+func FuzzMessageDecoder(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
@@ -28,3 +32,42 @@ func FuzzMessage(data []byte) int {
 	}
 	return 1
 }
+
+func FuzzAddressEncoder(data []byte) int {
+	ads, err := address.NewFromBytes(data)
+	if err != nil {
+		return -1
+	}
+	// FIXME: The fuzzer should produce as many valid addresses as possible, `newAddress`
+	// should be partially expanded here. At the moment we signal invalid ones with `-1`.
+
+	if err := ads.MarshalCBOR(new(bytes.Buffer)); err != nil {
+		panic(err)
+	}
+
+	return 1
+}
+
+func FuzzBigIntEncoder(data []byte) int {
+	bi, err := fromCborBytes(data)
+	if err != nil {
+		return -1
+	}
+
+	buf := new(bytes.Buffer)
+	if err := bi.MarshalCBOR(buf); err != nil {
+		panic(err)
+	}
+
+	//if !bytes.Equal(data, buf.Bytes()) {
+	//	panic("reencoding not equal")
+	//}
+	// FIXME: We can't check this because we're not interpreting the input `data` as CBOR
+	// (to avoid using the same `UnmarshalCBOR` we're trying to fuzz), `fromCborBytes` is
+	// a bit misleading, here `data` is just the payload.
+
+	return 1
+}
+
+// FIXME: Encapsulate params encoding logic from `Message.MarshalCBOR` and fuzz it in a
+// separate function.
