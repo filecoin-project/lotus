11ae8df2418434bd6bead7a30ee099468468c4c6
Łukasz Magiera <magik6k@gmail.com>
2020-02-12 03:12:39 +0100

    actors: Remove cborgen for old types

--- gen/main.go
+++ gen/main.go
@@ -7,7 +7,6 @@ import (
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/blocksync"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/statemachine"
@@ -62,48 +61,6 @@ func main() {
 		blocksync.BlockSyncRequest{},
 		blocksync.BlockSyncResponse{},
 		blocksync.BSTipSet{},
-	)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	err = gen.WriteTupleEncodersToFile("./chain/actors/cbor_gen.go", "actors",
-		actors.InitActorState{},
-		actors.ExecParams{},
-		actors.AccountActorState{},
-		actors.StorageMinerActorState{},
-		actors.StorageMinerConstructorParams{},
-		actors.SectorPreCommitInfo{},
-		actors.PreCommittedSector{},
-		actors.MinerInfo{},
-		actors.SubmitFallbackPoStParams{},
-		actors.PaymentVerifyParams{},
-		actors.UpdatePeerIDParams{},
-		actors.DeclareFaultsParams{},
-		actors.PaymentChannelActorState{},
-		actors.PCAConstructorParams{},
-		actors.LaneState{},
-		actors.PCAUpdateChannelStateParams{},
-		actors.PaymentInfo{},
-		actors.StoragePowerState{},
-		actors.CreateStorageMinerParams{},
-		actors.IsValidMinerParam{},
-		actors.PowerLookupParams{},
-		actors.UpdateStorageParams{},
-		actors.ArbitrateConsensusFaultParams{},
-		actors.PledgeCollateralParams{},
-		actors.MinerSlashConsensusFault{},
-		actors.StorageMarketState{},
-		actors.WithdrawBalanceParams{},
-		actors.StorageDealProposal{},
-		actors.PublishStorageDealsParams{},
-		actors.PublishStorageDealResponse{},
-		actors.ActivateStorageDealsParams{},
-		actors.ComputeDataCommitmentParams{},
-		actors.SectorProveCommitInfo{},
-		actors.CheckMinerParams{},
-		actors.CronActorState{},
 	)
 	if err != nil {
 		fmt.Println(err)
