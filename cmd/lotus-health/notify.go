14580b61446876cc324a7c0d22e6b624e3cfa2e9
Jakub Sztandera <kubuxu@protocol.ai>
2020-02-12 20:44:35 +0100

    Import go-systemd properly
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/coreos/go-systemd/dbus"
+	"github.com/coreos/go-systemd/v22/dbus"
 )
 
 func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
