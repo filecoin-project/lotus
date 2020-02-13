5ff013fceb95b917b427baf0c25247aef3d4d141
Łukasz Magiera <magik6k@gmail.com>
2020-02-13 01:28:23 +0100

    Rename paych -> paychmgr to fix cbor-gen

--- chain/types/ask.go
+++ chain/types/ask.go
@@ -1,25 +1,9 @@
 package types
 
 import (
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
-func init() {
-	cbor.RegisterCborType(SignedStorageAsk{})
-	cbor.RegisterCborType(StorageAsk{})
-}
 
 type SignedStorageAsk = storagemarket.SignedStorageAsk
 
-type StorageAsk struct {
-	// Price per GiB / Epoch
-	Price BigInt
-
-	MinPieceSize uint64
-	Miner        address.Address
-	Timestamp    uint64
-	Expiry       uint64
-	SeqNo        uint64
-}
+type StorageAsk = storagemarket.StorageAsk
