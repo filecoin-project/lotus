852e888232fb7bc4bef047151730649d6a0638d3
Łukasz Magiera <magik6k@gmail.com>
2020-02-21 19:00:10 +0100

    cmd/lotus: Fix build

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -15,15 +15,13 @@ import (
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
-	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/specs-actors/actors/runtime"
 
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -31,28 +29,11 @@ import (
 
 var glog = logging.Logger("genesis")
 
-func MakeGenesisMem(out io.Writer, gmc *genesis2.GenMinerCfg) func(bs dtypes.ChainBlockstore, w *wallet.Wallet, syscalls *types.VMSyscalls) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, w *wallet.Wallet, syscalls *types.VMSyscalls) modules.Genesis {
+func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls runtime.Syscalls) modules.Genesis {
+	return func(bs dtypes.ChainBlockstore, syscalls runtime.Syscalls) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
-			defk, err := w.GenerateKey(types.KTBLS)
-			if err != nil {
-				return nil, err
-			}
-
-			alloc := map[address.Address]types.BigInt{
-				defk: types.FromFil(1000),
-			}
-
-			for _, genm := range gmc.PreSeals {
-				waddr, err := w.Import(&genm.Key)
-				if err != nil {
-					return nil, err
-				}
-				alloc[waddr] = types.FromFil(10000)
-			}
-
-			b, err := genesis2.MakeGenesisBlock(bs, syscalls, alloc, gmc, 100000)
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), bs, syscalls, template)
 			if err != nil {
 				return nil, err
 			}
@@ -69,74 +50,35 @@ func MakeGenesisMem(out io.Writer, gmc *genesis2.GenMinerCfg) func(bs dtypes.Cha
 	}
 }
 
-func MakeGenesis(outFile, presealInfo, timestamp string) func(bs dtypes.ChainBlockstore, w *wallet.Wallet, syscalls *types.VMSyscalls) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, w *wallet.Wallet, syscalls *types.VMSyscalls) modules.Genesis {
+func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls runtime.Syscalls) modules.Genesis {
+	return func(bs dtypes.ChainBlockstore, syscalls runtime.Syscalls) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
-			presealInfo, err := homedir.Expand(presealInfo)
+			genesisTemplate, err := homedir.Expand(genesisTemplate)
 			if err != nil {
 				return nil, err
 			}
 
-			fdata, err := ioutil.ReadFile(presealInfo)
+			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
 			}
 
-			var preseals map[string]genesis.Miner
-			if err := json.Unmarshal(fdata, &preseals); err != nil {
+			var template genesis.Template
+			if err := json.Unmarshal(fdata, template); err != nil {
 				return nil, err
 			}
 
-			var fakePeerIDs []peer.ID
-			minerAddresses := make([]address.Address, 0, len(preseals))
-			for s := range preseals {
-				a, err := address.NewFromString(s)
-				if err != nil {
-					return nil, err
-				}
-				if a.Protocol() != address.ID {
-					return nil, xerrors.New("expected ID address")
-				}
-				minerAddresses = append(minerAddresses, a)
-				fakePeerIDs = append(fakePeerIDs, peer.ID("peer"+a.String()))
+			if template.Timestamp == 0 {
+				template.Timestamp = uint64(time.Now().Unix())
 			}
 
-			gmc := &genesis2.GenMinerCfg{
-				PeerIDs:    fakePeerIDs,
-				PreSeals:   preseals,
-				MinerAddrs: minerAddresses,
-			}
-
-			addrs := map[address.Address]types.BigInt{}
-
-			for _, miner := range preseals {
-				if _, err := w.Import(&miner.Key); err != nil {
-					return nil, xerrors.Errorf("importing miner key: %w", err)
-				}
-
-				_ = w.SetDefault(miner.Worker)
-
-				addrs[miner.Worker] = types.FromFil(100000)
-			}
-
-			ts := uint64(time.Now().Unix())
-			if timestamp != "" {
-				t, err := time.Parse(time.RFC3339, timestamp)
-				if err != nil {
-					return nil, xerrors.Errorf("parsing input genesis timestamp: %w", err)
-				}
-
-				glog.Infof("will use %s as the genesis timestamp", t)
-				ts = uint64(t.Unix())
-			}
-
-			b, err := genesis2.MakeGenesisBlock(bs, syscalls, addrs, gmc, ts)
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), bs, syscalls, template)
 			if err != nil {
 				return nil, err
 			}
 
-			fmt.Println("GENESIS MINER ADDRESS: ", gmc.MinerAddrs[0].String())
+			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
