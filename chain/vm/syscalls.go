5b657a2905a862d484378a65e7a26a68220280c9
whyrusleeping <why@ipfs.io>
2020-02-28 12:52:14 -0800

    pass on proper sector types

--- chain/vm/syscalls.go
+++ chain/vm/syscalls.go
@@ -105,9 +105,6 @@ func (ss *syscallShim) VerifySeal(info abi.SealVerifyInfo) error {
 	//_, span := trace.StartSpan(ctx, "ValidatePoRep")
 	//defer span.End()
 
-	commD := cidToCommD(info.UnsealedCID)
-	commR := cidToCommR(info.OnChain.SealedCID)
-
 	miner, err := address.NewIDAddress(uint64(info.Miner))
 	if err != nil {
 		return xerrors.Errorf("weirdly failed to construct address: %w", err)
@@ -117,7 +114,7 @@ func (ss *syscallShim) VerifySeal(info abi.SealVerifyInfo) error {
 	proof := []byte(info.OnChain.Proof)
 	seed := []byte(info.InteractiveRandomness)
 
-	log.Infof("Verif r:%x; d:%x; m:%s; t:%x; s:%x; N:%d; p:%x", commR, commD, miner, ticket, seed, info.SectorID.Number, proof)
+	log.Infof("Verif r:%x; d:%x; m:%s; t:%x; s:%x; N:%d; p:%x", info.OnChain.SealedCID, info.UnsealedCID, miner, ticket, seed, info.SectorID.Number, proof)
 
 	//func(ctx context.Context, maddr address.Address, ssize abi.SectorSize, commD, commR, ticket, proof, seed []byte, sectorID abi.SectorNumber)
 	ok, err := ss.verifier.VerifySeal(info)
