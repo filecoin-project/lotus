eef48c4be357c2f888f7c627eeb123666de6cde8
hannahhoward <hannah@hannahhoward.net>
2020-03-03 18:31:35 -0800

    feat(client): support car to filestore
    Store filestore reference for CAR files

--- node/impl/client/client.go
+++ node/impl/client/client.go
@@ -201,8 +201,23 @@ func (a *API) ClientImport(ctx context.Context, ref api.FileRef) (cid.Cid, error
 		return cid.Undef, err
 	}
 
+	stat, err := f.Stat()
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	file, err := files.NewReaderPathFile(ref.Path, f, stat)
+	if err != nil {
+		return cid.Undef, err
+	}
 	if ref.IsCAR {
-		result, err := car.LoadCar(a.Blockstore, f)
+		var store car.Store
+		if a.Filestore == nil {
+			store = a.Blockstore
+		} else {
+			store = (*filestore.Filestore)(a.Filestore)
+		}
+		result, err := car.LoadCar(store, file)
 		if err != nil {
 			return cid.Undef, err
 		}
@@ -212,16 +227,6 @@ func (a *API) ClientImport(ctx context.Context, ref api.FileRef) (cid.Cid, error
 		}
 
 		return result.Roots[0], nil
-	}
-
-	stat, err := f.Stat()
-	if err != nil {
-		return cid.Undef, err
-	}
-
-	file, err := files.NewReaderPathFile(ref.Path, f, stat)
-	if err != nil {
-		return cid.Undef, err
 	}
 
 	bufferedDS := ipld.NewBufferedDAG(ctx, a.LocalDAG)
@@ -286,20 +291,37 @@ func (a *API) ClientListImports(ctx context.Context) ([]api.Import, error) {
 	// TODO: make this less very bad by tracking root cids instead of using ListAll
 
 	out := make([]api.Import, 0)
+	lowest := make([]uint64, 0)
 	for {
 		r := next()
 		if r == nil {
 			return out, nil
 		}
-		if r.Offset != 0 {
-			continue
+		matched := false
+		for i := range out {
+			if out[i].FilePath == r.FilePath {
+				matched = true
+				if lowest[i] > r.Offset {
+					lowest[i] = r.Offset
+					out[i] = api.Import{
+						Status:   r.Status,
+						Key:      r.Key,
+						FilePath: r.FilePath,
+						Size:     r.Size,
+					}
+				}
+				break
+			}
 		}
-		out = append(out, api.Import{
-			Status:   r.Status,
-			Key:      r.Key,
-			FilePath: r.FilePath,
-			Size:     r.Size,
-		})
+		if !matched {
+			out = append(out, api.Import{
+				Status:   r.Status,
+				Key:      r.Key,
+				FilePath: r.FilePath,
+				Size:     r.Size,
+			})
+			lowest = append(lowest, r.Offset)
+		}
 	}
 }
 
