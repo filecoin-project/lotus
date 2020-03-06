8a7f249aaa164c20058d232f45cea714f596e48c
whyrusleeping <why@ipfs.io>
2020-03-05 16:20:57 -0800

    update to latest specs-actors

--- chain/vm/syscalls.go
+++ chain/vm/syscalls.go
@@ -71,7 +71,7 @@ func (ss *syscallShim) HashBlake2b(data []byte) [32]byte {
 	panic("NYI")
 }
 
-func (ss *syscallShim) VerifyConsensusFault(a, b []byte) error {
+func (ss *syscallShim) VerifyConsensusFault(a, b, extra []byte) (*runtime.ConsensusFault, error) {
 	panic("NYI")
 }
 
