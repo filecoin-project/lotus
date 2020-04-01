f40d39a1e0293790a8909f087a96490e1c478a23
Jeromy <jeromyj@gmail.com>
2020-04-01 11:35:09 -0700

    properly handle partial sync responses

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -52,7 +52,7 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
 	if err != nil {
-		log.Warnf("unable to set daedline: %+v", err)
+		log.Warnf("unable to set deadline: %+v", err)
 	}
 
 	n, err := crt.rd.Read(buf)
