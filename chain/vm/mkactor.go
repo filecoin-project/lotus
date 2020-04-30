3f02fe4cbb0ccd89d638348d3bec80061244274a
Aayush Rajasekaran <arajasek94@gmail.com>
2020-04-29 23:21:19 -0400

    Only charge for actor creation after making a meaningful state change

--- chain/vm/mkactor.go
+++ chain/vm/mkactor.go
@@ -2,7 +2,6 @@ package vm
 
 import (
 	"context"
-
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/specs-actors/actors/abi/big"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
@@ -35,10 +34,6 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, aer
 		return nil, aerrors.Escalate(err, "registering actor address")
 	}
 
-	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
-		return nil, err
-	}
-
 	act, aerr := makeActor(addr)
 	if aerr != nil {
 		return nil, aerr
@@ -54,6 +49,10 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, aer
 		return nil, aerrors.Absorb(err, exitcode.SysErrInvalidParameters, "registering actor address")
 	}
 	// call constructor on account
+
+	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
+		return nil, err
+	}
 
 	_, aerr = rt.internalSend(builtin.SystemActorAddr, addrID, builtin.MethodsAccount.Constructor, big.Zero(), p)
 	if aerr != nil {
