a90281157cfbb1c27215afe9eb66a0368a2987bc
Jeromy <jeromyj@gmail.com>
2020-05-06 22:05:43 -0700

    set minimum miner size to 2g

--- build/params_testnet.go
+++ build/params_testnet.go
@@ -5,7 +5,13 @@ package build
 
 import (
 	"github.com/filecoin-project/specs-actors/actors/abi"
+	"github.com/filecoin-project/specs-actors/actors/abi/big"
+	"github.com/filecoin-project/specs-actors/actors/builtin/power"
 )
+
+func init() {
+	power.ConsensusMinerMinPower = big.NewInt(2 << 30)
+}
 
 var SectorSizes = []abi.SectorSize{
 	512 << 20,
