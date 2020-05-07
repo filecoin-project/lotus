a31bede58953914fcd23993bf13c98e9d5f68363
Travis Person <travisperson@users.noreply.github.com>
2020-05-07 05:20:17 +0000

    Add param fetch to lotus-shed

---
+++ cmd/lotus-shed/params.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/docker/go-units"
+	paramfetch "github.com/filecoin-project/go-paramfetch"
+	"golang.org/x/xerrors"
+	"gopkg.in/urfave/cli.v2"
+
+	"github.com/filecoin-project/lotus/build"
+)
+
+var fetchParamCmd = &cli.Command{
+	Name:  "fetch-params",
+	Usage: "Fetch proving parameters",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "proving-params",
+			Usage: "download params used creating proofs for given size, i.e. 32GiB",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
+		if err != nil {
+			return err
+		}
+		sectorSize := uint64(sectorSizeInt)
+		err = paramfetch.GetParams(build.ParametersJson(), sectorSize)
+		if err != nil {
+			return xerrors.Errorf("fetching proof parameters: %w", err)
+		}
+
+		return nil
+	},
+}
