05d703e53bc717a644b045221dd8692e115f6e66
Łukasz Magiera <magik6k@gmail.com>
2020-06-04 21:19:15 +0200

    Fix testworker

--- testworker_test.go
+++ testworker_test.go
@@ -77,6 +77,10 @@ func (t *testWorker) FinalizeSector(ctx context.Context, sector abi.SectorID) er
 	panic("implement me")
 }
 
+func (t *testWorker) MoveStorage(ctx context.Context, sector abi.SectorID) error {
+	panic("implement me")
+}
+
 func (t *testWorker) Fetch(ctx context.Context, id abi.SectorID, fileType stores.SectorFileType, b bool, am stores.AcquireMode) error {
 	return nil
 }
