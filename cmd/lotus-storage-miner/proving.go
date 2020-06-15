12056c8904a41fcf398a5eb0dd299551ef9a44c4
chunqizhi <1558763837@qq.com>
2020-06-15 17:43:42 +0800

    go fmt

--- cmd/lotus-storage-miner/proving.go
+++ cmd/lotus-storage-miner/proving.go
@@ -15,8 +15,8 @@ import (
 )
 
 var provingCmd = &cli.Command{
-	Name: "proving",
-	Usage:"View proving information",
+	Name:  "proving",
+	Usage: "View proving information",
 	Subcommands: []*cli.Command{
 		provingInfoCmd,
 		provingDeadlinesCmd,
@@ -24,8 +24,8 @@ var provingCmd = &cli.Command{
 }
 
 var provingInfoCmd = &cli.Command{
-	Name: "info",
-	Usage:"View current state information",
+	Name:  "info",
+	Usage: "View current state information",
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
 		if err != nil {
@@ -147,8 +147,8 @@ func epochTime(curr, e abi.ChainEpoch) string {
 }
 
 var provingDeadlinesCmd = &cli.Command{
-	Name: "deadlines",
-	Usage:"View the current proving period deadlines information",
+	Name:  "deadlines",
+	Usage: "View the current proving period deadlines information",
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
 		if err != nil {
