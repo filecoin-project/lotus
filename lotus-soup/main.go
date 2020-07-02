19ce23e907ee01ff9a3a16f077b467587ed7a0e4
vyzo <vyzo@hackzen.org>
2020-07-02 18:44:13 +0300

    initialize block delay to its minimum value of 1

--- lotus-soup/main.go
+++ lotus-soup/main.go
@@ -4,12 +4,18 @@ import (
 	"github.com/testground/sdk-go/run"
 
 	"github.com/filecoin-project/oni/lotus-soup/testkit"
+
+	"github.com/filecoin-project/lotus/build"
 )
 
 var cases = map[string]interface{}{
 	"deals-e2e":         testkit.WrapTestEnvironment(dealsE2E),
 	"deals-stress-test": testkit.WrapTestEnvironment(dealStressTest),
 	"drand-halting":     testkit.WrapTestEnvironment(dealsE2E),
+}
+
+func init() {
+	build.BlockDelaySecs = 1
 }
 
 func main() {
