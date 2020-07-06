7b9ab5e2928f562483c2817e2f497f15d4e33e99
chunqizhi <1558763837@qq.com>
2020-07-06 17:49:05 +0800

    Fix an error in addCachePathsForSectorSize func

--- faults.go
+++ faults.go
@@ -98,9 +98,11 @@ func addCachePathsForSectorSize(chk map[string]int64, cacheDir string, ssize abi
 	case 512 << 20:
 		chk[filepath.Join(cacheDir, "sc-02-data-tree-r-last.dat")] = 0
 	case 32 << 30:
-		fallthrough
-	case 64 << 30:
 		for i := 0; i < 8; i++ {
+			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
+		}
+	case 64 << 30:
+		for i := 0; i < 16; i++ {
 			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
 		}
 	default:
