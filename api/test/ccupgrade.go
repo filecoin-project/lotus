4b44e4e73a853f3ec63c016671946153406f77da
Łukasz Magiera <magik6k@gmail.com>
2020-07-06 12:09:09 +0200

    fix ccupgrade test after master merge

--- api/test/ccupgrade.go
+++ api/test/ccupgrade.go
@@ -40,7 +40,7 @@ func TestCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration) {
 		defer close(done)
 		for atomic.LoadInt64(&mine) == 1 {
 			time.Sleep(blocktime)
-			if err := sn[0].MineOne(ctx, func(bool) {}); err != nil {
+			if err := sn[0].MineOne(ctx, func(bool, error) {}); err != nil {
 				t.Error(err)
 			}
 		}
