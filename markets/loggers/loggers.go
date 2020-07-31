0228ba1fb9c7a7a2dbcfc7add0350cfdf9bba463
hannahhoward <hannah@hannahhoward.net>
2020-07-30 18:54:59 -0700

    fix(markets): lower case labels

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -10,20 +10,20 @@ var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("Storage Event", "Name", storagemarket.ClientEvents[event], "Proposal CID", deal.ProposalCid, "State", storagemarket.DealStates[deal.State], "Message", deal.Message)
+	log.Infow("storage event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("Storage Event", "Name", storagemarket.ProviderEvents[event], "Proposal CID", deal.ProposalCid, "State", storagemarket.DealStates[deal.State], "Message", deal.Message)
+	log.Infow("storage event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("Retrieval Event", "Name", retrievalmarket.ClientEvents[event], "Deal ID", deal.ID, "State", retrievalmarket.DealStatuses[deal.Status], "Message", deal.Message)
+	log.Infow("retrieval event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-	log.Infow("Retrieval Event", "Name", retrievalmarket.ProviderEvents[event], "Deal ID", deal.ID, "Receiver", deal.Receiver, "State", retrievalmarket.DealStatuses[deal.Status], "Message", deal.Message)
+	log.Infow("retrieval event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
