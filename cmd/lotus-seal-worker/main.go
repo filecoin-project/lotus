de18d5521ef14bc5982adc8af998148a284d3029
Łukasz Magiera <magik6k@gmail.com>
2020-08-04 14:32:09 +0200

    workers: Enable unseal support

--- cmd/lotus-seal-worker/main.go
+++ cmd/lotus-seal-worker/main.go
@@ -107,6 +107,11 @@ var runCmd = &cli.Command{
 			Value: true,
 		},
 		&cli.BoolFlag{
+			Name:  "unseal",
+			Usage: "enable unsealing (32G sectors: 1 core, 128GiB Memory)",
+			Value: true,
+		},
+		&cli.BoolFlag{
 			Name:  "precommit2",
 			Usage: "enable precommit2 (32G sectors: all cores, 96GiB Memory)",
 			Value: true,
@@ -187,6 +192,9 @@ var runCmd = &cli.Command{
 
 		if cctx.Bool("precommit1") {
 			taskTypes = append(taskTypes, sealtasks.TTPreCommit1)
+		}
+		if cctx.Bool("unseal") {
+			taskTypes = append(taskTypes, sealtasks.TTUnseal)
 		}
 		if cctx.Bool("precommit2") {
 			taskTypes = append(taskTypes, sealtasks.TTPreCommit2)
