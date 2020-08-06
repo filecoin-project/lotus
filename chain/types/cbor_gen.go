5ef7328fe368659b7db23718fe01161a46bb84ea
Łukasz Magiera <magik6k@gmail.com>
2020-08-06 23:27:23 +0200

    Rerun cbor-gen

--- chain/types/cbor_gen.go
+++ chain/types/cbor_gen.go
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/abi"
 	"github.com/filecoin-project/specs-actors/actors/crypto"
 	"github.com/filecoin-project/specs-actors/actors/runtime/exitcode"
-	"github.com/ipfs/go-cid"
+	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	xerrors "golang.org/x/xerrors"
 )
@@ -632,7 +632,7 @@ func (t *ElectionProof) UnmarshalCBOR(r io.Reader) error {
 	return nil
 }
 
-var lengthBufMessage = []byte{139}
+var lengthBufMessage = []byte{138}
 
 func (t *Message) MarshalCBOR(w io.Writer) error {
 	if t == nil {
@@ -733,7 +733,7 @@ func (t *Message) UnmarshalCBOR(r io.Reader) error {
 		return fmt.Errorf("cbor input should be of type array")
 	}
 
-	if extra != 11 {
+	if extra != 10 {
 		return fmt.Errorf("cbor input had wrong number of fields")
 	}
 
@@ -801,11 +801,6 @@ func (t *Message) UnmarshalCBOR(r io.Reader) error {
 		if err := t.Value.UnmarshalCBOR(br); err != nil {
 			return xerrors.Errorf("unmarshaling t.Value: %w", err)
 		}
-
-	}
-	// t.GasPrice (big.Int) (struct)
-
-	{
 
 	}
 	// t.GasLimit (int64) (int64)
