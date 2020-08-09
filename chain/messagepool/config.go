5ea9df1824251da4b72bf030aa23dced9289684d
Jakub Sztandera <kubuxu@protocol.ai>
2020-08-09 02:22:37 +0200

    Use 25% overesitm
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -14,7 +14,7 @@ var (
 	MemPoolSizeLimitHiDefault = 30000
 	MemPoolSizeLimitLoDefault = 20000
 	PruneCooldownDefault      = time.Minute
-	GasLimitOverestimation    = 1.10
+	GasLimitOverestimation    = 1.25
 
 	ConfigKey = datastore.NewKey("/mpool/config")
 )
