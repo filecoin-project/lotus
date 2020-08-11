9032163c5a080a15167cbe3a0d5ae632173ba8d1
vyzo <vyzo@hackzen.org>
2020-08-11 20:47:50 +0300

    turn probability eyeballing into an actual test

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -4,5 +4,12 @@ import "testing"
 
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
-	t.Logf("%+v\n", mp.blockProbabilities(1-0.15))
+	bp := mp.blockProbabilities(1 - 0.15)
+	t.Logf("%+v\n", bp)
+	for i := 0; i < len(bp)-1; i++ {
+		if bp[i] < bp[i+1] {
+			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
+				i, bp[i], bp[i+1])
+		}
+	}
 }
