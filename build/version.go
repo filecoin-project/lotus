657b3901937daeb2465dc2648be8dd56f8365955
Łukasz Magiera <magik6k@gmail.com>
2020-08-11 12:11:59 +0200

    Bump version

--- build/version.go
+++ build/version.go
@@ -25,7 +25,7 @@ func buildType() string {
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "0.4.3"
+const BuildVersion = "0.4.4"
 
 func UserVersion() string {
 	return BuildVersion + buildType() + CurrentCommit
