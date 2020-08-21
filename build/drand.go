7456cdeb4224031e10c3ec52be2220d911f2f328
whyrusleeping <why@ipfs.io>
2020-08-20 17:21:33 -0700

    space race parameters

--- build/drand.go
+++ build/drand.go
@@ -2,7 +2,7 @@ package build
 
 import "github.com/filecoin-project/lotus/node/modules/dtypes"
 
-var DrandNetwork = DrandMainnet
+var DrandNetwork = DrandTestnet
 
 func DrandConfig() dtypes.DrandConfig {
 	return DrandConfigs[DrandNetwork]
