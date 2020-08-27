975cbbdaa43719fa2101deb1eaa0573c09f23919
Liviu Damian <liviudm@liviudm.com>
2020-08-27 22:17:40 +0300

    Nerpa genesis 20200827 v0.5.4

--- build/params_testnet.go
+++ build/params_testnet.go
@@ -13,10 +13,11 @@ import (
 )
 
 func init() {
-	power.ConsensusMinerMinPower = big.NewInt(10 << 40)
+	power.ConsensusMinerMinPower = big.NewInt(10 << 20)
 	miner.SupportedProofTypes = map[abi.RegisteredSealProof]struct{}{
-		abi.RegisteredSealProof_StackedDrg32GiBV1: {},
-		abi.RegisteredSealProof_StackedDrg64GiBV1: {},
+		abi.RegisteredSealProof_StackedDrg512MiBV1: {},
+		abi.RegisteredSealProof_StackedDrg32GiBV1:  {},
+		abi.RegisteredSealProof_StackedDrg64GiBV1:  {},
 	}
 }
 
