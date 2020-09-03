1d3a21f6c8919d403ef7f8da05f908c3dcd18454
Dirk McCormick <dirkmdev@gmail.com>
2020-09-03 08:22:40 +0200

    docs: paych - correct comments on store fields

--- paychmgr/store.go
+++ paychmgr/store.go
@@ -59,12 +59,12 @@ type ChannelInfo struct {
 	ChannelID string
 	// Channel address - may be nil if the channel hasn't been created yet
 	Channel *address.Address
-	// Control is the address of the account that created the channel
+	// Control is the address of the local node
 	Control address.Address
-	// Target is the address of the account on the other end of the channel
+	// Target is the address of the remote node (on the other end of the channel)
 	Target address.Address
-	// Direction indicates if the channel is inbound (this node is the Target)
-	// or outbound (this node is the Control)
+	// Direction indicates if the channel is inbound (Control is the "to" address)
+	// or outbound (Control is the "from" address)
 	Direction uint64
 	// Vouchers is a list of all vouchers sent on the channel
 	Vouchers []*VoucherInfo
