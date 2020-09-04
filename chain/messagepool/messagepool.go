275f071b12b8b0201eadb7714a2d960b0d4cf1ed
vyzo <vyzo@hackzen.org>
2020-09-05 00:28:13 +0300

    reduce mpool add failure log spam

--- chain/messagepool/messagepool.go
+++ chain/messagepool/messagepool.go
@@ -604,7 +604,7 @@ func (mp *MessagePool) addLocked(m *types.SignedMessage, strict bool) error {
 
 	incr, err := mset.add(m, mp, strict)
 	if err != nil {
-		log.Info(err)
+		log.Debug(err)
 		return err
 	}
 
