37c0f7549901a5403b619eb7df069005c5bad754
Łukasz Magiera <magik6k@gmail.com>
2020-09-09 13:07:15 +0200

    Bump version to 0.6.2-rc1

--- build/version.go
+++ build/version.go
@@ -25,7 +25,7 @@ func buildType() string {
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "0.6.1"
+const BuildVersion = "0.6.2-rc1"
 
 func UserVersion() string {
 	return BuildVersion + buildType() + CurrentCommit
