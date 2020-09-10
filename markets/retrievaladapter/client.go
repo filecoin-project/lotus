35bbacc063b779903822f436e3d8c262013d491d
hannahhoward <hannah@hannahhoward.net>
2020-09-10 13:27:40 -0700

    Revert "feat(markets): use new api"
    This reverts commit 5ad8e23eae0a386622a3121d4e6ebaf2dc64e091.

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -79,8 +79,14 @@ func (rcn *retrievalClientNode) WaitForPaymentChannelReady(ctx context.Context, 
 }
 
 func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {
-
-	channelAvailableFunds, err := rcn.payAPI.PaychAvailableFunds(paymentChannel)
+	// this doesn't actually work potentially -- the looked up from/to may pull up data for a different payment channel if for some reason the
+	// given address is settling
+	ci, err := rcn.payAPI.PaychMgr.GetChannelInfo(paymentChannel)
+	if err != nil {
+		return retrievalmarket.ChannelAvailableFunds{}, err
+	}
+	// assuming this is outbound... again, this is not a final implementation, pending PaychAvailableFundsByAddress
+	channelAvailableFunds, err := rcn.payAPI.PaychAvailableFunds(ci.Control, ci.Target)
 	if err != nil {
 		return retrievalmarket.ChannelAvailableFunds{}, err
 	}
