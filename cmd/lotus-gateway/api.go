1ffdd7d5b3a83ab313066714f1e305cbe967931a
Dirk McCormick <dirkmdev@gmail.com>
2020-09-30 10:43:59 +0200

    fix: gateway - remove tracing (JsonRPC automatically traces all calls)

--- cmd/lotus-gateway/api.go
+++ cmd/lotus-gateway/api.go
@@ -9,8 +9,6 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-
-	"go.opencensus.io/trace"
 )
 
 const LookbackCap = time.Hour
@@ -50,17 +48,12 @@ func (a *GatewayAPI) checkTipset(ctx context.Context, ts types.TipSetKey) error 
 }
 
 func (a *GatewayAPI) ChainHead(ctx context.Context) (*types.TipSet, error) {
-	ctx, span := trace.StartSpan(ctx, "ChainHead")
-	defer span.End()
 	// TODO: cache and invalidate cache when timestamp is up (or have internal ChainNotify)
 
 	return a.api.ChainHead(ctx)
 }
 
 func (a *GatewayAPI) ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error) {
-	ctx, span := trace.StartSpan(ctx, "ChainGetTipSet")
-	defer span.End()
-
 	if err := a.checkTipset(ctx, tsk); err != nil {
 		return nil, fmt.Errorf("bad tipset: %w", err)
 	}
@@ -70,18 +63,12 @@ func (a *GatewayAPI) ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*
 }
 
 func (a *GatewayAPI) MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error) {
-	ctx, span := trace.StartSpan(ctx, "MpoolPush")
-	defer span.End()
-
 	// TODO: additional anti-spam checks
 
 	return a.api.MpoolPushUntrusted(ctx, sm)
 }
 
 func (a *GatewayAPI) StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error) {
-	ctx, span := trace.StartSpan(ctx, "StateGetActor")
-	defer span.End()
-
 	if err := a.checkTipset(ctx, ts); err != nil {
 		return nil, fmt.Errorf("bad tipset: %w", err)
 	}
@@ -90,9 +77,6 @@ func (a *GatewayAPI) StateGetActor(ctx context.Context, actor address.Address, t
 }
 
 func (a *GatewayAPI) StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error) {
-	ctx, span := trace.StartSpan(ctx, "StateAccountKey")
-	defer span.End()
-
 	if err := a.checkTipset(ctx, tsk); err != nil {
 		return address.Undef, fmt.Errorf("bad tipset: %w", err)
 	}
