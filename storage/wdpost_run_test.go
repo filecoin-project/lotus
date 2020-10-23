4d874730538ae7b4845976b7a4c415bd58739942
Łukasz Magiera <magik6k@gmail.com>
2020-10-23 23:31:18 +0200

    Fix lint

--- storage/wdpost_run_test.go
+++ storage/wdpost_run_test.go
@@ -22,7 +22,6 @@ import (
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/journal"
