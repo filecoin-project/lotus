377f000a84966a87b52a6fbe27f7d51b877aab87
meows <b5c6@protonmail.com>
2020-10-30 10:52:43 -0500

    openrpc: init SchemaType mapper function
    This function will handle the manual configurations
    for app-specific data types w/r/t their json schema
    representation.
    
    This is useful for cases where the reflect library
    is unable to provide a sufficient representation
    automatically.
    
    Provided in this commit is an initial implementation
    for the integerD type (assuming number are represented
    in the API as hexs), and a commonly used cid.Cid type.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- api/openrpc/openrpc.go
+++ api/openrpc/openrpc.go
@@ -1,6 +1,7 @@
 package openrpc
 
 import (
+	"encoding/json"
 	"go/ast"
 	"net"
 	"reflect"
@@ -10,10 +11,80 @@ import (
 	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"
 	"github.com/filecoin-project/lotus/api/docgen"
 	"github.com/filecoin-project/lotus/build"
+	"github.com/ipfs/go-cid"
 	meta_schema "github.com/open-rpc/meta-schema"
 )
 
 var Comments, GroupDocs = docgen.ParseApiASTInfo()
+
+// schemaDictEntry represents a type association passed to the jsonschema reflector.
+type schemaDictEntry struct {
+	example interface{}
+	rawJson string
+}
+
+const integerD = `{
+          "title": "integer",
+          "type": "string",
+          "pattern": "^0x[a-fA-F0-9]+$",
+          "description": "Hex representation of the integer"
+        }`
+
+const cidCidD = `{"title": "Content Identifier", "type": "string", "description": "Cid represents a self-describing content addressed identifier. It is formed by a Version, a Codec (which indicates a multicodec-packed content type) and a Multihash."}`
+
+func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
+	unmarshalJSONToJSONSchemaType := func(input string) *jsonschema.Type {
+		var js jsonschema.Type
+		err := json.Unmarshal([]byte(input), &js)
+		if err != nil {
+			panic(err)
+		}
+		return &js
+	}
+
+	if ty.Kind() == reflect.Ptr {
+		ty = ty.Elem()
+	}
+
+	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {
+		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}
+	}
+
+	// Second, handle other types.
+	// Use a slice instead of a map because it preserves order, as a logic safeguard/fallback.
+	dict := []schemaDictEntry{
+		{cid.Cid{}, cidCidD},
+	}
+
+	for _, d := range dict {
+		if reflect.TypeOf(d.example) == ty {
+			tt := unmarshalJSONToJSONSchemaType(d.rawJson)
+
+			return tt
+		}
+	}
+
+	// Handle primitive types in case there are generic cases
+	// specific to our services.
+	switch ty.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		// Return all integer types as the hex representation integer schemea.
+		ret := unmarshalJSONToJSONSchemaType(integerD)
+		return ret
+	case reflect.Uintptr:
+		return &jsonschema.Type{Type: "number", Title: "uintptr-title"}
+	case reflect.Struct:
+	case reflect.Map:
+	case reflect.Slice, reflect.Array:
+	case reflect.Float32, reflect.Float64:
+	case reflect.Bool:
+	case reflect.String:
+	case reflect.Ptr, reflect.Interface:
+	default:
+	}
+
+	return nil
+}
 
 // NewLotusOpenRPCDocument defines application-specific documentation and configuration for its OpenRPC document.
 func NewLotusOpenRPCDocument() *go_openrpc_reflect.Document {
@@ -52,12 +123,7 @@ func NewLotusOpenRPCDocument() *go_openrpc_reflect.Document {
 
 	// Install overrides for the json schema->type map fn used by the jsonschema reflect package.
 	appReflector.FnSchemaTypeMap = func() func(ty reflect.Type) *jsonschema.Type {
-		return func(ty reflect.Type) *jsonschema.Type {
-			if ty.String() == "uintptr" {
-				return &jsonschema.Type{Type: "number", Title: "uintptr-title"}
-			}
-			return nil
-		}
+		return OpenRPCSchemaTypeMapper
 	}
 
 	appReflector.FnIsMethodEligible = func (m reflect.Method) bool {
