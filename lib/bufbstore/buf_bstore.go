659ceaabdd359f11c2966dc238b9012035ea130c
Raúl Kripalani <raul@protocol.ai>
2020-11-10 17:51:11 +0000

    fix comment.

--- lib/bufbstore/buf_bstore.go
+++ lib/bufbstore/buf_bstore.go
@@ -109,7 +109,7 @@ func (bs *BufferedBS) DeleteBlock(c cid.Cid) error {
 
 func (bs *BufferedBS) View(c cid.Cid, callback func([]byte) error) error {
 	if bs.writeviewer == nil || bs.readviewer == nil {
-		// one of the stores doesn't Viewer; fall back to pure Get behaviour.
+		// one of the stores isn't Viewer; fall back to pure Get behaviour.
 		blk, err := bs.Get(c)
 		if err != nil {
 			return err
