8c15661e5bb4ac514fec52b2434e561239b781ee
zgfzgf <1901989065@qq.com>
2020-11-20 07:44:39 +0800

    order resource table, put 2k last

--- extern/sector-storage/resources.go
+++ extern/sector-storage/resources.go
@@ -2,34 +2,31 @@ package sectorstorage
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
 type Resources struct {
-	MinMemory uint64 // What Must be in RAM for decent perf
-	MaxMemory uint64 // Memory required (swap + ram)
-
-	MaxParallelism int // -1 = multithread
+	MinMemory      uint64 // What Must be in RAM for decent perf
+	MaxMemory      uint64 // Memory required (swap + ram)
+	BaseMinMemory  uint64 // What Must be in RAM for decent perf (shared between threads)
+	MaxParallelism int    // -1 = multithread
 	CanGPU         bool
-
-	BaseMinMemory uint64 // What Must be in RAM for decent perf (shared between threads)
 }
 
 /*
-
  Percent of threads to allocate to parallel tasks
-
  12  * 0.92 = 11
  16  * 0.92 = 14
  24  * 0.92 = 22
  32  * 0.92 = 29
  64  * 0.92 = 58
  128 * 0.92 = 117
-
 */
-var ParallelNum uint64 = 92
-var ParallelDenom uint64 = 100
+
+var (
+	ParallelNum   uint64 = 92
+	ParallelDenom uint64 = 100
+)
 
 // TODO: Take NUMA into account
 func (r Resources) Threads(wcpus uint64) uint64 {
@@ -47,266 +44,201 @@ func (r Resources) Threads(wcpus uint64) uint64 {
 var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources{
 	sealtasks.TTAddPiece: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 8 << 30,
-			MinMemory: 8 << 30,
-
+			MaxMemory:      8 << 30,
+			MinMemory:      8 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 4 << 30,
-			MinMemory: 4 << 30,
-
+			MaxMemory:      4 << 30,
+			MinMemory:      4 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 1 << 30,
-			MinMemory: 1 << 30,
-
+			MaxMemory:      1 << 30,
+			MinMemory:      1 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 1 << 30,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,
-			MinMemory: 2 << 10,
-
-			MaxParallelism: 1,
-
-			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 8 << 20,
-			MinMemory: 8 << 20,
-
+			MaxMemory:      8 << 20,
+			MinMemory:      8 << 20,
+			BaseMinMemory:  8 << 20,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 8 << 20,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      2 << 10,
+			MinMemory:      2 << 10,
+			BaseMinMemory:  2 << 10,
+			MaxParallelism: 1,
 		},
 	},
 	sealtasks.TTPreCommit1: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 128 << 30,
-			MinMemory: 112 << 30,
-
+			MaxMemory:      128 << 30,
+			MinMemory:      112 << 30,
+			BaseMinMemory:  10 << 20,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 10 << 20,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 64 << 30,
-			MinMemory: 56 << 30,
-
+			MaxMemory:      64 << 30,
+			MinMemory:      56 << 30,
+			BaseMinMemory:  10 << 20,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 10 << 20,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 1 << 30,
-			MinMemory: 768 << 20,
-
+			MaxMemory:      1 << 30,
+			MinMemory:      768 << 20,
+			BaseMinMemory:  1 << 20,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 1 << 20,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,
-			MinMemory: 2 << 10,
-
-			MaxParallelism: 1,
-
-			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 8 << 20,
-			MinMemory: 8 << 20,
-
+			MaxMemory:      8 << 20,
+			MinMemory:      8 << 20,
+			BaseMinMemory:  8 << 20,
 			MaxParallelism: 1,
-
-			BaseMinMemory: 8 << 20,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      2 << 10,
+			MinMemory:      2 << 10,
+			BaseMinMemory:  2 << 10,
+			MaxParallelism: 1,
 		},
 	},
 	sealtasks.TTPreCommit2: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 30 << 30,
-			MinMemory: 30 << 30,
-
+			MaxMemory:      30 << 30,
+			MinMemory:      30 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: -1,
 			CanGPU:         true,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 15 << 30,
-			MinMemory: 15 << 30,
-
+			MaxMemory:      15 << 30,
+			MinMemory:      15 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: -1,
 			CanGPU:         true,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 3 << 29, // 1.5G
-			MinMemory: 1 << 30,
-
+			MaxMemory:      3 << 29, // 1.5G
+			MinMemory:      1 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: -1,
-
-			BaseMinMemory: 1 << 30,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,
-			MinMemory: 2 << 10,
-
-			MaxParallelism: -1,
-
-			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 8 << 20,
-			MinMemory: 8 << 20,
-
+			MaxMemory:      8 << 20,
+			MinMemory:      8 << 20,
+			BaseMinMemory:  8 << 20,
 			MaxParallelism: -1,
-
-			BaseMinMemory: 8 << 20,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      2 << 10,
+			MinMemory:      2 << 10,
+			BaseMinMemory:  2 << 10,
+			MaxParallelism: -1,
 		},
 	},
 	sealtasks.TTCommit1: { // Very short (~100ms), so params are very light
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 1 << 30,
-			MinMemory: 1 << 30,
-
+			MaxMemory:      1 << 30,
+			MinMemory:      1 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 0,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 1 << 30,
-			MinMemory: 1 << 30,
-
+			MaxMemory:      1 << 30,
+			MinMemory:      1 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 0,
-
-			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 1 << 30,
-			MinMemory: 1 << 30,
-
+			MaxMemory:      1 << 30,
+			MinMemory:      1 << 30,
+			BaseMinMemory:  1 << 30,
 			MaxParallelism: 0,
-
-			BaseMinMemory: 1 << 30,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,
-			MinMemory: 2 << 10,
-
-			MaxParallelism: 0,
-
-			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 8 << 20,
-			MinMemory: 8 << 20,
-
+			MaxMemory:      8 << 20,
+			MinMemory:      8 << 20,
+			BaseMinMemory:  8 << 20,
 			MaxParallelism: 0,
-
-			BaseMinMemory: 8 << 20,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      2 << 10,
+			MinMemory:      2 << 10,
+			BaseMinMemory:  2 << 10,
+			MaxParallelism: 0,
 		},
 	},
 	sealtasks.TTCommit2: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 190 << 30, // TODO: Confirm
-			MinMemory: 60 << 30,
-
+			MaxMemory:      190 << 30, // TODO: Confirm
+			MinMemory:      60 << 30,
+			BaseMinMemory:  64 << 30, // params
 			MaxParallelism: -1,
 			CanGPU:         true,
-
-			BaseMinMemory: 64 << 30, // params
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 150 << 30, // TODO: ~30G of this should really be BaseMaxMemory
-			MinMemory: 30 << 30,
-
+			MaxMemory:      150 << 30, // TODO: ~30G of this should really be BaseMaxMemory
+			MinMemory:      30 << 30,
+			BaseMinMemory:  32 << 30, // params
 			MaxParallelism: -1,
 			CanGPU:         true,
-
-			BaseMinMemory: 32 << 30, // params
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 3 << 29, // 1.5G
-			MinMemory: 1 << 30,
-
+			MaxMemory:      3 << 29, // 1.5G
+			MinMemory:      1 << 30,
+			BaseMinMemory:  10 << 30,
 			MaxParallelism: 1, // This is fine
 			CanGPU:         true,
-
-			BaseMinMemory: 10 << 30,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,
-			MinMemory: 2 << 10,
-
-			MaxParallelism: 1,
-			CanGPU:         true,
-
-			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 8 << 20,
-			MinMemory: 8 << 20,
-
+			MaxMemory:      8 << 20,
+			MinMemory:      8 << 20,
+			BaseMinMemory:  8 << 20,
 			MaxParallelism: 1,
 			CanGPU:         true,
-
-			BaseMinMemory: 8 << 20,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      2 << 10,
+			MinMemory:      2 << 10,
+			BaseMinMemory:  2 << 10,
+			MaxParallelism: 1,
+			CanGPU:         true,
 		},
 	},
 	sealtasks.TTFetch: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 1 << 20,
-			MinMemory: 1 << 20,
-
+			MaxMemory:      1 << 20,
+			MinMemory:      1 << 20,
+			BaseMinMemory:  0,
 			MaxParallelism: 0,
-			CanGPU:         false,
-
-			BaseMinMemory: 0,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 1 << 20,
-			MinMemory: 1 << 20,
-
+			MaxMemory:      1 << 20,
+			MinMemory:      1 << 20,
+			BaseMinMemory:  0,
 			MaxParallelism: 0,
-			CanGPU:         false,
-
-			BaseMinMemory: 0,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
-			MaxMemory: 1 << 20,
-			MinMemory: 1 << 20,
-
+			MaxMemory:      1 << 20,
+			MinMemory:      1 << 20,
+			BaseMinMemory:  0,
 			MaxParallelism: 0,
-			CanGPU:         false,
-
-			BaseMinMemory: 0,
-		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 1 << 20,
-			MinMemory: 1 << 20,
-
-			MaxParallelism: 0,
-			CanGPU:         false,
-
-			BaseMinMemory: 0,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
-			MaxMemory: 1 << 20,
-			MinMemory: 1 << 20,
-
+			MaxMemory:      1 << 20,
+			MinMemory:      1 << 20,
+			BaseMinMemory:  0,
 			MaxParallelism: 0,
-			CanGPU:         false,
-
-			BaseMinMemory: 0,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory:      1 << 20,
+			MinMemory:      1 << 20,
+			BaseMinMemory:  0,
+			MaxParallelism: 0,
 		},
 	},
 }
