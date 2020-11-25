2e544e3e6a2b226618b58397f8e3f21fdf08a8f8
Łukasz Magiera <magik6k@gmail.com>
2020-11-25 19:57:38 +0100

    configure SimultaneousTransfers in node/builder

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,7 +1,6 @@
 package modules
 
 import (
-	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
@@ -12,45 +11,37 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
 )
 
 // Graphsync creates a graphsync instance from the given loader and storer
-func Graphsync(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ChainBlockstore, h host.Host) (dtypes.Graphsync, error) {
-	graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-	loader := storeutil.LoaderForBlockstore(clientBs)
-	storer := storeutil.StorerForBlockstore(clientBs)
-	raw, err := r.Config()
-	if err != nil {
-		return nil, err
-	}
+func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ChainBlockstore, h host.Host) (dtypes.Graphsync, error) {
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ChainBlockstore, h host.Host) (dtypes.Graphsync, error) {
+		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
+		loader := storeutil.LoaderForBlockstore(clientBs)
+		storer := storeutil.StorerForBlockstore(clientBs)
 
-	cfg, ok := raw.(*config.FullNode)
-	if !ok {
-		return nil, xerrors.New("expected address of config.FullNode")
-	}
-
-	gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(cfg.Client.SimultaneousTransfers))
-	chainLoader := storeutil.LoaderForBlockstore(chainBs)
-	chainStorer := storeutil.StorerForBlockstore(chainBs)
-	err = gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
-	if err != nil {
-		return nil, err
-	}
-	gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-		_, has := requestData.Extension("chainsync")
-		if has {
-			// TODO: we should confirm the selector is a reasonable one before we validate
-			// TODO: this code will get more complicated and should probably not live here eventually
-			hookActions.ValidateRequest()
-			hookActions.UsePersistenceOption("chainstore")
+		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)
+		chainStorer := storeutil.StorerForBlockstore(chainBs)
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
+		if err != nil {
+			return nil, err
 		}
-	})
-	gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
-		_, has := requestData.Extension("chainsync")
-		if has {
-			hookActions.UsePersistenceOption("chainstore")
-		}
-	})
-	return gs, nil
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
+			_, has := requestData.Extension("chainsync")
+			if has {
+				// TODO: we should confirm the selector is a reasonable one before we validate
+				// TODO: this code will get more complicated and should probably not live here eventually
+				hookActions.ValidateRequest()
+				hookActions.UsePersistenceOption("chainstore")
+			}
+		})
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
+			_, has := requestData.Extension("chainsync")
+			if has {
+				hookActions.UsePersistenceOption("chainstore")
+			}
+		})
+		return gs, nil
+	}
 }
