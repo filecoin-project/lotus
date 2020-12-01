e07c6c71c017ed4ade07c330591ba37032257b48
vyzo <vyzo@hackzen.org>
2020-12-01 17:17:34 +0200

    splitstore constructor

--- chain/store/splitstore/liveset.go
+++ chain/store/splitstore/liveset.go
@@ -1,10 +1,15 @@
 package splitstore
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/bmatsuo/lmdb-go/lmdb"
 
 	cid "github.com/ipfs/go-cid"
 )
+
+var LiveSetMapSize int64 = 1 << 34 // 16G; TODO this may be a little too big, we should figure out how to gradually grow the map.
 
 type LiveSet interface {
 	Mark(cid.Cid) error
@@ -18,6 +23,39 @@ type liveSet struct {
 }
 
 var markBytes = []byte{}
+
+func NewLiveSetEnv(path string) (*lmdb.Env, error) {
+	env, err := lmdb.NewEnv()
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize LDMB env: %w", err)
+	}
+	if err = env.SetMapSize(LiveSetMapSize); err != nil {
+		return nil, fmt.Errorf("failed to set LMDB map size: %w", err)
+	}
+	if err = env.SetMaxDBs(2); err != nil {
+		return nil, fmt.Errorf("failed to set LMDB max dbs: %w", err)
+	}
+	if err = env.SetMaxReaders(1); err != nil {
+		return nil, fmt.Errorf("failed to set LMDB max readers: %w", err)
+	}
+
+	if st, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.MkdirAll(path, 0777); err != nil {
+			return nil, fmt.Errorf("failed to create LMDB data directory at %s: %w", path, err)
+		}
+	} else if err != nil {
+		return nil, fmt.Errorf("failed to stat LMDB data dir: %w", err)
+	} else if !st.IsDir() {
+		return nil, fmt.Errorf("LMDB path is not a directory %s", path)
+	}
+	err = env.Open(path, lmdb.NoSync|lmdb.WriteMap|lmdb.MapAsync|lmdb.NoReadahead, 0777)
+	if err != nil {
+		env.Close() //nolint:errcheck
+		return nil, fmt.Errorf("error opening LMDB database: %w", err)
+	}
+
+	return env, nil
+}
 
 func NewLiveSet(env *lmdb.Env, name string) (LiveSet, error) {
 	var db lmdb.DBI
