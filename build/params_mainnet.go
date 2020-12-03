a584fcb935c084f26bb88def6b5a21ec9b6c623b
Travis Person <travisperson@users.noreply.github.com>
2020-12-03 01:27:07 +0000

    sync: log a message when there aren't enough peers

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -59,4 +59,5 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
