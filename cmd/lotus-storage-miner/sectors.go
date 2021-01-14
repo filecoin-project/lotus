4a114150f0af84afc5be9739c1ae4c35d2d2e3dd
Łukasz Magiera <magik6k@gmail.com>
2021-01-14 16:14:24 +0100

    fix miner terminate cli arg checks

--- cmd/lotus-storage-miner/sectors.go
+++ cmd/lotus-storage-miner/sectors.go
@@ -444,9 +444,6 @@ var sectorsTerminateFlushCmd = &cli.Command{
 		}
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
-		if cctx.Args().Len() != 1 {
-			return xerrors.Errorf("must pass sector number")
-		}
 
 		mcid, err := nodeApi.SectorTerminateFlush(ctx)
 		if err != nil {
@@ -471,9 +468,6 @@ var sectorsTerminatePendingCmd = &cli.Command{
 		}
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
-		if cctx.Args().Len() != 1 {
-			return xerrors.Errorf("must pass sector number")
-		}
 
 		pending, err := nodeApi.SectorTerminatePending(ctx)
 		if err != nil {
