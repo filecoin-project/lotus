5a5e49dcd7c75d70aab9d6607b2e85e142273b11
Steven Allen <steven@stebalien.com>
2021-01-21 11:26:55 -0800

    support paych v3 messages

--- chain/actors/builtin/paych/message.go
+++ chain/actors/builtin/paych/message.go
@@ -19,6 +19,8 @@ func Message(version actors.Version, from address.Address) MessageBuilder {
 		return message0{from}
 	case actors.Version2:
 		return message2{from}
+	case actors.Version3:
+		return message3{from}
 	default:
 		panic(fmt.Sprintf("unsupported actors version: %d", version))
 	}
