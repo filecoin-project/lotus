cc91eab0fd3941d76e68dc4a24b96bbe1507f2fd
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-02-08 17:38:51 +0100

    fix lr.Datastore interface to include context

--- testplans/lotus-soup/testkit/role_miner.go
+++ testplans/lotus-soup/testkit/role_miner.go
@@ -165,7 +165,7 @@ func PrepareMiner(t *TestEnvironment) (*LotusMiner, error) {
 			return nil, err
 		}
 
-		ds, err := lr.Datastore("/metadata")
+		ds, err := lr.Datastore(context.Background(), "/metadata")
 		if err != nil {
 			return nil, err
 		}
