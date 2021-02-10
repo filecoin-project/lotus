7fd57d28e4335fdcb2aac8571de82c6b85187f5d
vyzo <vyzo@hackzen.org>
2021-02-10 19:21:42 +0200

    return annotated xerrors where appropriate

--- chain/store/splitstore/lmdb_util.go
+++ chain/store/splitstore/lmdb_util.go
@@ -4,18 +4,24 @@ import (
 	"math/rand"
 	"time"
 
+	"golang.org/x/xerrors"
+
 	"github.com/ledgerwatch/lmdb-go/lmdb"
 )
 
 func withMaxReadersRetry(f func() error) error {
 retry:
 	err := f()
-	if lmdb.IsErrno(err, lmdb.ReadersFull) {
+	if err != nil && lmdb.IsErrno(err, lmdb.ReadersFull) {
 		dt := time.Microsecond + time.Duration(rand.Intn(int(time.Millisecond)))
 		log.Debugf("MDB_READERS_FULL; retrying operation in %s", dt)
 		time.Sleep(dt)
 		goto retry
 	}
 
-	return err
+	if err != nil {
+		return xerrors.Errorf("error performing lmdb operation: %w", err)
+	}
+
+	return nil
 }
