f6c930d0aa53c3df36ce4f8d5d9dafb9a542917e
vyzo <vyzo@hackzen.org>
2021-02-13 16:54:19 +0200

    crank up blockstore max readers to 16K, reduce retry delays to 10us

--- chain/store/splitstore/lmdb_util.go
+++ chain/store/splitstore/lmdb_util.go
@@ -13,7 +13,7 @@ func withMaxReadersRetry(f func() error) error {
 retry:
 	err := f()
 	if err != nil && lmdb.IsErrno(err, lmdb.ReadersFull) {
-		dt := time.Microsecond + time.Duration(rand.Intn(int(time.Millisecond)))
+		dt := time.Microsecond + time.Duration(rand.Intn(int(10*time.Microsecond)))
 		log.Debugf("MDB_READERS_FULL; retrying operation in %s", dt)
 		time.Sleep(dt)
 		goto retry
