006c55a7c9c7a74ea07c4cea0e5bee7a573f539d
vyzo <vyzo@hackzen.org>
2021-03-02 19:07:54 +0200

    add startup log

--- blockstore/splitstore/splitstore.go
+++ blockstore/splitstore/splitstore.go
@@ -356,6 +356,8 @@ func (s *SplitStore) Start(chain ChainAccessor) error {
 		return xerrors.Errorf("error loading warmup epoch: %w", err)
 	}
 
+	log.Infof("starting splitstore at base epoch %d", s.baseEpoch)
+
 	// watch the chain
 	chain.SubscribeHeadChanges(s.HeadChange)
 
