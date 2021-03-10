425d75684d5000242e5d8bc11c609fc83b6c2d5d
vyzo <vyzo@hackzen.org>
2021-03-10 20:01:28 +0200

    make peer counting more efficient

--- node/impl/full.go
+++ node/impl/full.go
@@ -51,24 +51,31 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context) (status api.NodeStatus, er
 	delta := time.Since(timestamp).Seconds()
 	status.SyncStatus.Behind = uint64(delta / 30)
 
+	// get connected peers
 	pis, err := n.NetPeers(ctx)
 	if err != nil {
 		return status, err
 	}
 
+	peers := make(map[peer.ID]struct{})
+	for _, pi := range pis {
+		peers[pi.ID] = struct{}{}
+	}
+
+	// get scores for all connected and recent peers
 	scores, err := n.NetPubsubScores(ctx)
 	if err != nil {
 		return status, err
 	}
 
-	scoreMap := make(map[peer.ID]float64)
 	for _, score := range scores {
-		scoreMap[score.ID] = score.Score.Score
-	}
+		// we only care about connected peers
+		_, connected := peers[score.ID]
+		if !connected {
+			continue
+		}
 
-	for _, pi := range pis {
-		score := scoreMap[pi.ID]
-		if score > -1000 {
+		if score.Score.Score > -1000 {
 			status.PeerStatus.PeersToPublish++
 		}
 	}
