b68eed906a3fc9c8f6476851769d24ddf2a661ac
Łukasz Magiera <magik6k@gmail.com>
2021-04-02 12:37:20 +0200

    fix lint

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -101,9 +101,8 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 				if os.Getenv("LOTUS_ALLOW_TRUNCATED_LOG") == "1" {
 					log.Errorw("log entry potentially truncated")
 					return false, nil
-				} else {
-					return false, xerrors.Errorf("log entry potentially truncated, set LOTUS_ALLOW_TRUNCATED_LOG=1 to proceed: %w", err)
 				}
+				return false, xerrors.Errorf("log entry potentially truncated, set LOTUS_ALLOW_TRUNCATED_LOG=1 to proceed: %w", err)
 			default:
 				return false, xerrors.Errorf("unmarshaling log entry: %w", err)
 			}
