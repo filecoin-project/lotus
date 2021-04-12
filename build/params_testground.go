732bccae594066c11911c2293a193874852906b6
Aayush Rajasekaran <arajasek94@gmail.com>
2021-04-12 18:13:40 -0400

    Newest newtork version is now 11

--- build/params_testground.go
+++ build/params_testground.go
@@ -99,7 +99,7 @@ var (
 		0: DrandMainnet,
 	}
 
-	NewestNetworkVersion       = network.Version9
+	NewestNetworkVersion       = network.Version11
 	ActorUpgradeNetworkVersion = network.Version4
 
 	Devnet      = true
