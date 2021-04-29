2857f6c0edc9a6e31ebe3a646a6a3dc8063e2bd8
Steven Allen <steven@stebalien.com>
2021-04-29 08:46:38 -0700

    fix: use a consistent tipset in commands
    It's very easy to write an incorrect command that operates over
    different heads by using the "empty" tipset. This change makes the
    `LoadTipSet` command helper get the latest head from the lotus daemon if
    its unset.
    
    The cost is an extra call to get the head. That should be trivial in
    most cases.

--- cli/state.go
+++ cli/state.go
@@ -180,10 +180,13 @@ func ParseTipSetString(ts string) ([]cid.Cid, error) {
 	return cids, nil
 }
 
+// LoadTipSet gets the tipset from the context, or the head from the API.
+//
+// It always gets the head from the API so commands use a consistent tipset even if time pases.
 func LoadTipSet(ctx context.Context, cctx *cli.Context, api v0api.FullNode) (*types.TipSet, error) {
 	tss := cctx.String("tipset")
 	if tss == "" {
-		return nil, nil
+		return api.ChainHead(ctx)
 	}
 
 	return ParseTipSetRef(ctx, api, tss)
@@ -850,14 +853,6 @@ var StateListMessagesCmd = &cli.Command{
 			return err
 		}
 
-		if ts == nil {
-			head, err := api.ChainHead(ctx)
-			if err != nil {
-				return err
-			}
-			ts = head
-		}
-
 		windowSize := abi.ChainEpoch(100)
 
 		cur := ts
@@ -957,13 +952,6 @@ var StateComputeStateCmd = &cli.Command{
 		}
 
 		h := abi.ChainEpoch(cctx.Uint64("vm-height"))
-		if ts == nil {
-			head, err := api.ChainHead(ctx)
-			if err != nil {
-				return err
-			}
-			ts = head
-		}
 		if h == 0 {
 			h = ts.Height()
 		}
@@ -1763,13 +1751,6 @@ var StateSectorCmd = &cli.Command{
 		ts, err := LoadTipSet(ctx, cctx, api)
 		if err != nil {
 			return err
-		}
-
-		if ts == nil {
-			ts, err = api.ChainHead(ctx)
-			if err != nil {
-				return err
-			}
 		}
 
 		maddr, err := address.NewFromString(cctx.Args().Get(0))
