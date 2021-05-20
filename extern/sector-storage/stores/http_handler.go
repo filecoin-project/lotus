1a9b5760a70ff0ab004d7e05ec0f1bae87235f47
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-05-20 13:14:25 +0200

    add remoteGetAllocated http handler

--- extern/sector-storage/stores/http_handler.go
+++ extern/sector-storage/stores/http_handler.go
@@ -5,11 +5,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	logging "github.com/ipfs/go-log/v2"
 	"golang.org/x/xerrors"
 
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/extern/sector-storage/tarutil"
 
@@ -26,6 +29,7 @@ func (handler *FetchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")
+	mux.HandleFunc("/remote/{type}/{id}/{spt}/allocated/{offset}/{size}", handler.remoteGetAllocated).Methods("GET")
 	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")
 	mux.HandleFunc("/remote/{type}/{id}", handler.remoteDeleteSector).Methods("DELETE")
 
@@ -52,6 +56,104 @@ func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request
 	if err := json.NewEncoder(w).Encode(&st); err != nil {
 		log.Warnf("error writing stat response: %+v", err)
 	}
+}
+
+func (handler *FetchHandler) remoteGetAllocated(w http.ResponseWriter, r *http.Request) {
+	log.Infof("SERVE Alloc check %s", r.URL)
+	vars := mux.Vars(r)
+
+	id, err := storiface.ParseSectorID(vars["id"])
+	if err != nil {
+		log.Errorf("%+v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	ft, err := ftFromString(vars["type"])
+	if err != nil {
+		log.Errorf("%+v", err)
+		w.WriteHeader(500)
+		return
+	}
+	if ft != storiface.FTUnsealed {
+		log.Errorf("/allocated only supports unsealed sector files")
+		w.WriteHeader(500)
+		return
+	}
+
+	spti, err := strconv.ParseInt(vars["spt"], 10, 64)
+	if err != nil {
+		log.Errorf("parsing spt: %+v", err)
+		w.WriteHeader(500)
+		return
+	}
+	spt := abi.RegisteredSealProof(spti)
+	ssize, err := spt.SectorSize()
+	if err != nil {
+		log.Errorf("%+v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	offi, err := strconv.ParseInt(vars["offset"], 10, 64)
+	if err != nil {
+		log.Errorf("parsing offset: %+v", err)
+		w.WriteHeader(500)
+		return
+	}
+	szi, err := strconv.ParseInt(vars["size"], 10, 64)
+	if err != nil {
+		log.Errorf("parsing spt: %+v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	// The caller has a lock on this sector already, no need to get one here
+
+	// passing 0 spt because we don't allocate anything
+	si := storage.SectorRef{
+		ID:        id,
+		ProofType: 0,
+	}
+
+	paths, _, err := handler.Local.AcquireSector(r.Context(), si, ft, storiface.FTNone, storiface.PathStorage, storiface.AcquireMove)
+	if err != nil {
+		log.Errorf("%+v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	path := storiface.PathByType(paths, ft)
+	if path == "" {
+		log.Error("acquired path was empty")
+		w.WriteHeader(500)
+		return
+	}
+
+	pf, err := ffiwrapper.OpenPartialFile(abi.PaddedPieceSize(ssize), path)
+	if err != nil {
+		log.Error("opening partial file: ", err)
+		w.WriteHeader(500)
+		return
+	}
+	defer func() {
+		if err := pf.Close(); err != nil {
+			log.Error("close partial file: ", err)
+		}
+	}()
+
+	has, err := pf.HasAllocated(storiface.UnpaddedByteIndex(offi), abi.UnpaddedPieceSize(szi))
+	if err != nil {
+		log.Error("has allocated: ", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if has {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	w.WriteHeader(http.StatusRequestedRangeNotSatisfiable)
 }
 
 func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Request) {
