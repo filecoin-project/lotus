2f0a9f6c407658499f68d3767fcf166c91924067
Łukasz Magiera <magik6k@gmail.com>
2021-06-01 21:28:48 +0200

    Improve AddVerifiedClient test

--- api/test/test.go
+++ api/test/test.go
@@ -122,31 +122,45 @@ var OneFull = DefaultFullOpts(1)
 var TwoFull = DefaultFullOpts(2)
 
 var FullNodeWithLatestActorsAt = func(upgradeHeight abi.ChainEpoch) FullNodeOpts {
-	if upgradeHeight == -1 {
-		// Attention: Update this when introducing new actor versions or your tests will be sad
-		upgradeHeight = 4
+	// Attention: Update this when introducing new actor versions or your tests will be sad
+	return FullNodeWithActorsUpgradeAt(network.Version13, upgradeHeight)
+}
+
+var FullNodeWithActorsUpgradeAt = func(version network.Version, upgradeHeight abi.ChainEpoch) FullNodeOpts {
+	fullSchedule := stmgr.UpgradeSchedule{{
+		// prepare for upgrade.
+		Network:   network.Version9,
+		Height:    1,
+		Migration: stmgr.UpgradeActorsV2,
+	}, {
+		Network:   network.Version10,
+		Height:    2,
+		Migration: stmgr.UpgradeActorsV3,
+	}, {
+		Network:   network.Version12,
+		Height:    3,
+		Migration: stmgr.UpgradeActorsV4,
+	}, {
+		Network:   network.Version13,
+		Height:    4,
+		Migration: stmgr.UpgradeActorsV5,
+	}}
+
+	schedule := stmgr.UpgradeSchedule{}
+	for _, upgrade := range fullSchedule {
+		if upgrade.Network > version {
+			break
+		}
+
+		schedule = append(schedule, upgrade)
 	}
 
+	if upgradeHeight > 0 {
+		schedule[len(schedule)-1].Height = upgradeHeight
+	}
 	return FullNodeOpts{
 		Opts: func(nodes []TestNode) node.Option {
-			return node.Override(new(stmgr.UpgradeSchedule), stmgr.UpgradeSchedule{{
-				// prepare for upgrade.
-				Network:   network.Version9,
-				Height:    1,
-				Migration: stmgr.UpgradeActorsV2,
-			}, {
-				Network:   network.Version10,
-				Height:    2,
-				Migration: stmgr.UpgradeActorsV3,
-			}, {
-				Network:   network.Version12,
-				Height:    3,
-				Migration: stmgr.UpgradeActorsV4,
-			}, {
-				Network:   network.Version13,
-				Height:    upgradeHeight,
-				Migration: stmgr.UpgradeActorsV5,
-			}})
+			return node.Override(new(stmgr.UpgradeSchedule), fullSchedule)
 		},
 	}
 }
