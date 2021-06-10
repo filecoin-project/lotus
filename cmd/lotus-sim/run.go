936659d087ecba1f66b503e6e13900a266d6868e
Steven Allen <steven@stebalien.com>
2021-06-09 21:27:48 -0700

    feat(lotus-sim): print info on SIGUSR1

--- cmd/lotus-sim/run.go
+++ cmd/lotus-sim/run.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"syscall"
 
 	"github.com/urfave/cli/v2"
@@ -36,12 +38,29 @@ var runSimCommand = &cli.Command{
 		}
 		fmt.Fprintln(cctx.App.Writer, "running simulation")
 		targetEpochs := cctx.Int("epochs")
+
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGUSR1)
+		defer signal.Stop(ch)
+
 		for i := 0; targetEpochs == 0 || i < targetEpochs; i++ {
 			ts, err := sim.Step(cctx.Context)
 			if err != nil {
 				return err
 			}
+
 			fmt.Fprintf(cctx.App.Writer, "advanced to %d %s\n", ts.Height(), ts.Key())
+
+			// Print
+			select {
+			case <-ch:
+				if err := printInfo(cctx.Context, sim, cctx.App.Writer); err != nil {
+					fmt.Fprintf(cctx.App.ErrWriter, "ERROR: failed to print info: %s\n", err)
+				}
+			case <-cctx.Context.Done():
+				return cctx.Err()
+			default:
+			}
 		}
 		fmt.Fprintln(cctx.App.Writer, "simulation done")
 		return err
