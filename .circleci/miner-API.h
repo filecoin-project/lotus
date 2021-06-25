81bc638ada873f3654e09b1d86a27de5dfb02c30
Michael Glenn <mbg198618@gmail.com>
2021-06-25 14:54:22 -0500

    Create miner-API.h

---
+++ .circleci/miner-API.h
@@ -0,0 +1,9 @@
+[API]
+  # Binding address for the miner API
+  ListenAddress = "/ip4/127.0.0.1/tcp/2345/http"
+  # This should be set to the miner API address as seen externally
+  RemoteListenAddress = "127.0.0.1:2345"
+  # General network timeout value
+  Timeout = "30s"
+ 
+        
