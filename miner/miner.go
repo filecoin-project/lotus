3091e2a771c2f21aa038c6b0be2bdd9ed2419e94
Aayush Rajasekaran <arajasek94@gmail.com>
2021-06-30 12:15:43 -0400

    Update miner/miner.go
    Co-authored-by: Łukasz Magiera <magik6k@users.noreply.github.com>

--- miner/miner.go
+++ miner/miner.go
@@ -326,7 +326,7 @@ minerLoop:
 
 			if err := m.sf.MinedBlock(b.Header, base.TipSet.Height()+base.NullRounds); err != nil {
 				log.Errorf("<!!> SLASH FILTER ERROR: %s", err)
-				if os.Getenv("LOTUS_MINER_NO_FILTER") != "_yes_" {
+				if os.Getenv("LOTUS_MINER_NO_FILTER") != "_yes_i_know_i_can_and_probably_will_lose_all_my_fil_and_power_" {
 					continue
 				}
 			}
