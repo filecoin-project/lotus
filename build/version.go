29763df433a75f00d6b7be296603c9f30510d156
Łukasz Magiera <magik6k@gmail.com>
2021-07-02 20:56:09 +0200

    set version to v1.10.1-rc1

--- build/version.go
+++ build/version.go
@@ -29,7 +29,7 @@ func buildType() string {
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.10.1-dev"
+const BuildVersion = "1.10.1-rc1"
 
 func UserVersion() string {
 	return BuildVersion + buildType() + CurrentCommit
