5f7ae1f4891038fa38e7ad1779314d7e71dfaa6a
vyzo <vyzo@hackzen.org>
2021-07-04 09:55:12 +0300

    update splistore DI constructor

--- node/modules/blockstore.go
+++ node/modules/blockstore.go
@@ -78,10 +78,9 @@ func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.Locked
 		}
 
 		cfg := &splitstore.Config{
-			TrackingStoreType:  cfg.Splitstore.TrackingStoreType,
-			MarkSetType:        cfg.Splitstore.MarkSetType,
-			HotHeaders:         cfg.Splitstore.HotHeaders || cfg.Splitstore.ColdStoreType == "noop",
-			SkipMoveColdBlocks: cfg.Splitstore.ColdStoreType == "noop",
+			TrackingStoreType: cfg.Splitstore.TrackingStoreType,
+			MarkSetType:       cfg.Splitstore.MarkSetType,
+			DiscardColdBlocks: cfg.Splitstore.ColdStoreType == "noop",
 		}
 		ss, err := splitstore.Open(path, ds, hot, cold, cfg)
 		if err != nil {
