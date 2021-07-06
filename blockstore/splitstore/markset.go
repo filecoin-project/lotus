fdff1bebc91630c11cf0efe9df022a72fd5044ec
vyzo <vyzo@hackzen.org>
2021-07-06 14:44:40 +0300

    move map markset implementation to its own file

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -2,7 +2,6 @@ package splitstore
 
 import (
 	"path/filepath"
-	"sync"
 
 	"golang.org/x/xerrors"
 
@@ -12,7 +11,7 @@ import (
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
@@ -29,7 +28,7 @@ type MarkSetEnv interface {
 
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	switch mtype {
-	case "", "bloom":
+	case "bloom":
 		return NewBloomMarkSetEnv(false)
 	case "bloomts":
 		return NewBloomMarkSetEnv(true)
@@ -42,58 +41,4 @@ func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
-}
-
-type MapMarkSetEnv struct {
-	ts bool
-}
-
-var _ MarkSetEnv = (*MapMarkSetEnv)(nil)
-
-type MapMarkSet struct {
-	mx  sync.Mutex
-	set map[string]struct{}
-
-	ts bool
-}
-
-var _ MarkSet = (*MapMarkSet)(nil)
-
-func NewMapMarkSetEnv(ts bool) (*MapMarkSetEnv, error) {
-	return &MapMarkSetEnv{ts: ts}, nil
-}
-
-func (e *MapMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
-	return &MapMarkSet{
-		set: make(map[string]struct{}, sizeHint),
-		ts:  e.ts,
-	}, nil
-}
-
-func (e *MapMarkSetEnv) Close() error {
-	return nil
-}
-
-func (s *MapMarkSet) Mark(cid cid.Cid) error {
-	if s.ts {
-		s.mx.Lock()
-		defer s.mx.Unlock()
-	}
-
-	s.set[string(cid.Hash())] = struct{}{}
-	return nil
-}
-
-func (s *MapMarkSet) Has(cid cid.Cid) (bool, error) {
-	if s.ts {
-		s.mx.Lock()
-		defer s.mx.Unlock()
-	}
-
-	_, ok := s.set[string(cid.Hash())]
-	return ok, nil
-}
-
-func (s *MapMarkSet) Close() error {
-	return nil
 }
---
+++ blockstore/splitstore/markset_map.go
@@ -0,0 +1,61 @@
+package splitstore
+
+import (
+	"sync"
+
+	cid "github.com/ipfs/go-cid"
+)
+
+type MapMarkSetEnv struct {
+	ts bool
+}
+
+var _ MarkSetEnv = (*MapMarkSetEnv)(nil)
+
+type MapMarkSet struct {
+	mx  sync.Mutex
+	set map[string]struct{}
+
+	ts bool
+}
+
+var _ MarkSet = (*MapMarkSet)(nil)
+
+func NewMapMarkSetEnv(ts bool) (*MapMarkSetEnv, error) {
+	return &MapMarkSetEnv{ts: ts}, nil
+}
+
+func (e *MapMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
+	return &MapMarkSet{
+		set: make(map[string]struct{}, sizeHint),
+		ts:  e.ts,
+	}, nil
+}
+
+func (e *MapMarkSetEnv) Close() error {
+	return nil
+}
+
+func (s *MapMarkSet) Mark(cid cid.Cid) error {
+	if s.ts {
+		s.mx.Lock()
+		defer s.mx.Unlock()
+	}
+
+	s.set[string(cid.Hash())] = struct{}{}
+	return nil
+}
+
+func (s *MapMarkSet) Has(cid cid.Cid) (bool, error) {
+	if s.ts {
+		s.mx.Lock()
+		defer s.mx.Unlock()
+	}
+
+	_, ok := s.set[string(cid.Hash())]
+	return ok, nil
+}
+
+func (s *MapMarkSet) Close() error {
+	return nil
+}
