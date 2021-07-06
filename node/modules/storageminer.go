898687ea2ef91b6e3123d36c95693337a8628394
Anton Evangelatov <anton.evangelatov@gmail.com>
2021-07-06 17:28:52 +0200

    Update node/modules/storageminer.go
    Co-authored-by: Łukasz Magiera <magik6k@users.noreply.github.com>

--- node/modules/storageminer.go
+++ node/modules/storageminer.go
@@ -209,8 +209,6 @@ type StorageMinerParams struct {
 }
 
 func StorageMiner(fc config.MinerFeeConfig) func(params StorageMinerParams) (*storage.Miner, error) {
-	fmt.Printf("setting up storage miner with %#v \n", fc)
-
 	return func(params StorageMinerParams) (*storage.Miner, error) {
 		var (
 			ds     = params.MetadataDS
