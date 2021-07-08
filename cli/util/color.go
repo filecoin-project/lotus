4dd59b566ced9712a3af3e8accb6aaa612ab058b
Peter Rabbitson <ribasushi@protocol.ai>
2021-07-08 10:44:37 +0200

    Respect GOLOG_LOG_FMT=color

--- cli/util/color.go
+++ cli/util/color.go
@@ -6,4 +6,9 @@ import (
 	"github.com/mattn/go-isatty"
 )
 
-var DefaultColorUse = isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
+// DefaultColorUse is the globally referenced variable for all Lotus CLI tools
+// It sets to `true` if STDOUT is a TTY or if the variable GOLOG_LOG_FMT is set
+// to color (as recognizd by github.com/ipfs/go-log/v2)
+var DefaultColorUse = os.Getenv("GOLOG_LOG_FMT") == "color" ||
+	isatty.IsTerminal(os.Stdout.Fd()) ||
+	isatty.IsCygwinTerminal(os.Stdout.Fd())
