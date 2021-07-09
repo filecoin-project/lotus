6916566ca3bd7396aa6bb136a68b3b6e86f42e89
Dirk McCormick <dirkmdev@gmail.com>
2021-07-09 14:47:12 +0200

    test: TestDealCycleProviderRestart

--- itests/deals_concurrent_test.go
+++ itests/deals_concurrent_test.go
@@ -3,12 +3,16 @@ package itests
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 	"sync"
 	"testing"
 	"time"
 
+	ds "github.com/ipfs/go-datastore"
+	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/require"
 
+	"github.com/filecoin-project/dagstore"
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -105,6 +109,79 @@ func TestDealCyclesConcurrent(t *testing.T) {
 		t.Run(ns+"-stdretrieval-CAR", func(t *testing.T) { runTest(t, n, false, true) })
 		t.Run(ns+"-stdretrieval-NoCAR", func(t *testing.T) { runTest(t, n, false, false) })
 	}
+}
+
+// TestDealCycleProviderRestart verifies that
+// - the client makes a deal with the storage provider
+// - the storage provider restarts
+// - the client can make a retrieval deal with the storage provider
+func TestDealCycleProviderRestart(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	kit.QuietMiningLogs()
+
+	blockTime := 10 * time.Millisecond
+
+	// For these tests where the block time is artificially short, just use
+	// a deal start epoch that is guaranteed to be far enough in the future
+	// so that the deal starts sealing in time
+	startEpoch := abi.ChainEpoch(2 << 12)
+
+	// To simulate restarting the provider, we create two storage providers,
+	// and share a DAG store between them.
+	// We make a storage deal against the first provider, then start the second
+	// provider with the same DAG store and make a retrieval deal aaginst the
+	// second provider.
+	var (
+		client    kit.TestFullNode
+		provider1 kit.TestMiner
+		provider2 kit.TestMiner
+	)
+
+	// Use a single DAG store shared between miner1 and miner2
+	dagStoreDir := t.TempDir()
+	dagStoreDS := ds_sync.MutexWrap(ds.NewMapDatastore())
+	dagStore, err := dagstore.NewDAGStore(dagstore.Config{
+		TransientsDir: filepath.Join(dagStoreDir, "transients"),
+		IndexDir:      filepath.Join(dagStoreDir, "index"),
+		Datastore:     dagStoreDS,
+	})
+	require.NoError(t, err)
+	consOpts := kit.ConstructorOpts(
+		node.Override(new(*dagstore.DAGStore), dagStore),
+	)
+
+	// Set up the first provider
+	opts := []kit.NodeOpt{kit.WithAllSubsystems(), consOpts}
+	ens := kit.NewEnsemble(t, kit.MockProofs()).
+		FullNode(&client, opts...).
+		Miner(&provider1, &client, opts...).
+		Start()
+
+	ens.InterconnectAll().BeginMining(blockTime, &provider1)
+	dh := kit.NewDealHarness(t, &client, &provider1, &provider1)
+
+	// Make a storage deal against the first provider
+	deal, res, inPath := dh.MakeOnlineDeal(context.Background(), kit.MakeFullDealParams{
+		Rseed:      5,
+		StartEpoch: startEpoch,
+	})
+
+	// Set up the second provider
+	provider2Opts := append(opts,
+		kit.OwnerAddr(client.DefaultKey),
+		kit.MainMiner(&provider1),
+	)
+	ens.Miner(&provider2, &client, provider2Opts...).
+		Start().
+		Connect(provider2, client)
+
+	// Make a retrieval deal against the second provider
+	dh2 := kit.NewDealHarness(t, &client, &provider2, &provider2)
+	outPath := dh2.PerformRetrieval(context.Background(), deal, res.Root, false)
+	kit.AssertFilesEqual(t, inPath, outPath)
 }
 
 func TestSimultenousTransferLimit(t *testing.T) {
