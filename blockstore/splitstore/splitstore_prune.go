f3151aa564f1f290c3c98e54f58ee2052bb08d9c
vyzo <vyzo@hackzen.org>
2021-07-15 18:03:49 +0300

    drop the sort, takes forever

--- blockstore/splitstore/splitstore_prune.go
+++ blockstore/splitstore/splitstore_prune.go
@@ -273,21 +273,10 @@ func (s *SplitStore) doPrune(curTs *types.TipSet, retainStateP func(int64) bool,
 		return err
 	}
 
-	// 3. sort dead objects so that the dags with most references are deleted first
-	//    this ensures that we can't refer to a dag with its consituents already deleted, ie
-	//    we lave no dangling references.
-	log.Info("sorting dead objects")
-	startSort := time.Now()
-	err = s.sortObjects(dead)
-	if err != nil {
-		return xerrors.Errorf("error sorting objects: %w", err)
-	}
-	log.Infow("sorting done", "took", time.Since(startSort))
-
-	// 3.1 protect transactional refs once more
-	//     strictly speaking, this is not necessary as purge will do it before deleting each
-	//     batch.  however, there is likely a largish number of references accumulated during the sort
-	//     and this protects before entering purge context
+	// 3 protect transactional refs once more
+	//    strictly speaking, this is not necessary as purge will do it before deleting each
+	//    batch.  however, there is likely a number of references and this protects before
+	//    entering purge context
 	err = s.protectTxnRefs(markSet)
 	if err != nil {
 		return xerrors.Errorf("error protecting transactional refs: %w", err)
