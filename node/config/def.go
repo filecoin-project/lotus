6a244ee8f0fc2c5846ecb70c2c3d93f706237ab6
Raúl Kripalani <raul@protocol.ai>
2021-08-04 12:22:23 +0100

    clarify comment.

--- node/config/def.go
+++ node/config/def.go
@@ -189,9 +189,10 @@ func DefaultStorageMiner() *StorageMiner {
 			DealPublishControl: []string{},
 		},
 
+		// The default DAGStoreConfig doesn't define any paths for transients,
+		// indices and the datastore. Empty values will lead to these being
+		// placed under <repo>/dagStore.
 		DAGStore: DAGStoreConfig{
-			// zero value paths for transients, indices, datastore.
-			// by default they'll end up under the node repo.
 			MaxConcurrentIndex:         5,
 			MaxConcurrentReadyFetches:  2,
 			MaxConcurrencyStorageCalls: 100,
