5006d0a4c53d260eafb50bfba67fc45d8198e271
huhuafupop <huhuapop@outlook.com>
2021-08-14 19:11:04 -0400

    update version

--- build/version.go
+++ build/version.go
@@ -40,7 +40,7 @@ func buildType() string {
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.2-dev"
+const BuildVersion = "1.11.2-dev-huafu"
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
