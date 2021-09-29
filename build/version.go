7c015840a6c4f208fe1d51a49d60130c070192e6
Jennifer Wang <jiayingw703@gmail.com>
2021-09-29 02:42:38 -0400

    v1.11.3 release prep

--- build/version.go
+++ build/version.go
@@ -40,7 +40,7 @@ func buildType() string {
 }
 
 // BuildVersion is the local build version
-const BuildVersion = "1.11.3-rc2"
+const BuildVersion = "1.11.3"
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
