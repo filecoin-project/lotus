135186e33df46039cfb39eb1b69791e05d82bc54
emad22552 <emad22552@gmail.com>
2022-01-02 19:08:22 +0330

    go fmt ./client.go

--- cli/client.go
+++ cli/client.go
@@ -356,9 +356,9 @@ The minimum value is 518400 (6 months).`,
 		&CidBaseFlag,
 	},
 	Action: func(cctx *cli.Context) error {
-		
+
 		expectedArgsMsg := "expected 4 args: dataCid, miner, price, duration"
-		
+
 		if !cctx.Args().Present() {
 			if cctx.Bool("manual-stateless-deal") {
 				return xerrors.New("--manual-stateless-deal can not be combined with interactive deal mode: you must specify the " + expectedArgsMsg)
