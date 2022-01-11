d665416829726d95bbeee35e1fc46b5f3688defb
Jennifer Wang <jiayingw703@gmail.com>
2022-01-10 23:24:37 -0500

    bump the version to v1.14.0-rc1

--- build/version.go
+++ build/version.go
@@ -37,7 +37,7 @@ func BuildTypeString() string {
 }
 
 // BuildVersion is the local build version
-const BuildVersion = "1.13.3-dev"
+const BuildVersion = "1.14.0-rc1"
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
