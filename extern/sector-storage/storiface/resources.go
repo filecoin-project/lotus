60c9b614105a73cf36af337b47450193bc7cd954
Aayush Rajasekaran <arajasek94@gmail.com>
2022-01-18 10:39:02 -0500

    feat: sealing: Add ReplicaUpdate work to Resource table

--- extern/sector-storage/storiface/resources.go
+++ extern/sector-storage/storiface/resources.go
@@ -331,10 +331,146 @@ var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources
 			BaseMinMemory: 0,
 		},
 	},
+	// TODO: this should ideally be the actual replica update proof types
+	// TODO: actually measure this (and all the other replica update work)
+	sealtasks.TTReplicaUpdate: { // copied from addpiece
+		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
+			MaxMemory: 8 << 30,
+			MinMemory: 8 << 30,
+
+			MaxParallelism: 1,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
+			MaxMemory: 4 << 30,
+			MinMemory: 4 << 30,
+
+			MaxParallelism: 1,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
+			MaxMemory: 1 << 30,
+			MinMemory: 1 << 30,
+
+			MaxParallelism: 1,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory: 2 << 10,
+			MinMemory: 2 << 10,
+
+			MaxParallelism: 1,
+
+			BaseMinMemory: 2 << 10,
+		},
+		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
+			MaxMemory: 8 << 20,
+			MinMemory: 8 << 20,
+
+			MaxParallelism: 1,
+
+			BaseMinMemory: 8 << 20,
+		},
+	},
+	sealtasks.TTProveReplicaUpdate1: { // copied from commit1
+		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
+			MaxMemory: 1 << 30,
+			MinMemory: 1 << 30,
+
+			MaxParallelism: 0,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
+			MaxMemory: 1 << 30,
+			MinMemory: 1 << 30,
+
+			MaxParallelism: 0,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
+			MaxMemory: 1 << 30,
+			MinMemory: 1 << 30,
+
+			MaxParallelism: 0,
+
+			BaseMinMemory: 1 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory: 2 << 10,
+			MinMemory: 2 << 10,
+
+			MaxParallelism: 0,
+
+			BaseMinMemory: 2 << 10,
+		},
+		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
+			MaxMemory: 8 << 20,
+			MinMemory: 8 << 20,
+
+			MaxParallelism: 0,
+
+			BaseMinMemory: 8 << 20,
+		},
+	},
+	sealtasks.TTProveReplicaUpdate2: { // copied from commit2
+		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
+			MaxMemory: 190 << 30, // TODO: Confirm
+			MinMemory: 60 << 30,
+
+			MaxParallelism:    -1,
+			MaxParallelismGPU: 6,
+			GPUUtilization:    1.0,
+
+			BaseMinMemory: 64 << 30, // params
+		},
+		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
+			MaxMemory: 150 << 30, // TODO: ~30G of this should really be BaseMaxMemory
+			MinMemory: 30 << 30,
+
+			MaxParallelism:    -1,
+			MaxParallelismGPU: 6,
+			GPUUtilization:    1.0,
+
+			BaseMinMemory: 32 << 30, // params
+		},
+		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
+			MaxMemory: 3 << 29, // 1.5G
+			MinMemory: 1 << 30,
+
+			MaxParallelism: 1, // This is fine
+			GPUUtilization: 1.0,
+
+			BaseMinMemory: 10 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
+			MaxMemory: 2 << 10,
+			MinMemory: 2 << 10,
+
+			MaxParallelism: 1,
+			GPUUtilization: 1.0,
+
+			BaseMinMemory: 2 << 10,
+		},
+		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
+			MaxMemory: 8 << 20,
+			MinMemory: 8 << 20,
+
+			MaxParallelism: 1,
+			GPUUtilization: 1.0,
+
+			BaseMinMemory: 8 << 20,
+		},
+	},
 }
 
 func init() {
 	ResourceTable[sealtasks.TTUnseal] = ResourceTable[sealtasks.TTPreCommit1] // TODO: measure accurately
+	ResourceTable[sealtasks.TTRegenSectorKey] = ResourceTable[sealtasks.TTReplicaUpdate]
 
 	// V1_1 is the same as V1
 	for _, m := range ResourceTable {
