33ca5d113adef67f9e3bd3d3228152b315016923
Nikola Divic <divicnikola@gmail.com>
2022-01-18 17:56:03 +0100

    doc: stm annotations for various test files

--- paychmgr/paychget_test.go
+++ paychmgr/paychget_test.go
@@ -43,6 +43,7 @@ func testChannelResponse(t *testing.T, ch address.Address) types.MessageReceipt 
 // TestPaychGetCreateChannelMsg tests that GetPaych sends a message to create
 // a new channel with the correct funds
 func TestPaychGetCreateChannelMsg(t *testing.T) {
+	//stm: @TOKEN_PAYCH_CREATE_001
 	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
