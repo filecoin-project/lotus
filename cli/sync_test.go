ec0607900da136a481f64f72aabd4c2f5d17beee
Nikola Divic <divicnikola@gmail.com>
2022-02-11 17:45:19 +0100

    test: sync state cli command

---
+++ cli/sync_test.go
@@ -0,0 +1,56 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/types/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSyncStatus(t *testing.T) {
+	app, mockApi, buf, done := NewMockAppWithFullAPI(t, WithCategory("sync", SyncStatusCmd))
+	defer done()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ts1 := mock.TipSet(mock.MkBlock(nil, 0, 0))
+	ts2 := mock.TipSet(mock.MkBlock(ts1, 0, 0))
+
+	start := time.Now()
+	end := start.Add(time.Minute)
+
+	state := &api.SyncState{
+		ActiveSyncs: []api.ActiveSync{{
+			WorkerID: 1,
+			Base:     ts1,
+			Target:   ts2,
+			Stage:    api.StageMessages,
+			Height:   abi.ChainEpoch(0),
+			Start:    start,
+			End:      end,
+			Message:  "whatever",
+		}},
+		VMApplied: 0,
+	}
+
+	mockApi.EXPECT().SyncState(ctx).Return(state, nil)
+
+	err := app.Run([]string{"sync", "status"})
+	assert.NoError(t, err)
+
+	out := buf.String()
+
+	// output is plaintext, had to do string matching
+	assert.Contains(t, out, fmt.Sprintf("Base:\t[%s]", ts1.Blocks()[0].Cid().String()))
+	assert.Contains(t, out, fmt.Sprintf("Target:\t[%s]", ts2.Blocks()[0].Cid().String()))
+	assert.Contains(t, out, "Height diff:\t1")
+	assert.Contains(t, out, "Stage: message sync")
+	assert.Contains(t, out, "Height: 0")
+	assert.Contains(t, out, "Elapsed: 1m0s")
+}
