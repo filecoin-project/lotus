4c25358a0846c94cddb4100a2f91b98947d0209a
Łukasz Magiera <magik6k@gmail.com>
2022-03-17 21:13:35 +0100

    itests: Test Sealing.MakeCCSectorsAvailable

--- itests/ccupgrade_test.go
+++ itests/ccupgrade_test.go
@@ -191,6 +191,64 @@ func TestAbortUpgradeAvailable(t *testing.T) {
 	}
 }
 
+func TestMakeAvailable(t *testing.T) {
+	kit.QuietMiningLogs()
+
+	ctx := context.Background()
+	blockTime := 1 * time.Millisecond
+
+	client, miner, ens := kit.EnsembleMinimal(t, kit.GenesisNetworkVersion(network.Version15), kit.ThroughRPC(), kit.MutateSealingConfig(func(sc *config.SealingConfig) {
+		sc.MakeCCSectorsAvailable = true
+	}))
+	ens.InterconnectAll().BeginMiningMustPost(blockTime)
+
+	maddr, err := miner.ActorAddress(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	CCUpgrade := abi.SectorNumber(kit.DefaultPresealsPerBootstrapMiner + 1)
+
+	miner.PledgeSectors(ctx, 1, 0, nil)
+	sl, err := miner.SectorsList(ctx)
+	require.NoError(t, err)
+	require.Len(t, sl, 1, "expected 1 sector")
+	require.Equal(t, CCUpgrade, sl[0], "unexpected sector number")
+	{
+		si, err := client.StateSectorGetInfo(ctx, maddr, CCUpgrade, types.EmptyTSK)
+		require.NoError(t, err)
+		require.Less(t, 50000, int(si.Expiration))
+	}
+	waitForSectorActive(ctx, t, CCUpgrade, client, maddr)
+
+	sl, err = miner.SectorsList(ctx)
+	require.NoError(t, err)
+	require.Len(t, sl, 1, "expected 1 sector")
+
+	status, err := miner.SectorsStatus(ctx, CCUpgrade, true)
+	require.NoError(t, err)
+	assert.Equal(t, api.SectorState(sealing.Available), status.State)
+
+	dh := kit.NewDealHarness(t, client, miner, miner)
+	deal, res, inPath := dh.MakeOnlineDeal(ctx, kit.MakeFullDealParams{
+		Rseed:                        6,
+		SuspendUntilCryptoeconStable: true,
+	})
+	outPath := dh.PerformRetrieval(context.Background(), deal, res.Root, false)
+	kit.AssertFilesEqual(t, inPath, outPath)
+
+	sl, err = miner.SectorsList(ctx)
+	require.NoError(t, err)
+	require.Len(t, sl, 1, "expected 1 sector")
+
+	status, err = miner.SectorsStatus(ctx, CCUpgrade, true)
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(status.Deals))
+	miner.WaitSectorsProving(ctx, map[abi.SectorNumber]struct{}{
+		CCUpgrade: {},
+	})
+}
+
 func TestPreferNoUpgrade(t *testing.T) {
 	kit.QuietMiningLogs()
 
