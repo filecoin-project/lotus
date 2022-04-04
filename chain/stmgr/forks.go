a9ae38a1cedf1f3a9a91e1b47b14ea48052a4fb3
vyzo <vyzo@hackzen.org>
2022-04-04 12:03:48 +0300

    wire in nv16 migration signature and manifests

--- chain/stmgr/forks.go
+++ chain/stmgr/forks.go
@@ -8,9 +8,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
-
-	"github.com/filecoin-project/specs-actors/v7/actors/migration/nv15"
+	"github.com/filecoin-project/specs-actors/v8/actors/migration/nv16"
 
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
@@ -49,7 +47,9 @@ type MigrationCache interface {
 type MigrationFunc func(
 	ctx context.Context,
 	sm *StateManager, cache MigrationCache,
-	cb ExecMonitor, oldState cid.Cid,
+	cb ExecMonitor,
+	manifest cid.Cid,
+	oldState cid.Cid,
 	height abi.ChainEpoch, ts *types.TipSet,
 ) (newState cid.Cid, err error)
 
@@ -58,6 +58,7 @@ type MigrationFunc func(
 type PreMigrationFunc func(
 	ctx context.Context,
 	sm *StateManager, cache MigrationCache,
+	manifest cid.Cid,
 	oldState cid.Cid,
 	height abi.ChainEpoch, ts *types.TipSet,
 ) error
@@ -94,6 +95,10 @@ type Upgrade struct {
 	// These functions should fill the given cache with information that can speed up the
 	// eventual full migration at the upgrade epoch.
 	PreMigrations []PreMigration
+
+	// Manifest is the migration manifest CID, starting with nv16; for older migrations it is
+	// undefined
+	Manifest cid.Cid
 }
 
 type UpgradeSchedule []Upgrade
@@ -177,7 +182,7 @@ func (sm *StateManager) HandleStateForks(ctx context.Context, root cid.Cid, heig
 		// Yes, we clone the cache, even for the final upgrade epoch. Why? Reverts. We may
 		// have to migrate multiple times.
 		tmpCache := u.cache.Clone()
-		retCid, err = u.upgrade(ctx, sm, tmpCache, cb, root, height, ts)
+		retCid, err = u.upgrade(ctx, sm, tmpCache, cb, u.manifest, root, height, ts)
 		if err != nil {
 			log.Errorw("FAILED migration", "height", height, "from", root, "error", err)
 			return cid.Undef, err
@@ -214,7 +219,7 @@ func (sm *StateManager) hasExpensiveFork(height abi.ChainEpoch) bool {
 	return ok
 }
 
-func runPreMigration(ctx context.Context, sm *StateManager, fn PreMigrationFunc, cache *nv15.MemMigrationCache, ts *types.TipSet) {
+func runPreMigration(ctx context.Context, sm *StateManager, manifest cid.Cid, fn PreMigrationFunc, cache *nv16.MemMigrationCache, ts *types.TipSet) {
 	height := ts.Height()
 	parent := ts.ParentState()
 
@@ -226,7 +231,7 @@ func runPreMigration(ctx context.Context, sm *StateManager, fn PreMigrationFunc,
 	// migration to use the cache may assume that
 	// certain blocks exist, even if they don't.
 	tmpCache := cache.Clone()
-	err := fn(ctx, sm, tmpCache, parent, height, ts)
+	err := fn(ctx, sm, tmpCache, manifest, parent, height, ts)
 	if err != nil {
 		log.Errorw("FAILED pre-migration", "error", err)
 		return
@@ -277,7 +282,7 @@ func (sm *StateManager) preMigrationWorker(ctx context.Context) {
 					wg.Add(1)
 					go func() {
 						defer wg.Done()
-						runPreMigration(preCtx, sm, migrationFunc, cache, ts)
+						runPreMigration(preCtx, sm, migration.manifest, migrationFunc, cache, ts)
 					}()
 				},
 			})
