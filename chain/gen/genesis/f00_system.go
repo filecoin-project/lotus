d9b1d9cb35b6a44213d2d6d12d8810a01c6f13e5
vyzo <vyzo@hackzen.org>
2022-04-08 00:56:07 +0300

    initialize system actor for nv16 at genesis

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -3,10 +3,15 @@ package genesis
 import (
 	"context"
 
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/system"
+
+	"github.com/filecoin-project/specs-actors/v8/actors/builtin/manifest"
+	system8 "github.com/filecoin-project/specs-actors/v8/actors/builtin/system"
 
 	cbor "github.com/ipfs/go-ipld-cbor"
 
@@ -20,6 +25,21 @@ func SetupSystemActor(ctx context.Context, bs bstore.Blockstore, av actors.Versi
 	st, err := system.MakeState(adt.WrapStore(ctx, cst), av)
 	if err != nil {
 		return nil, err
+	}
+
+	if av >= actors.Version8 {
+		mfCid, ok := actors.GetManifest(av)
+		if !ok {
+			return nil, xerrors.Errorf("missing manifest for actors version %d", av)
+		}
+
+		mf := manifest.Manifest{}
+		if err := cst.Get(ctx, mfCid, &mf); err != nil {
+			return nil, xerrors.Errorf("loading manifest for actors version %d: %w", av, err)
+		}
+
+		st8 := st.GetState().(*system8.State)
+		st8.BuiltinActors = mf.Data
 	}
 
 	statecid, err := cst.Put(ctx, st.GetState())
