4157eebccb681d70dfddb6eec34c637b0017c066
vyzo <vyzo@hackzen.org>
2022-04-08 14:15:32 +0300

    fix lint

--- chain/stmgr/forks.go
+++ chain/stmgr/forks.go
@@ -271,6 +271,7 @@ func (sm *StateManager) preMigrationWorker(ctx context.Context) {
 				notAfterEpoch = stopEpoch - 1
 			}
 
+			manifest := migration.manifest
 			// Add an op to start a pre-migration.
 			schedule = append(schedule, op{
 				after:    afterEpoch,
@@ -281,7 +282,7 @@ func (sm *StateManager) preMigrationWorker(ctx context.Context) {
 					wg.Add(1)
 					go func() {
 						defer wg.Done()
-						runPreMigration(preCtx, sm, migration.manifest, migrationFunc, cache, ts)
+						runPreMigration(preCtx, sm, manifest, migrationFunc, cache, ts)
 					}()
 				},
 			})
