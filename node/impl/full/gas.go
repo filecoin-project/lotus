6319a4daa088cd13cbb9d8ccb3d0fe9cad7fcf29
vyzo <vyzo@hackzen.org>
2022-04-13 01:53:41 +0300

    some hacks to make things work

--- node/impl/full/gas.go
+++ node/impl/full/gas.go
@@ -381,7 +381,8 @@ func (m *GasModule) GasEstimateMessageGas(ctx context.Context, msg *types.Messag
 		msg.GasFeeCap = feeCap
 	}
 
-	messagepool.CapGasFee(m.GetMaxFee, msg, spec)
+	// XXX disable this to make things work.
+	//messagepool.CapGasFee(m.GetMaxFee, msg, spec)
 
 	return msg, nil
 }
