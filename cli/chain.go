4c074fd8ee95e574d4cee177c755f1b0ba845766
vyzo <vyzo@hackzen.org>
2022-04-13 11:09:15 +0300

    add chain exec command

--- cli/chain.go
+++ cli/chain.go
@@ -63,6 +63,7 @@ var ChainCmd = &cli.Command{
 		ChainEncodeCmd,
 		ChainDisputeSetCmd,
 		ChainInstallCmd,
+		ChainExecCmd,
 	},
 }
 
@@ -1556,6 +1557,106 @@ var ChainInstallCmd = &cli.Command{
 
 		afmt.Printf("Actor Code CID: %s\n", result.CodeCid)
 		afmt.Printf("Installed: %t\n", result.Installed)
+
+		return nil
+	},
+}
+
+var ChainExecCmd = &cli.Command{
+	Name:      "exec",
+	Usage:     "Execute a new actor and return its address",
+	ArgsUsage: "code-cid [params]",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "from",
+			Usage: "optionally specify the account to use for sending the exec message",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		afmt := NewAppFmt(cctx.App)
+
+		api, closer, err := GetFullNodeAPI(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+		ctx := ReqContext(cctx)
+
+		if argc := cctx.Args().Len(); argc < 1 || argc > 2 {
+			return xerrors.Errorf("must pass the code cid and (optionally) constructor params")
+		}
+
+		codeCid, err := cid.Decode(cctx.Args().First())
+		if err != nil {
+			return xerrors.Errorf("failed to decode code cid: %w", err)
+		}
+
+		var cparams []byte
+		if cctx.Args().Len() == 2 {
+			cparams, err = base64.StdEncoding.DecodeString(cctx.Args().Get(1))
+			if err != nil {
+				return xerrors.Errorf("decoding base64 value: %w", err)
+			}
+		}
+
+		params, err := actors.SerializeParams(&init8.ExecParams{
+			CodeCID:           codeCid,
+			ConstructorParams: cparams,
+		})
+		if err != nil {
+			return xerrors.Errorf("failed to serialize params: %w", err)
+		}
+
+		var fromAddr address.Address
+		if from := cctx.String("from"); from == "" {
+			defaddr, err := api.WalletDefaultAddress(ctx)
+			if err != nil {
+				return err
+			}
+
+			fromAddr = defaddr
+		} else {
+			addr, err := address.NewFromString(from)
+			if err != nil {
+				return err
+			}
+
+			fromAddr = addr
+		}
+
+		msg := &types.Message{
+			To:     builtin.InitActorAddr,
+			From:   fromAddr,
+			Value:  big.Zero(),
+			Method: 2,
+			Params: params,
+		}
+
+		afmt.Println("sending message...")
+		smsg, err := api.MpoolPushMessage(ctx, msg, nil)
+		if err != nil {
+			return xerrors.Errorf("failed to push message: %w", err)
+		}
+
+		afmt.Println("waiting for message to execute...")
+		wait, err := api.StateWaitMsg(ctx, smsg.Cid(), 0)
+		if err != nil {
+			return xerrors.Errorf("error waiting for message: %w", err)
+		}
+
+		// check it executed successfully
+		if wait.Receipt.ExitCode != 0 {
+			return xerrors.Errorf("actor execution failed")
+		}
+
+		var result init8.ExecReturn
+		r := bytes.NewReader(wait.Receipt.Return)
+		if err := result.UnmarshalCBOR(r); err != nil {
+			return xerrors.Errorf("error unmarshaling return value: %w", err)
+		}
+
+		afmt.Printf("ID Address: %s\n", result.IDAddress)
+		afmt.Printf("Robust Address: %s\n", result.RobustAddress)
 
 		return nil
 	},
