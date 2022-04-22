6f91ad77cf737b2eb2366713a7b3fa59abe7cfe6
Travis Person <travisperson@users.noreply.github.com>
2022-04-22 05:41:50 +0000

    fix: init network script
    In addition to updating the script so that it doesn't error due to incorrect
    flags, I have updated it to more closely reflect the process we do when
    setting up a new network. It's not a perfect model, but it's much closer
    now than it was before.

--- scripts/init-network.sh
+++ scripts/init-network.sh
@@ -6,43 +6,52 @@ NUM_SECTORS=2
 SECTOR_SIZE=2KiB
 
 
-sdt0111=$(mktemp -d)
-sdt0222=$(mktemp -d)
-sdt0333=$(mktemp -d)
+if ./lotus --version | grep -v debug; then
+  make debug
+fi
+
+sdt01000=$(mktemp -d)
+sdt01001=$(mktemp -d)
+sdt01002=$(mktemp -d)
 
 staging=$(mktemp -d)
 
-make debug
+./lotus-seed --sector-dir="${sdt01000}" pre-seal --fake-sectors --miner-addr=t01000 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS}
+./lotus-seed --sector-dir="${sdt01001}" pre-seal --fake-sectors --miner-addr=t01001 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS}
+./lotus-seed --sector-dir="${sdt01002}" pre-seal --fake-sectors --miner-addr=t01002 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS}
 
-./lotus-seed --sector-dir="${sdt0111}" pre-seal --miner-addr=t0111 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS} &
-./lotus-seed --sector-dir="${sdt0222}" pre-seal --miner-addr=t0222 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS} &
-./lotus-seed --sector-dir="${sdt0333}" pre-seal --miner-addr=t0333 --sector-offset=0 --sector-size=${SECTOR_SIZE} --num-sectors=${NUM_SECTORS} &
+./lotus-seed genesis new --network-name debugnet "${staging}/genesis.json"
+./lotus-seed genesis set-network-version "${staging}/genesis.json"
 
-wait
 
-./lotus-seed aggregate-manifests "${sdt0111}/pre-seal-t0111.json" "${sdt0222}/pre-seal-t0222.json" "${sdt0333}/pre-seal-t0333.json" > "${staging}/genesis.json"
+cat "${sdt01000}/pre-seal-t01000.json"
 
-lotus_path=$(mktemp -d)
 
-./lotus --repo="${lotus_path}" daemon --lotus-make-random-genesis="${staging}/devnet.car" --genesis-presealed-sectors="${staging}/genesis.json" --bootstrap=false &
-lpid=$!
+jq --arg MinerId "t01000" --arg VerifiedDeal "true" '
+    .[$MinerId].Sectors[] |= (.Deal.Label = .CommR."/")
+' < "${sdt01000}/pre-seal-t01000.json" > "${sdt01000}/pre-seal-t01000.json.updated" && mv "${sdt01000}/pre-seal-t01000.json.updated" "${sdt01000}/pre-seal-t01000.json"
 
-sleep 3
+jq --arg MinerId "t01001" --arg VerifiedDeal "true" '
+    .[$MinerId].Sectors[] |= (.Deal.Label = .CommR."/")
+' < "${sdt01000}/pre-seal-t01000.json" > "${sdt01000}/pre-seal-t01000.json.updated" && mv "${sdt01000}/pre-seal-t01000.json.updated" "${sdt01000}/pre-seal-t01000.json"
 
-kill "$lpid"
+jq --arg MinerId "t01002" --arg VerifiedDeal "true" '
+    .[$MinerId].Sectors[] |= (.Deal.Label = .CommR."/")
+' < "${sdt01000}/pre-seal-t01000.json" > "${sdt01000}/pre-seal-t01000.json.updated" && mv "${sdt01000}/pre-seal-t01000.json.updated" "${sdt01000}/pre-seal-t01000.json"
 
-wait
+./lotus-seed genesis add-miner "${staging}/genesis.json" "${sdt01000}/pre-seal-t01000.json"
+./lotus-seed genesis add-miner "${staging}/genesis.json" "${sdt01001}/pre-seal-t01001.json"
+./lotus-seed genesis add-miner "${staging}/genesis.json" "${sdt01002}/pre-seal-t01002.json"
 
+./lotus-seed genesis car --out "${staging}/devnet.car" "${staging}/genesis.json"
 cp "${staging}/devnet.car" build/genesis/devnet.car
 
-make debug
+ldt01000=$(mktemp -d)
+ldt01001=$(mktemp -d)
+ldt01002=$(mktemp -d)
 
-ldt0111=$(mktemp -d)
-ldt0222=$(mktemp -d)
-ldt0333=$(mktemp -d)
-
-sdlist=( "$sdt0111" "$sdt0222" "$sdt0333" )
-ldlist=( "$ldt0111" "$ldt0222" "$ldt0333" )
+sdlist=( "$sdt01000" "$sdt01001" "$sdt01002" )
+ldlist=( "$ldt01000" "$ldt01001" "$ldt01002" )
 
 for (( i=0; i<${#sdlist[@]}; i++ )); do
   preseal=${sdlist[$i]}
@@ -51,23 +60,15 @@ for (( i=0; i<${#sdlist[@]}; i++ )); do
   filename=${filefull%%.*}
   mineraddr=$(echo $filename | sed 's/pre-seal-//g')
 
-  wallet_raw=$(jq -rc ".${mineraddr}.Key" < ${preseal}/${filefull})
-  wallet_b16=$(./lotus-shed base16 "${wallet_raw}")
-  wallet_adr=$(./lotus-shed keyinfo --format="{{.Address}}" "${wallet_b16}")
-  wallet_adr_enc=$(./lotus-shed base32 "wallet-${wallet_adr}")
-
+  wallet_file="${preseal}/${filename}.key"
   mkdir -p "${ldlist[$i]}/keystore"
-  cat > "${ldlist[$i]}/keystore/${wallet_adr_enc}" <<EOF
-${wallet_raw}
-EOF
-
-  chmod 0700 "${ldlist[$i]}/keystore/${wallet_adr_enc}"
+  LOTUS_PATH="${ldlist[$i]}" ./lotus-shed keyinfo import "${wallet_file}"
 done
 
 pids=()
 for (( i=0; i<${#ldlist[@]}; i++ )); do
   repo=${ldlist[$i]}
-  ./lotus --repo="${repo}" daemon --api "3000$i" --bootstrap=false &
+  ./lotus --repo="${repo}" daemon --api "3000$i" --bootstrap=false --genesis build/genesis/devnet.car &
   pids+=($!)
 done
 
@@ -82,16 +83,16 @@ done
 
 sleep 3
 
-mdt0111=$(mktemp -d)
-mdt0222=$(mktemp -d)
-mdt0333=$(mktemp -d)
+mdt01000=$(mktemp -d)
+mdt01001=$(mktemp -d)
+mdt01002=$(mktemp -d)
 
-env LOTUS_PATH="${ldt0111}" LOTUS_MINER_PATH="${mdt0111}" ./lotus-miner init --genesis-miner --actor=t0111 --pre-sealed-sectors="${sdt0111}" --pre-sealed-metadata="${sdt0111}/pre-seal-t0111.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
-env LOTUS_PATH="${ldt0111}" LOTUS_MINER_PATH="${mdt0111}" ./lotus-miner run --nosync &
+env LOTUS_PATH="${ldt01000}" LOTUS_MINER_PATH="${mdt01000}" ./lotus-miner init --genesis-miner --actor=t01000 --pre-sealed-sectors="${sdt01000}" --pre-sealed-metadata="${sdt01000}/pre-seal-t01000.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
+env LOTUS_PATH="${ldt01000}" LOTUS_MINER_PATH="${mdt01000}" ./lotus-miner run --nosync &
 mpid=$!
 
-env LOTUS_PATH="${ldt0222}" LOTUS_MINER_PATH="${mdt0222}" ./lotus-miner init                 --actor=t0222 --pre-sealed-sectors="${sdt0222}" --pre-sealed-metadata="${sdt0222}/pre-seal-t0222.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
-env LOTUS_PATH="${ldt0333}" LOTUS_MINER_PATH="${mdt0333}" ./lotus-miner init                 --actor=t0333 --pre-sealed-sectors="${sdt0333}" --pre-sealed-metadata="${sdt0333}/pre-seal-t0333.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
+env LOTUS_PATH="${ldt01001}" LOTUS_MINER_PATH="${mdt01001}" ./lotus-miner init                 --actor=t01001 --pre-sealed-sectors="${sdt01001}" --pre-sealed-metadata="${sdt01001}/pre-seal-t01001.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
+env LOTUS_PATH="${ldt01002}" LOTUS_MINER_PATH="${mdt01002}" ./lotus-miner init                 --actor=t01002 --pre-sealed-sectors="${sdt01002}" --pre-sealed-metadata="${sdt01002}/pre-seal-t01002.json" --nosync=true --sector-size="${SECTOR_SIZE}" || true
 
 kill $mpid
 wait $mpid
@@ -102,16 +103,16 @@ done
 
 wait
 
-rm -rf $mdt0111
-rm -rf $mdt0222
-rm -rf $mdt0333
+rm -rf $mdt01000
+rm -rf $mdt01001
+rm -rf $mdt01002
 
-rm -rf $ldt0111
-rm -rf $ldt0222
-rm -rf $ldt0333
+rm -rf $ldt01000
+rm -rf $ldt01001
+rm -rf $ldt01002
 
-rm -rf $sdt0111
-rm -rf $sdt0222
-rm -rf $sdt0333
+rm -rf $sdt01000
+rm -rf $sdt01001
+rm -rf $sdt01002
 
 rm -rf $staging
