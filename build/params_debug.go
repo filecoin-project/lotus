6df782a153751a44d7c4738d30d9d6d4882ba41c
Cory Schwartz <cory@protocol.ai>
2022-04-25 22:27:23 -0700

    remove policy from debug

--- build/params_debug.go
+++ build/params_debug.go
@@ -3,16 +3,7 @@
 
 package build
 
-import (
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-)
-
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1, abi.RegisteredSealProof_StackedDrg8MiBV1)
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
-	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
 }
