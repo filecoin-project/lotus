a803021eef8ce75d94f04dd7a8c3c867271e7705
vyzo <vyzo@hackzen.org>
2022-05-12 16:48:31 +0300

    add TODO about the bundle load cost

--- cli/init.go
+++ cli/init.go
@@ -11,6 +11,8 @@ import (
 func init() {
 	// preload manifest so that we have the correct code CID inventory for cli since that doesn't
 	// go through CI
+	// TODO loading the bundle in every cli invocation adds some latency; we should figure out a way
+	//      to load actor CIDs without incurring this hit.
 	bs := blockstore.NewMemory()
 
 	if err := bundle.FetchAndLoadBundles(context.Background(), bs, build.BuiltinActorReleases); err != nil {
