6360107d53c8d018f753877be995372ec03a2a99
vyzo <vyzo@hackzen.org>
2022-05-16 21:48:12 +0300

    godocs for bundle struct

--- build/bundle.go
+++ build/bundle.go
@@ -16,13 +16,13 @@ type BundleSpec struct {
 }
 
 type Bundle struct {
-	// actors version in this bundle
+	// Version is the actors version in this bundle
 	Version actors.Version
-	// release id
+	// Release is the release id
 	Release string
-	// bundle path: optional, uses the appropriate release bundle if unset
+	// Path is the (optional) bundle path; uses the appropriate release bundle if unset
 	Path string
-	// development version; when set, in conjunction with path, it will always
-	// load the bundle to the blockstore
+	// Devlopment indicates whether this is a development version; when set, in conjunction with path,
+	// it will always load the bundle to the blockstore
 	Development bool
 }
