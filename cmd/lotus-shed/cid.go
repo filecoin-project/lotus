82e439172ead06e30c3a06f06be10bcb3c6135b2
Aayush <arajasek94@gmail.com>
2022-06-08 13:32:44 -0400

    Drop actor meta shimming for testing

--- cmd/lotus-shed/cid.go
+++ cmd/lotus-shed/cid.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/ipld/go-car"
-	"golang.org/x/net/context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
