c3ee957cc6fb8192a8e275e1df747c686d88fc48
Alfonso de la Rocha <adlrocha@tutamail.com>
2022-09-29 11:15:36 +0200

    rm signature fns from cns iface

--- chain/consensus/iface.go
+++ chain/consensus/iface.go
@@ -6,7 +6,6 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.opencensus.io/stats"
 
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
@@ -42,12 +41,6 @@ type Consensus interface {
 	// such as the ordering of transactions, the inclusion of consensus proofs, the signature
 	// of the block, etc.
 	CreateBlock(ctx context.Context, w api.Wallet, bt *api.BlockTemplate) (*types.FullBlock, error)
-
-	// SignBlock determines how should blocks be signed for them to be considered valid by the consensus
-	// engine.
-	SignBlock(ctx context.Context, w api.Wallet, addr address.Address, next *types.BlockHeader) error
-	// VerifyBlockSignature verifies the signature scheme implemented by the consensus algorithm.
-	VerifyBlockSignature(ctx context.Context, h *types.BlockHeader, addr address.Address) error
 }
 
 // RewardFunc parametrizes the logic for rewards when a message is executed.
