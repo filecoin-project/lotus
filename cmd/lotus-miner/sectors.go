70bb865f814be48d2d116fbc825c9bf93d54e5d9
Phi <orjan.roren@gmail.com>
2022-09-30 10:39:41 +0200

    fix: renew --only-cc with sectorfile
    Add the ability to extend/renew only CC-sectors with the `--only-cc` when using a sector-file.

--- cmd/lotus-miner/sectors.go
+++ cmd/lotus-miner/sectors.go
@@ -918,6 +918,9 @@ var sectorsRenewCmd = &cli.Command{
 				}
 
 				si, found := activeSectorsInfo[abi.SectorNumber(id)]
+				if len(si.DealIDs) > 0 && cctx.Bool("only-cc") {
+					continue
+				}
 				if !found {
 					return xerrors.Errorf("sector %d is not active", id)
 				}
