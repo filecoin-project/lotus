04a27ef6eb6bbd895e3381bb871b91a1ff758e60
Kevin Li <ychiaoli18@users.noreply.github.com>
2022-11-11 14:49:32 -0500

    api: ethrpc: add missing fields

--- api/docgen/docgen.go
+++ api/docgen/docgen.go
@@ -369,7 +369,7 @@ func init() {
 	addExample(&ethint)
 	ethaddr, _ := api.EthAddressFromHex("0x5CbEeCF99d3fDB3f25E309Cc264f240bb0664031")
 	addExample(&ethaddr)
-	ethhash, _ := api.EthHashFromCid(c)
+	ethhash, _ := api.NewEthHashFromCid(c)
 	addExample(&ethhash)
 
 	ethFeeHistoryReward := [][]api.EthBigInt{}
