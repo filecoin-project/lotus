348494d7c80e8fefa290637448554efd61f8088c
Steven Allen <steven@stebalien.com>
2022-11-17 06:55:40 -0800

    feat: actors: add the evm to the builtin actors shims (#9672)

--- chain/actors/agen/main.go
+++ chain/actors/agen/main.go
@@ -28,6 +28,7 @@ var actors = map[string][]int{
 	"reward":   lotusactors.Versions,
 	"verifreg": lotusactors.Versions,
 	"datacap":  lotusactors.Versions[8:],
+	"evm":      lotusactors.Versions[9:],
 }
 
 func main() {
