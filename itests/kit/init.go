d574d04075a2e57885e5bb5f84be2e708d7dba46
Alfonso de la Rocha <adlrocha@tutamail.com>
2022-12-13 13:03:42 +0100

    set small cb delivery delay for paych itests

--- itests/kit/init.go
+++ itests/kit/init.go
@@ -3,6 +3,7 @@ package kit
 import (
 	"fmt"
 	"os"
+	"time"
 
 	logging "github.com/ipfs/go-log/v2"
 
@@ -39,6 +40,12 @@ func init() {
 	policy.SetProviderCollateralSupplyTarget(big.NewInt(0), big.NewInt(100))
 
 	build.InsecurePoStValidation = true
+
+	// NOTE: If we want the payment channel itests to pass that use a
+	// block time of 5*millisecond, we need to set the consistent broadcast
+	// delay to something lower than that block time.
+	// todo: configure such a low delay only for paych tests.
+	build.CBDeliveryDelay = 2 * time.Millisecond
 
 	if err := os.Setenv("BELLMAN_NO_GPU", "1"); err != nil {
 		panic(fmt.Sprintf("failed to set BELLMAN_NO_GPU env variable: %s", err))
