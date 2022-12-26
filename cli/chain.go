d8e82602909a7d702e0e4bd7155b700b1c3134a6
rickiey <rickiey@qq.com>
2022-12-26 10:18:25 +0800

    fix:Strip out empty spaces and prefix around contract bytes

--- cli/chain.go
+++ cli/chain.go
@@ -1554,7 +1554,10 @@ var ChainExecEVMCmd = &cli.Command{
 			return xerrors.Errorf("failed to read contract: %w", err)
 		}
 		if cctx.Bool("hex") {
-			contract, err = hex.DecodeString(string(contract))
+			contractCode := strings.TrimSpace(string(contract))
+			contractCode = strings.TrimPrefix(contractCode, "0x")
+			contractCode = strings.TrimPrefix(contractCode, "0X")
+			contract, err = hex.DecodeString(contractCode)
 			if err != nil {
 				return xerrors.Errorf("failed to decode contract: %w", err)
 			}
