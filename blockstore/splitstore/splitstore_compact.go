6a1c01c68508d6c8c60153003eb09d2d983d1d30
zenground0 <ZenGround0@users.noreply.github.com>
2023-01-03 13:05:40 -0700

    Retain tipset reference

--- blockstore/splitstore/splitstore_compact.go
+++ blockstore/splitstore/splitstore_compact.go
@@ -905,6 +905,14 @@ func (s *SplitStore) walkChain(ts *types.TipSet, inclState, inclMsgs abi.ChainEp
 	walkCnt := new(int64)
 	scanCnt := new(int64)
 
+	tsRef := func(cids []cid.Cid) (cid.Cid, error) {
+		blk, err := types.NewTipSetKey(cids...).ToStorageBlock()
+		if err != nil {
+			return cid.Undef, err
+		}
+		return blk.Cid(), nil
+	}
+
 	stopWalk := func(_ cid.Cid) error { return errStopWalk }
 
 	walkBlock := func(c cid.Cid) error {
@@ -926,9 +934,17 @@ func (s *SplitStore) walkChain(ts *types.TipSet, inclState, inclMsgs abi.ChainEp
 		err = s.view(c, func(data []byte) error {
 			return hdr.UnmarshalCBOR(bytes.NewBuffer(data))
 		})
-
 		if err != nil {
 			return xerrors.Errorf("error unmarshaling block header (cid: %s): %w", c, err)
+		}
+
+		// tipset CID references are retained
+		if pRef, err := tsRef(hdr.Parents); err != nil {
+			return xerrors.Errorf("error computing cid reference to parent tipset")
+		} else {
+			if err := s.walkObjectIncomplete(pRef, visitor, fHot, stopWalk); err != nil {
+				return xerrors.Errorf("error walking parent tipset cid reference")
+			}
 		}
 
 		// message are retained if within the inclMsgs boundary
@@ -979,6 +995,15 @@ func (s *SplitStore) walkChain(ts *types.TipSet, inclState, inclMsgs abi.ChainEp
 		}
 
 		return nil
+	}
+
+	// retain ref to chain head
+	if hRef, err := tsRef(ts.Cids()); err != nil {
+		return xerrors.Errorf("error computing cid reference to parent tipset")
+	} else {
+		if err := s.walkObjectIncomplete(hRef, visitor, fHot, stopWalk); err != nil {
+			return xerrors.Errorf("error walking parent tipset cid reference")
+		}
 	}
 
 	for len(toWalk) > 0 {
