d6c216a97fb160c04011a9387bbc090e21965673
Shrenuj Bansal <shrenuj.bansal@protocol.ai>
2023-01-10 19:19:07 -0500

    make gen and fix lint as always

--- itests/eth_balance_test.go
+++ itests/eth_balance_test.go
@@ -2,14 +2,14 @@ package itests
 
 import (
 	"context"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/types/ethtypes"
@@ -86,12 +86,12 @@ func TestEthGetBalanceBuiltinActor(t *testing.T) {
 	fid, err := address.NewFromString("f05")
 	require.NoError(t, err)
 
-	kit.SendFunds(ctx, t, client, fid, abi.TokenAmount{big.NewInt(10).Int})
+	kit.SendFunds(ctx, t, client, fid, abi.TokenAmount{Int: big.NewInt(10).Int})
 
 	ethAddr, err := ethtypes.EthAddressFromFilecoinAddress(fid)
 	require.NoError(t, err)
 
 	ebal, err := client.EthGetBalance(ctx, ethAddr, "latest")
 	require.NoError(t, err)
-	require.Equal(t, ethtypes.EthBigInt{big.NewInt(10).Int}, ebal)
+	require.Equal(t, ethtypes.EthBigInt{Int: big.NewInt(10).Int}, ebal)
 }
