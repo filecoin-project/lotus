ee2fb1b0f6aeaae2f6c534c8c04c3ae8715e488d
Raúl Kripalani <raul@protocol.ai>
2023-01-12 17:24:05 +0000

    switch away.

--- node/impl/full/state.go
+++ node/impl/full/state.go
@@ -616,19 +616,17 @@ func (m *StateModule) StateWaitMsg(ctx context.Context, msg cid.Cid, confidence 
 
 		vmsg := cmsg.VMMessage()
 
-		t, err := stmgr.GetReturnType(ctx, m.StateManager, vmsg.To, vmsg.Method, ts)
-		if err != nil {
-			if errors.Is(err, stmgr.ErrMetadataNotFound) {
-				// This is not necessarily an error -- EVM methods (and in the future native actors) may
-				// return just bytes, and in the not so distant future we'll have native wasm actors
-				// that are by definition not in the registry.
-				// So in this case, log a debug message and retun the raw bytes.
-				log.Debugf("failed to get return type: %s", err)
-				returndec = recpt.Return
-			} else {
-				return nil, xerrors.Errorf("failed to get return type: %w", err)
-			}
-		} else {
+		switch t, err := stmgr.GetReturnType(ctx, m.StateManager, vmsg.To, vmsg.Method, ts); {
+		case errors.Is(err, stmgr.ErrMetadataNotFound):
+			// This is not necessarily an error -- EVM methods (and in the future native actors) may
+			// return just bytes, and in the not so distant future we'll have native wasm actors
+			// that are by definition not in the registry.
+			// So in this case, log a debug message and retun the raw bytes.
+			log.Debugf("failed to get return type: %s", err)
+			returndec = recpt.Return
+		case err != nil:
+			return nil, xerrors.Errorf("failed to get return type: %w", err)
+		default:
 			if err := t.UnmarshalCBOR(bytes.NewReader(recpt.Return)); err != nil {
 				return nil, err
 			}
