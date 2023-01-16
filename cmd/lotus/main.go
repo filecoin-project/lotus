fc82aab74b9f0187d38fb01890af287b0c0f6cc7
Richard Guan <richard.guan@protocol.ai>
2023-01-16 16:14:19 +1100

    add bool flag and gen documentation

--- cmd/lotus/main.go
+++ cmd/lotus/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os"
 
+	"github.com/fatih/color"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/mattn/go-isatty"
 	"github.com/urfave/cli/v2"
@@ -52,6 +53,10 @@ func main() {
 			}
 			jaeger = tracing.SetupJaegerTracing("lotus/" + cmd.Name)
 
+			if cctx.IsSet("color") {
+				color.NoColor = !cctx.Bool("color")
+			}
+
 			if originBefore != nil {
 				return originBefore(cctx)
 			}
@@ -74,6 +79,12 @@ func main() {
 				EnvVars: []string{"LOTUS_PANIC_REPORT_PATH"},
 				Hidden:  true,
 				Value:   "~/.lotus", // should follow --repo default
+			},
+			&cli.BoolFlag{
+				// examined in the Before above
+				Name:        "color",
+				Usage:       "use color in display output",
+				DefaultText: "depends on output being a TTY",
 			},
 			&cli.StringFlag{
 				Name:    "repo",
