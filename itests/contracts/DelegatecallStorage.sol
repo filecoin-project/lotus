7c234b392b34b19cd78c086a464a80bc95da7388
mike seiler <snissn@gmail.com>
2023-01-23 18:17:12 -0500

    reformat

--- itests/contracts/DelegatecallStorage.sol
+++ itests/contracts/DelegatecallStorage.sol
@@ -5,21 +5,21 @@ contract DelegatecallStorage {
     uint public counter;
 
     function getCounter() public view returns (uint){
-	return counter;
+      return counter;
     }
     function setVars(address _contract, uint _counter) public payable returns (uint){
-         (bool success, ) = _contract.delegatecall(
-            abi.encodeWithSignature("setVars(uint256)", _counter)
-        );
-        require(success, 'Error message: Delegatecall failed');
-	return counter;
+      (bool success, ) = _contract.delegatecall(
+          abi.encodeWithSignature("setVars(uint256)", _counter)
+      );
+      require(success, 'Error message: Delegatecall failed');
+      return counter;
     }
     function setVarsSelf(address _contract, uint _counter) public payable returns (uint){
-         (bool success, ) = _contract.delegatecall(
-            abi.encodeWithSignature("setVarsSelf(address,uint256)", _contract, _counter)
-        );
-        require(success, 'Error message: Delegatecall failed');
-	return counter;
+      (bool success, ) = _contract.delegatecall(
+        abi.encodeWithSignature("setVarsSelf(address,uint256)", _contract, _counter)
+      );
+      require(success, 'Error message: Delegatecall failed');
+      return counter;
     }
     function setVarsRevert(address _contract, uint _counter) public payable returns (uint){
          (bool success, ) = _contract.delegatecall(
