3b642fab4f2653f397f3ac8b17b84a36d1bd9ab3
Geoff Stuart <geoffrey.stuart@protocol.ai>
2023-02-02 00:05:55 -0500

    fix test

--- itests/eth_config_test.go
+++ itests/eth_config_test.go
@@ -3,6 +3,7 @@ package itests
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -22,41 +23,47 @@ func TestEthFilterAPIDisabledViaConfig(t *testing.T) {
 
 	_, err := client.EthNewPendingTransactionFilter(ctx)
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthGetLogs(ctx, &ethtypes.EthFilterSpec{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthGetFilterChanges(ctx, ethtypes.EthFilterID{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthGetFilterLogs(ctx, ethtypes.EthFilterID{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthNewFilter(ctx, &ethtypes.EthFilterSpec{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthNewBlockFilter(ctx)
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthNewPendingTransactionFilter(ctx)
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthUninstallFilter(ctx, ethtypes.EthFilterID{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
-	_, err = client.EthSubscribe(ctx, "newHeads", nil)
+	subscribeParams, err := json.Marshal(ethtypes.EthSubscribeParams{
+		EventType: "",
+		Params:    nil,
+	})
+	require.NoError(t, err)
+
+	_, err = client.EthSubscribe(ctx, subscribeParams)
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 
 	_, err = client.EthUnsubscribe(ctx, ethtypes.EthSubscriptionID{})
 	require.NotNil(t, err)
-	require.Equal(t, err.Error(), api.ErrNotSupported.Error())
+	require.Equal(t, api.ErrNotSupported.Error(), err.Error())
 }
