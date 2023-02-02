fc4ce449f3a29ebd22a085eb85bee0d1f1538aa9
Jiaying Wang <42981373+jennijuju@users.noreply.github.com>
2023-02-02 08:44:40 -0500

    Update wallet.go

--- cli/wallet.go
+++ cli/wallet.go
@@ -45,7 +45,7 @@ var walletCmd = &cli.Command{
 var walletNew = &cli.Command{
 	Name:      "new",
 	Usage:     "Generate a new key of the given type",
-	ArgsUsage: "[bls|secp256k1 (default secp256k1)]",
+	ArgsUsage: "[bls|secp256k1|delegated (default secp256k1)]",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetFullNodeAPI(cctx)
 		if err != nil {
