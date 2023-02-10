d9e3bde62456a1d2656b707a58aa2bd8622c05c2
Raúl Kripalani <raul@protocol.ai>
2023-02-10 22:20:20 +0000

    events: re-add a legacy event for backwards compatibility.

--- gen/main.go
+++ gen/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/exchange"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types/legacy"
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/cmd/lotus-shed/shedgen"
 	"github.com/filecoin-project/lotus/node/hello"
@@ -37,6 +38,15 @@ func main() {
 		types.StateInfo0{},
 		types.Event{},
 		types.EventEntry{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = gen.WriteTupleEncodersToFile("./chain/types/legacy/cbor_gen.go", "legacy",
+		legacy.Event{},
+		legacy.EventEntry{},
 	)
 	if err != nil {
 		fmt.Println(err)
