b7db4cb280cc07f35804bb8d90f61a249db300b3
Steven Allen <steven@stebalien.com>
2023-03-09 13:17:17 -0800

    feat: mempool: Reduce minimum replace fee from 1.25x to 1.1x (#10416)
    However, we're leaving the default at 1.25x for backwards compatibility, for now.
    
    Also:
    
    1. Actually use the configured replace fee ratio.
    2. Store said ratios as percentages instead of floats. 1.25, or 1+1/(2^2),
    can be represented as a float. 1.1, or 1 + 1/(2 * 5), cannot.
    
    fixes #10415

---
+++ chain/types/percent_test.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPercent(t *testing.T) {
+	for _, tc := range []struct {
+		p Percent
+		s string
+	}{
+		{100, "1.0"},
+		{111, "1.11"},
+		{12, "0.12"},
+		{-12, "-0.12"},
+		{1012, "10.12"},
+		{-1012, "-10.12"},
+		{0, "0.0"},
+	} {
+		tc := tc
+		t.Run(fmt.Sprintf("%d <> %s", tc.p, tc.s), func(t *testing.T) {
+			m, err := tc.p.MarshalJSON()
+			require.NoError(t, err)
+			require.Equal(t, tc.s, string(m))
+			var p Percent
+			require.NoError(t, p.UnmarshalJSON([]byte(tc.s)))
+			require.Equal(t, tc.p, p)
+		})
+	}
+
+}
