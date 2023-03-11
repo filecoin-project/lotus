3649ae373b18ebe294ee6b5642f5bdc305393207
vyzo <vyzo@hackzen.org>
2023-03-11 18:21:16 +0200

    implementation details

--- chain/index/interface.go
+++ chain/index/interface.go
@@ -2,19 +2,22 @@ package index
 
 import (
 	"context"
+	"errors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 )
 
+var ErrNotFound = errors.New("message not found")
+
 // MsgInfo is the Message metadata the index tracks.
 type MsgInfo struct {
 	// the message this record refers to
 	Message cid.Cid
+	// the tipset where this messages was executed
+	Tipset cid.Cid
 	// the epoch whre this message was executed
 	Epoch abi.ChainEpoch
-	// the tipset where this messages executed
-	Tipset cid.Cid
 	// the canonical execution order of the message in the tipset
 	Index int
 }
