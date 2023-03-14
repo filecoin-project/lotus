c183e6f95a9d0a0afdb8efc89adae3a7ca490aa0
Łukasz Magiera <magik6k@users.noreply.github.com>
2023-03-14 08:41:32 +0100

    Revert "feat: chain: make chain tipset fetching 1000x faster"

--- chain/store/index.go
+++ chain/store/index.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"os"
 	"strconv"
-	"sync"
 
+	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,7 +13,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-var DefaultChainIndexCacheSize = 32 << 15
+var DefaultChainIndexCacheSize = 32 << 10
 
 func init() {
 	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {
@@ -27,8 +27,7 @@ func init() {
 }
 
 type ChainIndex struct {
-	indexCacheLk sync.Mutex
-	indexCache   map[types.TipSetKey]*lbEntry
+	skipCache *lru.ARCCache
 
 	loadTipSet loadTipSetFunc
 
@@ -37,14 +36,17 @@ type ChainIndex struct {
 type loadTipSetFunc func(context.Context, types.TipSetKey) (*types.TipSet, error)
 
 func NewChainIndex(lts loadTipSetFunc) *ChainIndex {
+	sc, _ := lru.NewARC(DefaultChainIndexCacheSize)
 	return &ChainIndex{
-		indexCache: make(map[types.TipSetKey]*lbEntry, DefaultChainIndexCacheSize),
+		skipCache:  sc,
 		loadTipSet: lts,
 		skipLength: 20,
 	}
 }
 
 type lbEntry struct {
+	ts           *types.TipSet
+	parentHeight abi.ChainEpoch
 	targetHeight abi.ChainEpoch
 	target       types.TipSetKey
 }
@@ -56,36 +58,25 @@ func (ci *ChainIndex) GetTipsetByHeight(ctx context.Context, from *types.TipSet,
 
 	rounded, err := ci.roundDown(ctx, from)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to round down: %w", err)
+		return nil, err
 	}
 
-	ci.indexCacheLk.Lock()
-	defer ci.indexCacheLk.Unlock()
 	cur := rounded.Key()
 	for {
-		lbe, ok := ci.indexCache[cur]
+		cval, ok := ci.skipCache.Get(cur)
 		if !ok {
 			fc, err := ci.fillCache(ctx, cur)
 			if err != nil {
-				return nil, xerrors.Errorf("failed to fill cache: %w", err)
+				return nil, err
 			}
-			lbe = fc
+			cval = fc
 		}
 
-		if to == lbe.targetHeight {
-			ts, err := ci.loadTipSet(ctx, lbe.target)
-			if err != nil {
-				return nil, xerrors.Errorf("failed to load tipset: %w", err)
-			}
-
-			return ts, nil
-		}
-		if to > lbe.targetHeight {
-			ts, err := ci.loadTipSet(ctx, cur)
-			if err != nil {
-				return nil, xerrors.Errorf("failed to load tipset: %w", err)
-			}
-			return ci.walkBack(ctx, ts, to)
+		lbe := cval.(*lbEntry)
+		if lbe.ts.Height() == to || lbe.parentHeight < to {
+			return lbe.ts, nil
+		} else if to > lbe.targetHeight {
+			return ci.walkBack(ctx, lbe.ts, to)
 		}
 
 		cur = lbe.target
@@ -96,17 +87,16 @@ func (ci *ChainIndex) GetTipsetByHeightWithoutCache(ctx context.Context, from *t
 	return ci.walkBack(ctx, from, to)
 }
 
-// Caller must hold indexCacheLk
 func (ci *ChainIndex) fillCache(ctx context.Context, tsk types.TipSetKey) (*lbEntry, error) {
 	ts, err := ci.loadTipSet(ctx, tsk)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to load tipset: %w", err)
+		return nil, err
 	}
 
 	if ts.Height() == 0 {
 		return &lbEntry{
-			targetHeight: 0,
-			target:       tsk,
+			ts:           ts,
+			parentHeight: 0,
 		}, nil
 	}
 
@@ -134,10 +124,12 @@ func (ci *ChainIndex) fillCache(ctx context.Context, tsk types.TipSetKey) (*lbEn
 	}
 
 	lbe := &lbEntry{
+		ts:           ts,
+		parentHeight: parent.Height(),
 		targetHeight: skipTarget.Height(),
 		target:       skipTarget.Key(),
 	}
-	ci.indexCache[tsk] = lbe
+	ci.skipCache.Add(tsk, lbe)
 
 	return lbe, nil
 }
@@ -152,7 +144,7 @@ func (ci *ChainIndex) roundDown(ctx context.Context, ts *types.TipSet) (*types.T
 
 	rounded, err := ci.walkBack(ctx, ts, target)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to walk back: %w", err)
+		return nil, err
 	}
 
 	return rounded, nil
@@ -172,7 +164,7 @@ func (ci *ChainIndex) walkBack(ctx context.Context, from *types.TipSet, to abi.C
 	for {
 		pts, err := ci.loadTipSet(ctx, ts.Parents())
 		if err != nil {
-			return nil, xerrors.Errorf("failed to load tipset: %w", err)
+			return nil, err
 		}
 
 		if to > pts.Height() {
