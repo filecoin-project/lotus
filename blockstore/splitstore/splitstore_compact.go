ec1b984266340082b42ee2627478e2960fb39029
zenground0 <ZenGround0@users.noreply.github.com>
2023-03-20 08:18:39 -0600

    Demote now common logs

--- blockstore/splitstore/splitstore_compact.go
+++ blockstore/splitstore/splitstore_compact.go
@@ -452,7 +452,7 @@ func (s *SplitStore) doTxnProtect(root cid.Cid, markSet MarkSet) (int64, error) 
 		},
 		func(c cid.Cid) error {
 			if s.txnMissing != nil {
-				log.Warnf("missing object reference %s in %s", c, root)
+				log.Debugf("missing object reference %s in %s", c, root)
 				s.txnRefsMx.Lock()
 				s.txnMissing[c] = struct{}{}
 				s.txnRefsMx.Unlock()
