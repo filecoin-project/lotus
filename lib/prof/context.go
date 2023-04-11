e70bd7709494bd6501e466c0babb0e026e0df1f7
vyzo <vyzo@hackzen.org>
2023-04-11 17:48:29 +0300

    add context option to pass around a looger for aggregating timing profile

---
+++ lib/prof/context.go
@@ -0,0 +1,53 @@
+package prof
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	logging "github.com/ipfs/go-log/v2"
+)
+
+type profileLoggerKeyType struct{}
+
+var profileLoggerKey = profileLoggerKeyType{}
+
+type ProfileLogger interface {
+	Done()
+}
+
+type profileLogger struct {
+	log   *logging.ZapEventLogger
+	start time.Time
+	what  string
+}
+
+var _ ProfileLogger = (*profileLogger)(nil)
+
+type emptyProfileLogger struct{}
+
+var _ ProfileLogger = emptyProfileLogger{}
+
+func WithProfileContext(ctx context.Context, name string) context.Context {
+	logger := logging.Logger(name)
+	return context.WithValue(ctx, profileLoggerKey, logger)
+}
+
+func UsingProfileLogger(ctx context.Context, what string) ProfileLogger {
+	if v := ctx.Value(profileLoggerKey); v != nil {
+		return newProfileLogger(v.(*logging.ZapEventLogger), what)
+	}
+
+	return emptyProfileLogger{}
+}
+
+func (emptyProfileLogger) Done() {}
+
+func newProfileLogger(log *logging.ZapEventLogger, what string) ProfileLogger {
+	start := time.Now()
+	return &profileLogger{start: start, log: log, what: what}
+}
+
+func (l *profileLogger) Done() {
+	l.log.Infow(fmt.Sprintf("%s done", l.what), "took", time.Since(l.start))
+}
