fa1d4a716f9bc5e06e5f7d2779f45bc75f42c5c2
Steven Allen <steven@stebalien.com>
2023-04-26 13:05:25 -0700

    fix: minus minus

--- chain/sync.go
+++ chain/sync.go
@@ -1188,7 +1188,7 @@ func (syncer *Syncer) collectChain(ctx context.Context, ts *types.TipSet, hts *t
 	ss.SetStage(api.StagePersistHeaders)
 
 	// Write tipsets from oldest to newest.
-	for i := len(headers) - 1; i >= 0; i++ {
+	for i := len(headers) - 1; i >= 0; i-- {
 		ts := headers[i]
 		if err := syncer.store.PersistTipset(ctx, ts); err != nil {
 			err = xerrors.Errorf("failed to persist synced tipset to the chainstore: %w", err)
