5137a0f5c85d6eafd06cfef8f13f793274b6ea85
Aayush <arajasek94@gmail.com>
2023-05-02 10:34:29 -0400

    try: make TestBatchDealInput less flaky

--- itests/batch_deal_test.go
+++ itests/batch_deal_test.go
@@ -22,9 +22,8 @@ import (
 	"github.com/filecoin-project/lotus/storage/pipeline/sealiface"
 )
 
-func TestBatchDealInput(t *testing.T) {
-	//stm: @MINER_SECTOR_STATUS_001, @MINER_SECTOR_LIST_001
-	kit.QuietMiningLogs()
+func runBatchDealTest(t *testing.T, piece, deals, expectSectors int) {
+	//kit.QuietMiningLogs()
 
 	var (
 		blockTime = 10 * time.Millisecond
@@ -35,121 +34,121 @@ func TestBatchDealInput(t *testing.T) {
 		dealStartEpoch = abi.ChainEpoch(2 << 12)
 	)
 
-	run := func(piece, deals, expectSectors int) func(t *testing.T) {
-		return func(t *testing.T) {
-			t.Logf("batchtest start")
+	t.Logf("batchtest start")
 
-			ctx := context.Background()
+	ctx := context.Background()
 
-			publishPeriod := 10 * time.Second
-			maxDealsPerMsg := uint64(deals)
+	publishPeriod := 10 * time.Second
+	maxDealsPerMsg := uint64(deals)
 
-			// Set max deals per publish deals message to maxDealsPerMsg
-			opts := kit.ConstructorOpts(node.Options(
-				node.Override(
-					new(*storageadapter.DealPublisher),
-					storageadapter.NewDealPublisher(nil, storageadapter.PublishMsgConfig{
-						Period:         publishPeriod,
-						MaxDealsPerMsg: maxDealsPerMsg,
-					})),
-				node.Override(new(dtypes.GetSealingConfigFunc), func() (dtypes.GetSealingConfigFunc, error) {
-					return func() (sealiface.Config, error) {
-						cfg := config.DefaultStorageMiner()
-						sc := modules.ToSealingConfig(cfg.Dealmaking, cfg.Sealing)
-						sc.MaxWaitDealsSectors = 2
-						sc.MaxSealingSectors = 1
-						sc.MaxSealingSectorsForDeals = 3
-						sc.AlwaysKeepUnsealedCopy = true
-						sc.WaitDealsDelay = time.Hour
-						sc.BatchPreCommits = false
-						sc.AggregateCommits = false
+	// Set max deals per publish deals message to maxDealsPerMsg
+	opts := kit.ConstructorOpts(node.Options(
+		node.Override(
+			new(*storageadapter.DealPublisher),
+			storageadapter.NewDealPublisher(nil, storageadapter.PublishMsgConfig{
+				Period:         publishPeriod,
+				MaxDealsPerMsg: maxDealsPerMsg,
+			})),
+		node.Override(new(dtypes.GetSealingConfigFunc), func() (dtypes.GetSealingConfigFunc, error) {
+			return func() (sealiface.Config, error) {
+				cfg := config.DefaultStorageMiner()
+				sc := modules.ToSealingConfig(cfg.Dealmaking, cfg.Sealing)
+				sc.MaxWaitDealsSectors = 2
+				sc.MaxSealingSectors = 1
+				sc.MaxSealingSectorsForDeals = 1
+				sc.AlwaysKeepUnsealedCopy = false
+				sc.WaitDealsDelay = time.Hour
+				sc.BatchPreCommits = false
+				sc.AggregateCommits = false
 
-						return sc, nil
-					}, nil
-				}),
-			))
-			client, miner, ens := kit.EnsembleMinimal(t, kit.MockProofs(), opts, kit.ThroughRPC())
-			ens.InterconnectAll().BeginMining(blockTime)
-			dh := kit.NewDealHarness(t, client, miner, miner)
+				return sc, nil
+			}, nil
+		}),
+	))
+	client, miner, _, ens := kit.EnsembleOneTwo(t, kit.MockProofs(), opts, kit.ThroughRPC())
+	ens.InterconnectAll().BeginMining(blockTime)
+	dh := kit.NewDealHarness(t, client, miner, miner)
 
-			err := miner.MarketSetAsk(ctx, big.Zero(), big.Zero(), 200, 128, 32<<30)
+	err := miner.MarketSetAsk(ctx, big.Zero(), big.Zero(), 200, 128, 32<<30)
+	require.NoError(t, err)
+
+	t.Logf("batchtest ask set")
+
+	checkNoPadding := func() {
+		sl, err := miner.SectorsListNonGenesis(ctx)
+		require.NoError(t, err)
+
+		sort.Slice(sl, func(i, j int) bool {
+			return sl[i] < sl[j]
+		})
+
+		for _, snum := range sl {
+			si, err := miner.SectorsStatus(ctx, snum, false)
 			require.NoError(t, err)
 
-			t.Logf("batchtest ask set")
+			// fmt.Printf("S %d: %+v %s\n", snum, si.Deals, si.State)
 
-			checkNoPadding := func() {
-				sl, err := miner.SectorsListNonGenesis(ctx)
-				require.NoError(t, err)
-
-				sort.Slice(sl, func(i, j int) bool {
-					return sl[i] < sl[j]
-				})
-
-				for _, snum := range sl {
-					si, err := miner.SectorsStatus(ctx, snum, false)
-					require.NoError(t, err)
-
-					// fmt.Printf("S %d: %+v %s\n", snum, si.Deals, si.State)
-
-					for _, deal := range si.Deals {
-						if deal == 0 {
-							fmt.Printf("sector %d had a padding piece!\n", snum)
-						}
-					}
+			for _, deal := range si.Deals {
+				if deal == 0 {
+					fmt.Printf("sector %d had a padding piece!\n", snum)
 				}
 			}
-
-			// Starts a deal and waits until it's published
-			runDealTillSeal := func(rseed int) {
-				res, _, _, err := kit.CreateImportFile(ctx, client, rseed, piece)
-				require.NoError(t, err)
-
-				dp := dh.DefaultStartDealParams()
-				dp.Data.Root = res.Root
-				dp.DealStartEpoch = dealStartEpoch
-
-				deal := dh.StartDeal(ctx, dp)
-				dh.WaitDealSealed(ctx, deal, false, true, checkNoPadding)
-			}
-
-			// Run maxDealsPerMsg deals in parallel
-			done := make(chan struct{}, maxDealsPerMsg)
-			for rseed := 0; rseed < int(maxDealsPerMsg); rseed++ {
-				rseed := rseed
-				go func() {
-					runDealTillSeal(rseed)
-					done <- struct{}{}
-				}()
-			}
-
-			t.Logf("batchtest deals started")
-
-			// Wait for maxDealsPerMsg of the deals to be published
-			for i := 0; i < int(maxDealsPerMsg); i++ {
-				<-done
-			}
-
-			t.Logf("batchtest deals published")
-
-			checkNoPadding()
-
-			t.Logf("batchtest no padding")
-
-			sl, err := miner.SectorsListNonGenesis(ctx)
-			require.NoError(t, err)
-			require.Equal(t, len(sl), expectSectors)
-
-			t.Logf("batchtest done")
 		}
 	}
 
-	t.Run("4-p1600B", run(1600, 4, 4))
-	t.Run("4-p513B", run(513, 4, 2))
-	if !testing.Short() {
-		t.Run("32-p257B", run(257, 32, 8))
+	// Starts a deal and waits until it's published
+	runDealTillSeal := func(rseed int) {
+		res, _, _, err := kit.CreateImportFile(ctx, client, rseed, piece)
+		require.NoError(t, err)
 
-		// fixme: this appears to break data-transfer / markets in some really creative ways
-		//t.Run("32-p10B", run(10, 32, 2))
-		// t.Run("128-p10B", run(10, 128, 8))
+		dp := dh.DefaultStartDealParams()
+		dp.Data.Root = res.Root
+		dp.DealStartEpoch = dealStartEpoch
+
+		deal := dh.StartDeal(ctx, dp)
+		dh.WaitDealSealed(ctx, deal, false, true, checkNoPadding)
+	}
+
+	// Run maxDealsPerMsg deals in parallel
+	done := make(chan struct{}, maxDealsPerMsg)
+	for rseed := 0; rseed < int(maxDealsPerMsg); rseed++ {
+		rseed := rseed
+		go func() {
+			runDealTillSeal(rseed)
+			done <- struct{}{}
+		}()
+	}
+
+	t.Logf("batchtest deals started")
+
+	// Wait for maxDealsPerMsg of the deals to be published
+	for i := 0; i < int(maxDealsPerMsg); i++ {
+		<-done
+	}
+
+	t.Logf("batchtest deals published")
+
+	checkNoPadding()
+
+	t.Logf("batchtest no padding")
+
+	sl, err := miner.SectorsListNonGenesis(ctx)
+	require.NoError(t, err)
+	require.Equal(t, len(sl), expectSectors)
+
+	t.Logf("batchtest done")
+}
+
+func TestBatchDealInputp1600B(t *testing.T) {
+	runBatchDealTest(t, 1600, 4, 4)
+}
+
+func TestBatchDealInputp513B(t *testing.T) {
+	runBatchDealTest(t, 513, 4, 2)
+}
+
+func TestBatchDealInputp257B(t *testing.T) {
+	if !testing.Short() {
+		runBatchDealTest(t, 257, 32, 8)
 	}
 }
