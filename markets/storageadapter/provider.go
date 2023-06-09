7f306a5b02993b3560f88bcdb8fe413800b6ec34
Andrew Jackson (Ajax) <snadrus@gmail.com>
2023-06-09 11:21:31 -0500

    Synthetic PoRep Impl

--- markets/storageadapter/provider.go
+++ markets/storageadapter/provider.go
@@ -23,7 +23,6 @@ import (
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -168,25 +167,6 @@ func (n *ProviderNodeAdapter) GetMinerWorkerAddress(ctx context.Context, maddr a
 		return address.Address{}, err
 	}
 	return mi.Worker, nil
-}
-
-func (n *ProviderNodeAdapter) GetProofType(ctx context.Context, maddr address.Address, tok shared.TipSetToken) (abi.RegisteredSealProof, error) {
-	tsk, err := types.TipSetKeyFromBytes(tok)
-	if err != nil {
-		return 0, err
-	}
-
-	mi, err := n.StateMinerInfo(ctx, maddr, tsk)
-	if err != nil {
-		return 0, err
-	}
-
-	nver, err := n.StateNetworkVersion(ctx, tsk)
-	if err != nil {
-		return 0, err
-	}
-
-	return miner.PreferredSealProofTypeFromWindowPoStType(nver, mi.WindowPoStProofType)
 }
 
 func (n *ProviderNodeAdapter) SignBytes(ctx context.Context, signer address.Address, b []byte) (*crypto.Signature, error) {
