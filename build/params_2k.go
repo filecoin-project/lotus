cf27bcf6da98e9047c062ccd787ab3145661a94e
Phi <orjan.roren@gmail.com>
2023-07-07 11:56:30 +0200

    feat: introduce local nv21 skeleton
    Introduce nv21 skeleton for local testing:
    
    - Use local go-state-types with actor_version_checklist changes: https://github.com/filecoin-project/go-state-types/blob/master/actors_version_checklist.md
    - Imports mock v12-actors bundle
    - Define upgrade heights
    - Generate adapters
    - Add upgrade schedule and migration
    - Add actorstype to the NewActorRegistry in /chain/consensus/computestate.go
    - Add upgrade field to api/types.go/ForkUpgradeParams
    - Add upgrade to node/impl/full/state.go
    - Add network version to chain/state/statetree.go
    - make jen
    - make docsgen-cli

--- build/params_2k.go
+++ build/params_2k.go
@@ -122,7 +122,7 @@ func init() {
 	UpgradeHyggeHeight = getUpgradeHeight("LOTUS_HYGGE_HEIGHT", UpgradeHyggeHeight)
 	UpgradeLightningHeight = getUpgradeHeight("LOTUS_LIGHTNING_HEIGHT", UpgradeLightningHeight)
 	UpgradeThunderHeight = getUpgradeHeight("LOTUS_THUNDER_HEIGHT", UpgradeThunderHeight)
-	UpgradeWatermelonHeight = getUpgradeHeight("LOTUS_WATERMELON_HEIGHT", UpgradeWatermelonHeight)
+	UpgradeWatermelonHeight = getUpgradeHeight("LOTUS_Watermelon_HEIGHT", UpgradeWatermelonHeight)
 
 	BuildType |= Build2k
 
