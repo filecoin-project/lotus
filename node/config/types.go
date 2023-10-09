ce27d5fcf22ee763706ebf7c48367e503efc5ea6
Phi <orjan.roren@gmail.com>
2023-10-09 11:17:03 +0100

    fix: config: update sector lifetime comment
    Update SectorLifetime duration comment to be in line with FIP-0052: https://fips.filecoin.io/FIPS/fip-0052.html

--- node/config/types.go
+++ node/config/types.go
@@ -358,7 +358,7 @@ type SealingConfig struct {
 
 	// CommittedCapacitySectorLifetime is the duration a Committed Capacity (CC) sector will
 	// live before it must be extended or converted into sector containing deals before it is
-	// terminated. Value must be between 180-540 days inclusive
+	// terminated. Value must be between 180-1278 days.
 	CommittedCapacitySectorLifetime Duration
 
 	// Period of time that a newly created sector will wait for more deals to be packed in to before it starts to seal.
