17bea82edc180f8ba6b01788c05b5a1c6a91bdeb
Steven Allen <steven@stebalien.com>
2023-10-26 13:55:25 -0700

    fix: api: compute gasUsedRatio based on max gas in the tipset
    We were computing this based on the max block gas, but this is
    incorrect. The new value isn't entirely correct either (we should
    probably compute an average of the gas used in each block in the
    tipset?), but it's good enough.
    
    fixes #10515

--- node/impl/full/eth.go
+++ node/impl/full/eth.go
@@ -733,10 +733,11 @@ func (a *EthModule) EthFeeHistory(ctx context.Context, p jsonrpc.RawParams) (eth
 		}
 
 		rewards, totalGasUsed := calculateRewardsAndGasUsed(rewardPercentiles, txGasRewards)
+		maxGas := build.BlockGasLimit * int64(len(ts.Blocks()))
 
 		// arrays should be reversed at the end
 		baseFeeArray = append(baseFeeArray, ethtypes.EthBigInt(basefee))
-		gasUsedRatioArray = append(gasUsedRatioArray, float64(totalGasUsed)/float64(build.BlockGasLimit))
+		gasUsedRatioArray = append(gasUsedRatioArray, float64(totalGasUsed)/float64(maxGas))
 		rewardsArray = append(rewardsArray, rewards)
 		oldestBlkHeight = uint64(ts.Height())
 		blocksIncluded++
