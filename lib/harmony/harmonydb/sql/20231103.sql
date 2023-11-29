d6b3f6a36937f83e219718b6ac88e9d7954d9083
Łukasz Magiera <magik6k@gmail.com>
2023-11-29 16:44:22 +0100

    more comments on message_sends

--- lib/harmony/harmonydb/sql/20231103.sql
+++ lib/harmony/harmonydb/sql/20231103.sql
@@ -22,12 +22,20 @@ create table message_sends
 );
 
 comment on column message_sends.from_key is 'text f[1/3/4]... address';
-comment on column message_sends.nonce is 'assigned message nonce';
 comment on column message_sends.to_addr is 'text f[0/1/2/3/4]... address';
-comment on column message_sends.signed_data is 'signed message data';
-comment on column message_sends.signed_cid is 'signed message cid';
 comment on column message_sends.send_reason is 'optional description of send reason';
-comment on column message_sends.send_success is 'whether this message was broadcasted to the network already';
+comment on column message_sends.send_task_id is 'harmony task id of the send task';
+
+comment on column message_sends.unsigned_data is 'unsigned message data';
+comment on column message_sends.unsigned_cid is 'unsigned message cid';
+
+comment on column message_sends.nonce is 'assigned message nonce, set while the send task is executing';
+comment on column message_sends.signed_data is 'signed message data, set while the send task is executing';
+comment on column message_sends.signed_cid is 'signed message cid, set while the send task is executing';
+
+comment on column message_sends.send_time is 'time when the send task was executed, set after pushing the message to the network';
+comment on column message_sends.send_success is 'whether this message was broadcasted to the network already, null if not yet attempted, true if successful, false if failed';
+comment on column message_sends.send_error is 'error message if send_success is false';
 
 create unique index message_sends_success_index
     on message_sends (from_key, nonce)
