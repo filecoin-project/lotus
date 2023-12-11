b14399ef1b21ed296bc9c2f1b3c82396c7f31d81
Łukasz Magiera <magik6k@gmail.com>
2023-12-11 16:55:20 +0100

    lpweb: more hacking

--- provider/lpweb/rpc.go
+++ provider/lpweb/rpc.go
@@ -1,32 +1,15 @@
 package lpweb
 
 import (
-	"context"
-	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/lib/harmony/harmonydb"
-	"github.com/filecoin-project/lotus/node/config"
-	"golang.org/x/xerrors"
 )
 
-type lprpc struct {
+type webApp struct {
 	db   *harmonydb.DB
 	full api.FullNode
-	cfg  *config.LotusProviderConfig
 }
 
-func (r *lprpc) SpIDs(ctx context.Context) ([]address.Address, error) {
-	ca := r.cfg.Addresses.MinerAddresses
-	out := make([]address.Address, len(ca))
-
-	for i, s := range ca {
-		a, err := address.NewFromString(s)
-		if err != nil {
-			return nil, xerrors.Errorf("parsing config address: %w", err)
-		}
-
-		out[i] = a
-	}
-
-	return out, nil
+func ServeWeb(listen string) error {
+	return nil
 }
