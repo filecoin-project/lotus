99ecc475386254f6e2556b7c13e68bc8e6b30956
Rod Vagg <rod@vagg.org>
2024-02-26 21:28:18 +1100

    More clarity in actor event API docs

--- api/api_full.go
+++ api/api_full.go
@@ -890,13 +890,21 @@ type FullNode interface {
 
 	// GetActorEvents returns all FVM and built-in Actor events that match the given filter.
 	// This is a request/response API.
+	// Results available from this API may be limited by the MaxFilterResults and MaxFilterHeightRange
+	// configuration options and also the amount of historical data available in the node.
 	GetActorEvents(ctx context.Context, filter *types.ActorEventFilter) ([]*types.ActorEvent, error) //perm:read
 
-	// SubscribeActorEvents returns a long-lived stream of all FVM and built-in Actor events that match the given filter.
-	// Events that match the given filter are written to the stream in real-time as they are emitted from the FVM.
-	// The response stream is closed when the client disconnects or if there is an error while writing an event to the stream.
-	// This API also allows clients to read all historical events matching the given filter before
-	// any real-time events are written to the response stream.
+	// SubscribeActorEvents returns a long-lived stream of all FVM and built-in Actor events that
+	// match the given filter.
+	// Events that match the given filter are written to the stream in real-time as they are emitted
+	// from the FVM.
+	// The response stream is closed when the client disconnects, when a ToHeight is specified and is
+	// reached, or if there is an error while writing an event to the stream.
+	// This API also allows clients to read all historical events matching the given filter before any
+	// real-time events are written to the response stream if the filter specifies an earlier
+	// FromHeight.
+	// Results available from this API may be limited by the MaxFilterResults and MaxFilterHeightRange
+	// configuration options and also the amount of historical data available in the node.
 	// NOTE: THIS API IS ONLY SUPPORTED OVER WEBSOCKETS FOR NOW
 	SubscribeActorEvents(ctx context.Context, filter *types.ActorEventFilter) (<-chan *types.ActorEvent, error) //perm:read
 }
