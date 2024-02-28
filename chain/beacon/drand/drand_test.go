f2bca588b2127587366470ceb90779e35225eafd
Aayush Rajasekaran <arajasek94@gmail.com>
2024-02-28 15:19:00 -0500

    Merge pull request #11632 from hanabi1224/hm/drand-test
    feat: drand quicknet: allow scheduling drand quicknet upgrade before nv22 on 2k devnet

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -33,7 +33,15 @@ func TestMaxBeaconRoundForEpoch(t *testing.T) {
 	todayTs := uint64(1652222222)
 	db, err := NewDrandBeacon(todayTs, build.BlockDelaySecs, nil, build.DrandConfigs[build.DrandTestnet])
 	assert.NoError(t, err)
+	assert.True(t, db.IsChained())
 	mbr15 := db.MaxBeaconRoundForEpoch(network.Version15, 100)
 	mbr16 := db.MaxBeaconRoundForEpoch(network.Version16, 100)
 	assert.Equal(t, mbr15+1, mbr16)
+}
+
+func TestQuicknetIsChained(t *testing.T) {
+	todayTs := uint64(1652222222)
+	db, err := NewDrandBeacon(todayTs, build.BlockDelaySecs, nil, build.DrandConfigs[build.DrandQuicknet])
+	assert.NoError(t, err)
+	assert.False(t, db.IsChained())
 }
