da657b2aa8eee0183edd38c23d4b92154900cb3a
Łukasz Magiera <magik6k@gmail.com>
2024-03-19 16:46:23 +0100

    curio web: node info page

--- curiosrc/web/hapi/routes.go
+++ curiosrc/web/hapi/routes.go
@@ -2,12 +2,13 @@ package hapi
 
 import (
 	"embed"
-	"html/template"
+	"text/template"
 
 	"github.com/gorilla/mux"
 	logging "github.com/ipfs/go-log/v2"
 	"golang.org/x/xerrors"
 
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/cmd/curio/deps"
 )
 
@@ -15,7 +16,7 @@ import (
 var templateFS embed.FS
 
 func Routes(r *mux.Router, deps *deps.Deps) error {
-	t, err := template.ParseFS(templateFS, "web/*")
+	t, err := makeTemplate().ParseFS(templateFS, "web/*")
 	if err != nil {
 		return xerrors.Errorf("parse templates: %w", err)
 	}
@@ -28,15 +29,26 @@ func Routes(r *mux.Router, deps *deps.Deps) error {
 	go a.watchRpc()
 	go a.watchActor()
 
-	r.HandleFunc("/simpleinfo/actorsummary", a.actorSummary)
-	r.HandleFunc("/simpleinfo/machines", a.indexMachines)
-	r.HandleFunc("/simpleinfo/tasks", a.indexTasks)
-	r.HandleFunc("/simpleinfo/taskhistory", a.indexTasksHistory)
-	r.HandleFunc("/simpleinfo/pipeline-porep", a.indexPipelinePorep)
+	r.HandleFunc("/actorsummary", a.actorSummary)
+	r.HandleFunc("/machines", a.indexMachines)
+	r.HandleFunc("/tasks", a.indexTasks)
+	r.HandleFunc("/taskhistory", a.indexTasksHistory)
+	r.HandleFunc("/pipeline-porep", a.indexPipelinePorep)
 
 	// pipeline-porep page
-	r.HandleFunc("/simpleinfo/pipeline-porep/sectors", a.pipelinePorepSectors)
+	r.HandleFunc("/pipeline-porep/sectors", a.pipelinePorepSectors)
+
+	// node info page
+	r.HandleFunc("/node/{id}", a.nodeInfo)
 	return nil
+}
+
+func makeTemplate() *template.Template {
+	return template.New("").Funcs(template.FuncMap{
+		"toHumanBytes": func(b int64) string {
+			return types.SizeStr(types.NewInt(uint64(b)))
+		},
+	})
 }
 
 var log = logging.Logger("curio/web")
