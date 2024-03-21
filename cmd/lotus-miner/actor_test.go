30cd1fa73746ce1ba394cb097f3c51d537e2cf09
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-03-21 12:25:20 -0500

    actors are done

--- cmd/lotus-miner/actor_test.go
+++ cmd/lotus-miner/actor_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/cli/spcli"
 	"github.com/filecoin-project/lotus/itests/kit"
 	"github.com/filecoin-project/lotus/node/repo"
 )
@@ -67,7 +68,7 @@ func TestWorkerKeyChange(t *testing.T) {
 	// Initialize wallet.
 	kit.SendFunds(ctx, t, client1, newKey, abi.NewTokenAmount(0))
 
-	require.NoError(t, run(actorProposeChangeWorker, "--really-do-it", newKey.String()))
+	require.NoError(t, run(spcli.ActorProposeChangeWorkerCmd(LMActorGetter), "--really-do-it", newKey.String()))
 
 	result := output.String()
 	output.Reset()
@@ -82,12 +83,12 @@ func TestWorkerKeyChange(t *testing.T) {
 	require.NotZero(t, targetEpoch)
 
 	// Too early.
-	require.Error(t, run(actorConfirmChangeWorker, "--really-do-it", newKey.String()))
+	require.Error(t, run(spcli.ActorConfirmChangeWorkerCmd(LMActorGetter), "--really-do-it", newKey.String()))
 	output.Reset()
 
 	client1.WaitTillChain(ctx, kit.HeightAtLeast(abi.ChainEpoch(targetEpoch)))
 
-	require.NoError(t, run(actorConfirmChangeWorker, "--really-do-it", newKey.String()))
+	require.NoError(t, run(spcli.ActorConfirmChangeWorkerCmd(LMActorGetter), "--really-do-it", newKey.String()))
 	output.Reset()
 
 	head, err := client1.ChainHead(ctx)
--- cmd/sptool/actor.go
+++ cmd/sptool/actor.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"os"
+	"path"
+
+	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 
+	"github.com/filecoin-project/go-address"
+
+	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/cli/spcli"
 )
 
@@ -10,6 +18,52 @@ var actorCmd = &cli.Command{
 	Name:  "actor",
 	Usage: "Manage Filecoin Miner Actor Metadata",
 	Subcommands: []*cli.Command{
+		spcli.ActorSetAddrsCmd(SPTActorGetter),
 		spcli.ActorWithdrawCmd(SPTActorGetter),
+		spcli.ActorRepayDebtCmd(SPTActorGetter),
+		spcli.ActorSetPeeridCmd(SPTActorGetter),
+		spcli.ActorSetOwnerCmd(SPTActorGetter),
+		addTomlArg(spcli.ActorControlCmd(SPTActorGetter, getControlAddresses)),
+		spcli.ActorProposeChangeWorkerCmd(SPTActorGetter),
+		spcli.ActorConfirmChangeWorkerCmd(SPTActorGetter),
+		spcli.ActorCompactAllocatedCmd(SPTActorGetter),
+		spcli.ActorProposeChangeBeneficiaryCmd(SPTActorGetter),
+		spcli.ActorConfirmChangeBeneficiaryCmd(SPTActorGetter),
 	},
+}
+
+func addTomlArg(cmd *cli.Command) *cli.Command {
+	cmd.Flags = append(cmd.Flags, &cli.StringFlag{
+		Name:        "toml",
+		Usage:       "path to toml file, or folder containing config.toml",
+		EnvVars:     []string{"LOTUS_MINER_PATH"},
+		DefaultText: "~/.lotusminer",
+	})
+	return cmd
+}
+
+func getControlAddresses(cctx *cli.Context, actor address.Address) (lapi.AddressConfig, error) {
+	p, err := homedir.Expand(cctx.String("toml"))
+	if err != nil {
+		return lapi.AddressConfig{}, err
+	}
+	st, err := os.Stat(p)
+	if err != nil {
+		return lapi.AddressConfig{}, err
+	}
+	if st.IsDir() {
+		p = path.Join(p, "config.toml")
+	}
+	b, err := os.ReadFile(p)
+	if err != nil {
+		return lapi.AddressConfig{}, err
+	}
+	var cfg struct {
+		Addresses lapi.AddressConfig
+	}
+	_, err = toml.Decode(string(b), &cfg)
+	if err != nil {
+		return lapi.AddressConfig{}, err
+	}
+	return cfg.Addresses, nil
 }
