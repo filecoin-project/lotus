119be6db5ba018b4f80c7a7666feeff311e6c75c
Peter Rabbitson <ribasushi@protocol.ai>
2024-03-23 14:53:30 +0100

    Always store events in the blockstore, making this conditional is pointless

--- node/builder_chain.go
+++ node/builder_chain.go
@@ -203,10 +203,14 @@ func ConfigFullNode(c interface{}) Option {
 			Override(SetupFallbackBlockstoresKey, modules.InitFallbackBlockstores),
 		),
 
+		// FORCE always storing events in the blockstore, making this conditional is pointless
+		//
+		// (ignore original text below)
+		//
 		// If the Eth JSON-RPC is enabled, enable storing events at the ChainStore.
 		// This is the case even if real-time and historic filtering are disabled,
 		// as it enables us to serve logs in eth_getTransactionReceipt.
-		If(cfg.Fevm.EnableEthRPC || cfg.Events.EnableActorEventsAPI, Override(StoreEventsKey, modules.EnableStoringEvents)),
+		Override(StoreEventsKey, modules.EnableStoringEvents),
 
 		If(cfg.Wallet.RemoteBackend != "",
 			Override(new(*remotewallet.RemoteWallet), remotewallet.SetupRemoteWallet(cfg.Wallet.RemoteBackend)),
