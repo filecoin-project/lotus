8aa5de1038dca0d95853a1f5aaa279f042cee91a
Jonathan <jpschwartz2@uwalumni.com>
2024-04-17 17:24:35 -0700

    Pass Timestamp through ExecuteMessageParams

--- conformance/driver.go
+++ conformance/driver.go
@@ -196,6 +196,7 @@ func (d *Driver) ExecuteTipset(bs blockstore.Blockstore, ds ds.Batching, params 
 type ExecuteMessageParams struct {
 	Preroot        cid.Cid
 	Epoch          abi.ChainEpoch
+	Timestamp      uint64
 	Message        *types.Message
 	CircSupply     abi.TokenAmount
 	BaseFee        abi.TokenAmount
@@ -249,6 +250,7 @@ func (d *Driver) ExecuteMessage(bs blockstore.Blockstore, params ExecuteMessageP
 	vmOpts := &vm.VMOpts{
 		StateBase: params.Preroot,
 		Epoch:     params.Epoch,
+		Timestamp: params.Timestamp,
 		Bstore:    bs,
 		Syscalls:  vm.Syscalls(ffiwrapper.ProofVerifier),
 		CircSupplyCalc: func(_ context.Context, _ abi.ChainEpoch, _ *state.StateTree) (abi.TokenAmount, error) {
