308c81c01e245f43706c914305d24dd0f1e0af89
LexLuthr <lexluthr@curiostorage.org>
2024-04-30 22:57:53 +0530

    sql parser

--- lib/harmony/harmonydb/sql/20240430-init-piece-createdat.sql
+++ lib/harmony/harmonydb/sql/20240430-init-piece-createdat.sql
@@ -16,48 +16,46 @@ CREATE OR REPLACE FUNCTION insert_sector_market_piece(
     v_f05_deal_proposal jsonb,
     v_f05_deal_start_epoch bigint,
     v_f05_deal_end_epoch bigint
-    ) RETURNS void AS $$
-        BEGIN
-        INSERT INTO sectors_sdr_initial_pieces (
-            sp_id,
-            sector_number,
-            piece_index,
-            created_at,
-            piece_cid,
-            piece_size,
-            data_url,
-            data_headers,
-            data_raw_size,
-            data_delete_on_finalize,
-            f05_publish_cid,
-            f05_deal_id,
-            f05_deal_proposal,
-            f05_deal_start_epoch,
-            f05_deal_end_epoch
-        ) VALUES (
-                     v_sp_id,
-                     v_sector_number,
-                     v_piece_index,
-                     NOW(),
-                     v_piece_cid,
-                     v_piece_size,
-                     v_data_url,
-                     v_data_headers,
-                     v_data_raw_size,
-                     v_data_delete_on_finalize,
-                     v_f05_publish_cid,
-                     v_f05_deal_id,
-                     v_f05_deal_proposal,
-                     v_f05_deal_start_epoch,
-                     v_f05_deal_end_epoch
-                 )
-            ON CONFLICT (sp_id, sector_number, piece_index) DO NOTHING;
-
-        IF NOT FOUND THEN
-                RAISE EXCEPTION 'Conflict detected for piece_index %', v_piece_index;
-        END IF;
-        END;
-        $$ LANGUAGE plpgsql;
+) RETURNS void AS $$
+BEGIN
+    INSERT INTO sectors_sdr_initial_pieces (
+        sp_id,
+        sector_number,
+        piece_index,
+        created_at,
+        piece_cid,
+        piece_size,
+        data_url,
+        data_headers,
+        data_raw_size,
+        data_delete_on_finalize,
+        f05_publish_cid,
+        f05_deal_id,
+        f05_deal_proposal,
+        f05_deal_start_epoch,
+        f05_deal_end_epoch
+    ) VALUES (
+         v_sp_id,
+         v_sector_number,
+         v_piece_index,
+         NOW(),
+         v_piece_cid,
+         v_piece_size,
+         v_data_url,
+         v_data_headers,
+         v_data_raw_size,
+         v_data_delete_on_finalize,
+         v_f05_publish_cid,
+         v_f05_deal_id,
+         v_f05_deal_proposal,
+         v_f05_deal_start_epoch,
+         v_f05_deal_end_epoch
+    ) ON CONFLICT (sp_id, sector_number, piece_index) DO NOTHING;
+    IF NOT FOUND THEN
+            RAISE EXCEPTION 'Conflict detected for piece_index %', v_piece_index;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
 
 
 
@@ -74,41 +72,39 @@ CREATE OR REPLACE FUNCTION insert_sector_ddo_piece(
     v_direct_start_epoch bigint,
     v_direct_end_epoch bigint,
     v_direct_piece_activation_manifest jsonb
-    ) RETURNS void AS $$
-    BEGIN
+) RETURNS void AS $$
+BEGIN
     INSERT INTO sectors_sdr_initial_pieces (
-        sp_id,
-        sector_number,
-        piece_index,
-        created_at,
-        piece_cid,
-        piece_size,
-        data_url,
-        data_headers,
-        data_raw_size,
-        data_delete_on_finalize,
-        direct_start_epoch,
-        direct_end_epoch,
-        direct_piece_activation_manifest
+    sp_id,
+    sector_number,
+    piece_index,
+    created_at,
+    piece_cid,
+    piece_size,
+    data_url,
+    data_headers,
+    data_raw_size,
+    data_delete_on_finalize,
+    direct_start_epoch,
+    direct_end_epoch,
+    direct_piece_activation_manifest
     ) VALUES (
-                 v_sp_id,
-                 v_sector_number,
-                 v_piece_index,
-                 NOW(),
-                 v_piece_cid,
-                 v_piece_size,
-                 v_data_url,
-                 v_data_headers,
-                 v_data_raw_size,
-                 v_data_delete_on_finalize,
-                 v_direct_start_epoch,
-                 v_direct_end_epoch,
-                 v_direct_piece_activation_manifest
-             )
-        ON CONFLICT (sp_id, sector_number, piece_index) DO NOTHING;
-
+         v_sp_id,
+         v_sector_number,
+         v_piece_index,
+         NOW(),
+         v_piece_cid,
+         v_piece_size,
+         v_data_url,
+         v_data_headers,
+         v_data_raw_size,
+         v_data_delete_on_finalize,
+         v_direct_start_epoch,
+         v_direct_end_epoch,
+         v_direct_piece_activation_manifest
+    ) ON CONFLICT (sp_id, sector_number, piece_index) DO NOTHING;
     IF NOT FOUND THEN
-            RAISE EXCEPTION 'Conflict detected for piece_index %', v_piece_index;
+        RAISE EXCEPTION 'Conflict detected for piece_index %', v_piece_index;
     END IF;
-    END;
-    $$ LANGUAGE plpgsql;
+END;
+$$ LANGUAGE plpgsql;
