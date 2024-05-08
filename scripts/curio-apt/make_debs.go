a90d1604426d740804327eb82380c4047f7fce8c
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-05-07 21:40:20 -0500

    it works

--- scripts/curio-apt/make_debs.go
+++ scripts/curio-apt/make_debs.go
@@ -13,7 +13,6 @@ import (
 	"github.com/codeskyblue/go-sh"
 )
 
-var output string
 var version string
 
 func main() {
@@ -25,13 +24,15 @@ func main() {
 
 	version = os.Args[1]
 
+	// Import the key (repeat imports are OK)
+	OrPanic(sh.Command("gpg", "--import", os.Args[2]).Run())
+
 	base, err := os.MkdirTemp(os.TempDir(), "curio-apt")
 	OrPanic(err)
 
 	part2(base, "curio-cuda", "")
 	part2(base, "curio-opencl", "FFI_USE_OPENCL=1")
-	fmt.Println("Done.")
-	fmt.Println(output)
+	fmt.Println("Done. DEB files are in ", base)
 }
 
 func part2(base, product, extra string) {
@@ -50,6 +51,8 @@ func part2(base, product, extra string) {
 		sess.SetEnv(v[0], v[1])
 	}
 	fmt.Println("making")
+
+	// This ENV is only for fixing this script. It will result in a bad build.
 	if os.Getenv("CURIO_DEB_NOBUILD") != "1" {
 		OrPanic(sess.Command("make", "clean", "all").Run())
 	}
@@ -67,7 +70,7 @@ func part2(base, product, extra string) {
 	f = []byte(strings.ReplaceAll(string(f), "$VERSION", version))
 	os.WriteFile(path.Join(dir, "DEBIAN", "control"), f, 0644)
 
-	// Option 1: piece by piece
+	// Option 1: piece by piece. Maybe could work, but it is complex.
 	// Build a .changes file
 	//OrPanic(sh.Command("dpkg-genchanges", "-b", "-u.").SetDir(dir).Run())
 	// Sign the .changes file
@@ -76,16 +79,15 @@ func part2(base, product, extra string) {
 	//OrPanic(sh.Command("dpkg-deb", "--build", ".").SetDir(dir).Run())
 
 	// Option 2: The following command should sign the deb file.
-	// FAIL B/C wants to build
+	// FAIL B/C wants to build.
 	//sh.Command("dpkg-buildpackage", "--build=binary").SetDir(dir).Run()
 
-	fmt.Println(base)
-	sh.Command("ls", "-al", base).Run()
-	// Option 3: ChatGPT Said so
+	// Option 3: Use new helpler commands outside of regular DEB stuff.
 	OrPanic(sh.NewSession().SetDir(base).Command("dpkg-deb", "--build", product).Run())
+
+	// Sign the DEB we built.
 	OrPanic(sh.NewSession().SetDir(base).Command(
-		"dpkg-sig", "--sign builder", "--signbuilder", os.Args[2],
-		product+".deb").Run())
+		"dpkg-sig", "--sign builder", "-k", "B751F6AC4FA6D98F", product+".deb").Run())
 }
 
 func copyFile(src, dest string) error {
