51731091c244d798d80b57fd6d229e7c1fbaef62
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-05-10 10:53:55 -0500

    local fonts

--- curiosrc/web/static/ux/curio-ux.mjs
+++ curiosrc/web/static/ux/curio-ux.mjs
@@ -25,7 +25,6 @@ class CurioUX extends LitElement {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="https://fonts.cdnfonts.com/css/metropolis-2" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="/ux/main.css" onload="document.body.style.visibility = 'initial'">
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 `
