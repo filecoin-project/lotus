a33a4155432fd7921e48e4a8e51d89dbc50c6935
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-05-20 16:58:14 -0400

    build commit and date

--- curiosrc/build/build.go
+++ curiosrc/build/build.go
@@ -2,3 +2,8 @@ package build
 
 // IsOpencl is set to the value of FFI_USE_OPENCL
 var IsOpencl string
+
+// Format: 8 HEX then underscore then ISO8701 date
+// Ex: 4c5e98f28_2024-05-17T18:42:27-04:00
+// NOTE: git date for repeatabile builds.
+var Commit string
