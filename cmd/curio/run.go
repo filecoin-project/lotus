e8bb6904e1be774b4a01a7d6ab2c92b3074154df
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-05-22 10:21:02 -0500

    oops

--- cmd/curio/run.go
+++ cmd/curio/run.go
@@ -9,10 +9,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 	"go.opencensus.io/stats"
-	"go.opencensus.io/tag"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/cmd/curio/deps"
 	"github.com/filecoin-project/lotus/cmd/curio/rpc"
@@ -89,11 +87,7 @@ var runCmd = &cli.Command{
 			log.Errorf("ensuring tempdir exists: %s", err)
 		}
 
-		ctx, _ := tag.New(lcli.DaemonContext(cctx),
-			tag.Insert(metrics.Version, build.BuildVersion),
-			tag.Insert(metrics.Commit, build.CurrentCommit),
-			tag.Insert(metrics.NodeType, "curio"),
-		)
+		ctx := lcli.DaemonContext(cctx)
 		shutdownChan := make(chan struct{})
 		{
 			var ctxclose func()
@@ -126,7 +120,7 @@ var runCmd = &cli.Command{
 			return err
 		}
 
-		go ffiSelfTest()
+		go ffiSelfTest() // Panics on failure
 
 		taskEngine, err := tasks.StartTasks(ctx, dependencies)
 
