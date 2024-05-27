0cbece4db64a50e1799b3a75ffff20bb7f9fdbdf
Łukasz Magiera <magik6k@gmail.com>
2024-05-27 18:52:37 +0200

    Trim harmonydb schema to what's needed here

--- lib/harmony/harmonydb/sql/20240401-storage-miner-filter.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE storage_path ADD COLUMN IF NOT EXISTS allow_miners varchar DEFAULT '';
-ALTER TABLE storage_path ADD COLUMN IF NOT EXISTS deny_miners varchar DEFAULT ''; -- comma separated list of miner addresses
