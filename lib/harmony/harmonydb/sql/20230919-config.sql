dce7f5d32d1cf9aa28f15f24c4ba592454da48d4
Łukasz Magiera <magik6k@gmail.com>
2024-05-27 18:52:37 +0200

    Trim harmonydb schema to what's needed here

--- lib/harmony/harmonydb/sql/20230919-config.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE harmony_config (
-    id SERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(300) UNIQUE NOT NULL,
-    config TEXT NOT NULL
-);
--- lib/harmony/harmonydb/sql/20240416-harmony_singleton_task.sql
+++
@@ -1,8 +0,0 @@
-create table harmony_task_singletons (
-    task_name varchar(255) not null,
-    task_id bigint,
-    last_run_time timestamp,
-
-    primary key (task_name),
-    foreign key (task_id) references harmony_task (id) on delete set null
-);
