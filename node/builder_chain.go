c404a6df7b9782dc8f9eae9a458761b3767ab453
Jakub Sztandera <oss@kubuxu.com>
2024-06-24 17:14:37 +0200

    Fix building
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- node/builder_chain.go
+++ node/builder_chain.go
@@ -151,7 +151,7 @@ var ChainNode = Options(
 		Override(HandleIncomingBlocksKey, modules.HandleIncomingBlocks),
 	),
 
-	ApplyIf(build.F3Enabled, Override(new(*lf3.F3), lf3.New)),
+	If(build.F3Enabled, Override(new(*lf3.F3), lf3.New)),
 )
 
 func ConfigFullNode(c interface{}) Option {
