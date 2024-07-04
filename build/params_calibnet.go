4843962388bfbd29dc44fde1e82c930681808719
Jiaying Wang <42981373+jennijuju@users.noreply.github.com>
2024-07-04 05:04:09 -0400

    Update f3 setting for calib (#12181)

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -152,5 +152,5 @@ const Eip155ChainId = 314159
 
 var WhitelistedBlock = cid.Undef
 
-const F3Enabled = false
-const F3BootstrapEpoch abi.ChainEpoch = -1
+const F3Enabled = true
+const F3BootstrapEpoch abi.ChainEpoch = UpgradeWaffleHeight + 100
