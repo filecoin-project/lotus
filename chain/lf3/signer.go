04dc2f8ec479082037e2ed032a6f66eefdd54918
Jakub Sztandera <oss@kubuxu.com>
2024-07-08 13:49:10 +0200

    Update go-f3
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- chain/lf3/signer.go
+++ chain/lf3/signer.go
@@ -11,28 +11,22 @@ import (
 	"github.com/filecoin-project/lotus/api"
 )
 
+var _ gpbft.Signer = (*signer)(nil)
+
 type signer struct {
 	wallet api.Wallet
 }
 
 // Sign signs a message with the private key corresponding to a public key.
 // The the key must be known by the wallet and be of BLS type.
-func (s *signer) Sign(sender gpbft.PubKey, msg []byte) ([]byte, error) {
+func (s *signer) Sign(ctx context.Context, sender gpbft.PubKey, msg []byte) ([]byte, error) {
 	addr, err := address.NewBLSAddress(sender)
 	if err != nil {
 		return nil, xerrors.Errorf("converting pubkey to address: %w", err)
 	}
-	sig, err := s.wallet.WalletSign(context.TODO(), addr, msg, api.MsgMeta{Type: api.MTUnknown})
+	sig, err := s.wallet.WalletSign(ctx, addr, msg, api.MsgMeta{Type: api.MTUnknown})
 	if err != nil {
 		return nil, xerrors.Errorf("error while signing: %w", err)
 	}
 	return sig.Data, nil
-}
-
-// MarshalPayloadForSigning marshals the given payload into the bytes that should be signed.
-// This should usually call `Payload.MarshalForSigning(NetworkName)` except when testing as
-// that method is slow (computes a merkle tree that's necessary for testing).
-// Implementations must be safe for concurrent use.
-func (s *signer) MarshalPayloadForSigning(nn gpbft.NetworkName, p *gpbft.Payload) []byte {
-	return p.MarshalForSigning(nn)
 }
