9e7442898e3cb1712f2e44080d67640faeddd99d
Andrew Jackson (Ajax) <snadrus@gmail.com>
2024-07-15 08:42:33 -0500

    fix and remove obsolete

--- build/buildconstants/params_interop.go
+++ build/buildconstants/params_interop.go
@@ -8,15 +8,12 @@ import (
 	"strconv"
 
 	"github.com/ipfs/go-cid"
-	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
-
-var log = logging.Logger("buildconstants")
 
 var NetworkBundle = "caterpillarnet"
 var ActorDebugging = false
