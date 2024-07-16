3d42fb6227979fb4baed339516a41cdf825421c6
Peter Rabbitson <ribasushi@leporine.io>
2024-07-16 15:23:36 +0200

    chore: move BuildTypeString() to the buildconstants
    While technically the entire "what are we building" should stay in build/... ,
    not having the static-ish machinery split across 2 namespaces is an improvement

--- build/version.go
+++ build/version.go
@@ -6,38 +6,6 @@ import (
 	"github.com/filecoin-project/lotus/build/buildconstants"
 )
 
-type BuildVersion string
-
-var CurrentCommit string
-var BuildType = buildconstants.BuildType                 // Deprecated: Use buildconstants.BuildType instead
-var BuildMainnet = buildconstants.BuildMainnet           // Deprecated: Use buildconstants.BuildMainnet instead
-var Build2k = buildconstants.Build2k                     // Deprecated: Use buildconstants.Build2k instead
-var BuildDebug = buildconstants.BuildDebug               // Deprecated: Use buildconstants.BuildDebug instead
-var BuildCalibnet = buildconstants.BuildCalibnet         // Deprecated: Use buildconstants.BuildCalibnet instead
-var BuildInteropnet = buildconstants.BuildInteropnet     // Deprecated: Use buildconstants.BuildInteropnet instead
-var BuildButterflynet = buildconstants.BuildButterflynet // Deprecated: Use buildconstants.BuildButterflynet instead
-
-func BuildTypeString() string {
-	switch BuildType {
-	case buildconstants.BuildDefault:
-		return ""
-	case buildconstants.BuildMainnet:
-		return "+mainnet"
-	case buildconstants.Build2k:
-		return "+2k"
-	case buildconstants.BuildDebug:
-		return "+debug"
-	case buildconstants.BuildCalibnet:
-		return "+calibnet"
-	case buildconstants.BuildInteropnet:
-		return "+interopnet"
-	case buildconstants.BuildButterflynet:
-		return "+butterflynet"
-	default:
-		return "+huh?"
-	}
-}
-
 // NodeBuildVersion is the local build version of the Lotus daemon
 const NodeBuildVersion string = "1.28.0-rc5"
 
@@ -59,3 +27,17 @@ func MinerUserVersion() BuildVersion {
 
 	return BuildVersion(MinerBuildVersion + BuildTypeString() + CurrentCommit)
 }
+
+var BuildType = buildconstants.BuildType                 // Deprecated: Use buildconstants.BuildType instead
+var BuildMainnet = buildconstants.BuildMainnet           // Deprecated: Use buildconstants.BuildMainnet instead
+var Build2k = buildconstants.Build2k                     // Deprecated: Use buildconstants.Build2k instead
+var BuildDebug = buildconstants.BuildDebug               // Deprecated: Use buildconstants.BuildDebug instead
+var BuildCalibnet = buildconstants.BuildCalibnet         // Deprecated: Use buildconstants.BuildCalibnet instead
+var BuildInteropnet = buildconstants.BuildInteropnet     // Deprecated: Use buildconstants.BuildInteropnet instead
+var BuildButterflynet = buildconstants.BuildButterflynet // Deprecated: Use buildconstants.BuildButterflynet instead
+
+var BuildTypeString = buildconstants.BuildTypeString // Deprecated: Use buildconstants.BuildTypeString instead
+
+type BuildVersion string
+
+var CurrentCommit string
