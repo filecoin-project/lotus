08049927c904fd73ec4d7c4a970c104cb5ba79fd
Masih H. Derkani <m@derkani.org>
2024-07-23 22:49:40 +0100

    Set up otel metrics reporting to prometheus
    Register otel collector bridging to the default prometheus registry so
    that F3 metrics (which use the newer otel APIs for metrics) get included
    in Lotus metrics.

--- chain/lf3/f3.go
+++ chain/lf3/f3.go
@@ -10,6 +10,9 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
@@ -49,6 +52,20 @@ type F3Params struct {
 }
 
 var log = logging.Logger("f3")
+
+func init() {
+	// Set up otel to prometheus reporting so that F3 metrics are reported via lotus
+	// prometheus metrics. This bridge eventually gets picked up by opencensus
+	// exporter as HTTP handler. This by default registers an otel collector against
+	// the global prometheus registry. In the future, we should clean up metrics in
+	// Lotus and move it all to use otel. For now, bridge away.
+	if bridge, err := prometheus.New(); err != nil {
+		log.Errorf("could not create the otel prometheus exporter: %v", err)
+	} else {
+		provider := metric.NewMeterProvider(metric.WithReader(bridge))
+		otel.SetMeterProvider(provider)
+	}
+}
 
 func New(mctx helpers.MetricsCtx, lc fx.Lifecycle, params F3Params) (*F3, error) {
 
