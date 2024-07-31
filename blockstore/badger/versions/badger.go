2a6504476b047b1a3d62598cd50f2a1e5bb87956
Mikers <snissn@gmail.com>
2024-07-31 09:53:42 -1000

    clamp unused

--- blockstore/badger/versions/badger.go
+++ blockstore/badger/versions/badger.go
@@ -48,13 +48,3 @@ func OpenBadgerDB(opts Options) (BadgerDB, error) {
 	}
 	return db, nil
 }
-
-func clamp(x, min, max int) int {
-	if x < min {
-		return min
-	}
-	if x > max {
-		return max
-	}
-	return x
-}
