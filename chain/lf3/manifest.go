bd1364191e781ff5cf5d6c7e2cf5fa60c1f11c40
Steven Allen <steven@stebalien.com>
2024-09-20 14:07:01 +0200

    remove unused code

--- chain/lf3/manifest.go
+++ chain/lf3/manifest.go
@@ -4,7 +4,6 @@ import (
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	"go.uber.org/fx"
 
 	"github.com/filecoin-project/go-f3/manifest"
 
@@ -16,13 +15,6 @@ import (
 // If the manifest changes more than this number, the F3
 // message topic will be filtered
 var MaxDynamicManifestChangesAllowed = 1000
-
-type ManifestOut struct {
-	fx.Out
-
-	ManifestProvider manifest.ManifestProvider
-	PubsubTopics     []string `group:"pubsub-topic,flatten"`
-}
 
 func NewManifestProvider(config *Config, ps *pubsub.PubSub, mds dtypes.MetadataDS) manifest.ManifestProvider {
 	if config.DynamicManifestProvider == "" {
