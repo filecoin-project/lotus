1b9b81537b34424eac56b48c67b921ca7d994d19
Steven Allen <steven@stebalien.com>
2024-10-07 15:03:55 +0000

    feat(f3): prepare for f3 bootstrap (#12552)
    This patch:
    
    1. Bootstraps F3 when we hit the `F3BootstrapEpoch` (when non-negative).
    2. Refuses any/all dynamic manifests once we get within one finality of said epoch.
    3. Sets the F3 network name for mainnet to "filecoin".
    4. Refuses any/all dynamic manifests that don't start with the expected
        network name prefix.

--- itests/f3_test.go
+++ itests/f3_test.go
@@ -5,15 +5,18 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/filecoin-project/go-f3/gpbft"
 	"github.com/filecoin-project/go-f3/manifest"
 	"github.com/filecoin-project/go-state-types/abi"
 
+	"github.com/filecoin-project/lotus/chain/lf3"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/itests/kit"
 	"github.com/filecoin-project/lotus/node"
@@ -21,8 +24,9 @@ import (
 )
 
 const (
-	DefaultBootsrapEpoch = 20
-	DefaultFinality      = 5
+	DefaultBootstrapEpoch                   = 20
+	DefaultFinality                         = 5
+	BaseNetworkName       gpbft.NetworkName = "test"
 )
 
 type testEnv struct {
@@ -61,7 +65,7 @@ func TestF3_Rebootstrap(t *testing.T) {
 
 	cpy := *e.m
 	cpy.BootstrapEpoch = 25
-	cpy.NetworkName += "/1"
+	cpy.NetworkName = BaseNetworkName + "/2"
 	e.ms.UpdateManifest(&cpy)
 
 	newManifest := e.waitTillManifestChange(&cpy, 20*time.Second)
@@ -90,12 +94,41 @@ func TestF3_PauseAndRebootstrap(t *testing.T) {
 	e.waitTillF3Runs(30 * time.Second)
 
 	cpy := *e.m
-	cpy.NetworkName += "/1"
+	cpy.NetworkName = BaseNetworkName + "/2"
 	cpy.BootstrapEpoch = 25
 	e.ms.UpdateManifest(&cpy)
 
 	e.waitTillManifestChange(&cpy, 20*time.Second)
 	e.waitTillF3Rebootstrap(20 * time.Second)
+}
+
+// Tests that pause/resume and rebootstrapping F3 works
+func TestF3_Bootstrap(t *testing.T) {
+	kit.QuietMiningLogs()
+
+	var bootstrapEpoch abi.ChainEpoch = 50
+	blocktime := 100 * time.Millisecond
+	staticManif := lf3.NewManifest(BaseNetworkName, DefaultFinality, bootstrapEpoch, blocktime, cid.Undef)
+	dynamicManif := *staticManif
+	dynamicManif.BootstrapEpoch = 5
+	dynamicManif.EC.Finalize = false
+	dynamicManif.NetworkName = BaseNetworkName + "/1"
+
+	e := setupWithStaticManifest(t, staticManif, true)
+	e.ms.UpdateManifest(&dynamicManif)
+	e.waitTillManifestChange(&dynamicManif, 20*time.Second)
+	e.waitTillF3Instance(2, 20*time.Second)
+	e.waitTillManifestChange(staticManif, 20*time.Second)
+	e.waitTillF3Instance(2, 20*time.Second)
+
+	// Try to switch back, we should ignore the manifest update.
+	e.ms.UpdateManifest(&dynamicManif)
+	time.Sleep(time.Second)
+	for _, n := range e.minerFullNodes {
+		m, err := n.F3GetManifest(e.testCtx)
+		require.NoError(e.t, err)
+		require.True(t, m.Equal(staticManif))
+	}
 }
 
 func (e *testEnv) waitTillF3Rebootstrap(timeout time.Duration) {
@@ -176,8 +209,15 @@ func (e *testEnv) waitFor(f func(n *kit.TestFullNode) bool, timeout time.Duratio
 // and the second full-node is an observer that is not directly connected to
 // a miner. The last return value is the manifest sender for the network.
 func setup(t *testing.T, blocktime time.Duration) *testEnv {
+	manif := lf3.NewManifest(BaseNetworkName+"/1", DefaultFinality, DefaultBootstrapEpoch, blocktime, cid.Undef)
+	return setupWithStaticManifest(t, manif, false)
+}
+
+func setupWithStaticManifest(t *testing.T, manif *manifest.Manifest, testBootstrap bool) *testEnv {
 	ctx, stopServices := context.WithCancel(context.Background())
 	errgrp, ctx := errgroup.WithContext(ctx)
+
+	blocktime := manif.EC.Period
 
 	t.Cleanup(func() {
 		stopServices()
@@ -188,7 +228,15 @@ func setup(t *testing.T, blocktime time.Duration) *testEnv {
 	manifestServerHost, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/127.0.0.1/udp/0/quic-v1"))
 	require.NoError(t, err)
 
-	f3NOpt := kit.F3Enabled(DefaultBootsrapEpoch, blocktime, DefaultFinality, manifestServerHost.ID())
+	cfg := &lf3.Config{
+		BaseNetworkName:          BaseNetworkName,
+		StaticManifest:           manif,
+		DynamicManifestProvider:  manifestServerHost.ID(),
+		PrioritizeStaticManifest: testBootstrap,
+		AllowDynamicFinalize:     !testBootstrap,
+	}
+
+	f3NOpt := kit.F3Enabled(cfg)
 	f3MOpt := kit.ConstructorOpts(node.Override(node.F3Participation, modules.F3Participation))
 
 	var (
