9a64de7625fb4307942478296fc5ccc209a35308
aarshkshah1992 <aarshkshah1992@gmail.com>
2024-10-07 15:11:37 +0400

    fix var name

--- chain/index/api.go
+++ chain/index/api.go
@@ -121,7 +121,7 @@ func (si *SqliteIndexer) ChainValidateIndex(ctx context.Context, epoch abi.Chain
 	}
 
 	indexedData, err := getAndVerifyIndexedData()
-	var backfill bool
+	var bf bool
 	if err != nil {
 		if !backfill {
 			return nil, xerrors.Errorf("failed to verify indexed data at height %d: %w", expectedTs.Height(), err)
@@ -136,7 +136,7 @@ func (si *SqliteIndexer) ChainValidateIndex(ctx context.Context, epoch abi.Chain
 		if err != nil {
 			return nil, xerrors.Errorf("failed to verify indexed data at height %d after backfill: %w", expectedTs.Height(), err)
 		}
-		backfill = true
+		bf = true
 	}
 
 	return &types.IndexValidation{
@@ -145,7 +145,7 @@ func (si *SqliteIndexer) ChainValidateIndex(ctx context.Context, epoch abi.Chain
 		IndexedMessagesCount:     uint64(indexedData.nonRevertedMessageCount),
 		IndexedEventsCount:       uint64(indexedData.nonRevertedEventCount),
 		IndexedEventEntriesCount: uint64(indexedData.nonRevertedEventEntriesCount),
-		Backfilled:               backfill,
+		Backfilled:               bf,
 	}, nil
 }
 
