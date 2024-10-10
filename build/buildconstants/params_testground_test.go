a116278c9874b24e016e36486b4ac23357fa413d
Masih H. Derkani <m@derkani.org>
2024-10-10 15:48:03 +0100

    Set address current network for non-mainnet since default is changed (#12574)
    Default `address.CurrentNetwork` was changed in an upstream dependency.
    Update the build constants for non-mainnet to explicitly set it to
    `Testnet`.
    
    Add tests to assert it.

---
+++ build/buildconstants/params_testground_test.go
@@ -0,0 +1,16 @@
+//go:build testground
+// +build testground
+
+package buildconstants
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-address"
+)
+
+func Test_NetworkName(t *testing.T) {
+	require.Equal(t, address.CurrentNetwork, address.Testnet)
+}
