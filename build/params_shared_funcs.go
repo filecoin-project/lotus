2f38c67fd507eb2ff269a75cfb34e40233bc6b46
Jakub Sztandera <oss@kubuxu.com>
2024-11-06 14:55:10 +0700

    feat(f3): allow to disable f3 passive testing
    using `LOTUS_DISABLE_F3_PASSIVE_TESTING=1`
    
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -55,3 +55,24 @@ func IsF3Enabled() bool {
 		return false
 	}
 }
+
+func IsF3PassiveTestingEnabled() bool {
+	if !IsF3Enabled() {
+		return false
+	}
+	const F3DisablePassiveTesting = "LOTUS_DISABLE_F3_PASSIVE_TESTING"
+
+	v, disableEnvVarSet := os.LookupEnv(F3DisablePassiveTesting)
+	if !disableEnvVarSet {
+		// Environment variable to disable F3 passive testing is not set.
+		return true
+	}
+	switch strings.TrimSpace(strings.ToLower(v)) {
+	case "", "0", "false", "no":
+		// Consider these values as "do not disable".
+		return true
+	default:
+		// Consider any other value as disable.
+		return false
+	}
+}
