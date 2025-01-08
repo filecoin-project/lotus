553dae1fe69666978c65e8881c77095066319884
taozui472 <taozui472@gmail.com>
2025-01-08 09:56:07 +0800

    remove duplication words

--- itests/sector_miner_collateral_test.go
+++ itests/sector_miner_collateral_test.go
@@ -326,7 +326,7 @@ func TestPledgeCalculations(t *testing.T) {
 		return markettypes.ClientDealProposal{Proposal: dp, ClientSignature: *sig}
 	}
 
-	// make a deal of a given size that may or may not be be verified (and require an allocation)
+	// make a deal of a given size that may or may not be verified (and require an allocation)
 	makeDealOfSize := func(paddedSize int, verified bool) (abi.PieceInfo, abi.DealID) {
 		marketPieceSize := abi.PaddedPieceSize(paddedSize)
 		pieceData := make([]byte, marketPieceSize)
