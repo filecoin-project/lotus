04a5f33546ab82724c4b070c6b5e9ba8fd8f73fb
Phi-rjan <orjan.roren@gmail.com>
2025-02-17 16:15:32 +0100

    chore: fix comments in .go (#12903)
    * Update api_full.go
    
    * Update api_storage.go
    
    * chore: make gen
    
    chore: make gen
    
    ---------
    
    Co-authored-by: comfsrt <155266597+comfsrt@users.noreply.github.com>

--- api/api_storage.go
+++ api/api_storage.go
@@ -215,7 +215,7 @@ type StorageMiner interface {
 	// in this instance.
 	RuntimeSubsystems(ctx context.Context) (MinerSubsystems, error) //perm:read
 
-	// CreateBackup creates node backup onder the specified file name. The
+	// CreateBackup creates node backup under the specified file name. The
 	// method requires that the lotus-miner is running with the
 	// LOTUS_BACKUP_BASE_PATH environment variable set to some path, and that
 	// the path specified when calling CreateBackup is within the base path
