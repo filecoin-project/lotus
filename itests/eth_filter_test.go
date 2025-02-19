24db4103aa61d80631e302d4de836786a68cc50d
Masih H. Derkani <m@derkani.org>
2025-02-19 17:26:30 +0000

    Fix lint issues:
    * `non-constant format string`

--- itests/eth_filter_test.go
+++ itests/eth_filter_test.go
@@ -2344,7 +2344,7 @@ func AssertEthLogs(t *testing.T, actual []*ethtypes.EthLog, expected []ExpectedE
 				}
 			}
 
-			t.Errorf(buf.String())
+			t.Error(buf.String())
 		}
 	}
 
@@ -2355,7 +2355,7 @@ func AssertEthLogs(t *testing.T, actual []*ethtypes.EthLog, expected []ExpectedE
 			buf.WriteString(fmt.Sprintf("  address: %s\n", expected[i].Address))
 			buf.WriteString(fmt.Sprintf("  topics: %s\n", formatTopics(expected[i].Topics)))
 			buf.WriteString(fmt.Sprintf("  data: %x\n", expected[i].Data))
-			t.Errorf(buf.String())
+			t.Error(buf.String())
 		}
 	}
 }
