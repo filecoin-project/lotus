3dc3767ea12baa467d6a3b88ba59bf09ca940e1b
Phi <orjan.roren@gmail.com>
2025-06-05 11:33:19 +0200

    chore: update butterfly-nv25 upgrade height
    chore: update butterfly-nv25 upgrade height

--- build/buildconstants/params_butterfly.go
+++ build/buildconstants/params_butterfly.go
@@ -75,7 +75,7 @@ const UpgradeTuktukHeight = -28
 var UpgradeTuktukPowerRampDurationEpochs = uint64(builtin.EpochsInYear)
 
 // ??????
-const UpgradeTeepHeight = 4320
+const UpgradeTeepHeight = 100
 
 var UpgradeTeepInitialFilReserved = wholeFIL(1_600_000_000) // FIP-0100: 300M -> 1.6B FIL
 
