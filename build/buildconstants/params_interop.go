9e5d3f49ae6b861299471df9cbf687fcb6b3a8b6
Phi-rjan <orjan.roren@gmail.com>
2025-09-11 18:55:51 +0200

    chore: update nv27 codename (#13334)
    * chore: update nv27 codename
    
    chore: update nv27 codename
    
    * chore: update nv27 codename
    
    chore: update nv27 codename

--- build/buildconstants/params_interop.go
+++ build/buildconstants/params_interop.go
@@ -82,7 +82,7 @@ var UpgradeTockHeight = abi.ChainEpoch(-30)
 // This fix upgrade only ran on calibrationnet
 const UpgradeTockFixHeight abi.ChainEpoch = -4
 
-const UpgradeXxHeight = 50
+const UpgradeGoldenWeekHeight = 50
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandQuicknet,
