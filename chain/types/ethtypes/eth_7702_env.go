eb29fda740723f131d35c92b053e5b3ad7432bd1
Mikers <snissn@gmail.com>
2025-11-07 23:32:13 -1000

    ethtypes: add EthAccountApplyAndCallActorAddr stub for non-tag builds

--- chain/types/ethtypes/eth_7702_env.go
+++ chain/types/ethtypes/eth_7702_env.go
@@ -4,3 +4,4 @@ import "github.com/filecoin-project/go-address"
 
 // Stub declarations for builds without the eip7702_enabled tag so references compile.
 var EvmApplyAndCallActorAddr address.Address
+var EthAccountApplyAndCallActorAddr address.Address
