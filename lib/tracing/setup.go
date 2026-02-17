958a1d11bf0f9052fe0191bf8bec19f00ac4ad6d
Rod Vagg <rod@vagg.org>
2026-02-17 16:05:01 +1100

    chore(tracing): update otel semconv v1.7.0 to v1.39.0

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -9,7 +9,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 	"go.uber.org/zap"
 )
 
