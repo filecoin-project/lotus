ed6b61667b1580d601deddc074ee4df0837375ad
LexLuthr <lexluthr@curiostorage.org>
2026-09-18 19:14:13 +0400

    fix apply speed

--- chain/consensus/filcns/filecoin.go
+++ chain/consensus/filcns/filecoin.go
@@ -78,9 +78,13 @@ var RewardFunc = func(ctx context.Context, vmi vm.Interface, em stmgr.ExecMonito
 		Params:     ser,
 	}
 
-	before, flushErr := vmi.Flush(ctx)
-	if flushErr != nil {
-		return xerrors.Errorf("flushing state before reward: %w", flushErr)
+	var before, after cid.Cid
+	var flushErr error
+	if em != nil {
+		before, flushErr = vmi.Flush(ctx)
+		if flushErr != nil {
+			return xerrors.Errorf("flushing state before reward: %w", flushErr)
+		}
 	}
 
 	ret, actErr := vmi.ApplyImplicitMessage(ctx, rwMsg)
@@ -98,12 +102,12 @@ var RewardFunc = func(ctx context.Context, vmi vm.Interface, em stmgr.ExecMonito
 		}
 	}
 
-	after, flushErr := vmi.Flush(ctx)
-	if flushErr != nil {
-		return xerrors.Errorf("flushing state after reward: %w", flushErr)
-	}
-
 	if em != nil {
+		after, flushErr = vmi.Flush(ctx)
+		if flushErr != nil {
+			return xerrors.Errorf("flushing state after reward: %w", flushErr)
+		}
+
 		if err := em.RewardApplied(ts, before, after, rwMsg, ret); err != nil {
 			return xerrors.Errorf("observing reward state: %w", err)
 		}
