127c16d11af4fe3eeb094cbcff12788114a3121a
Rod Vagg <rod@vagg.org>
2026-09-19 18:16:04 +1000

    chore(butterflynet): schedule Solstice at epoch 5220 for the 2026-09-19 gamma reset

--- build/buildconstants/params_butterfly.go
+++ build/buildconstants/params_butterfly.go
@@ -86,7 +86,7 @@ var UpgradeGoldenWeekHeight = abi.ChainEpoch(-31)
 
 const UpgradeFireHorseHeight = -32
 
-const UpgradeSolsticeHeight = 5340
+const UpgradeSolsticeHeight = 5220
 
 // SolsticeEpochsPerQuarter matches the quarter the SRA is deployed with: two hours on
 // butterflynet. The ramp runs nine of them.
